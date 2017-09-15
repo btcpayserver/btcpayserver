99ae549c244591675ecfe30e76559c20c7f4d979
NicolasDorier <nicolas.dorier@gmail.com>
2017-09-16 01:20:57 +0900

    prevent non admin to go to server pages

--- BTCPayServer/Controllers/ServerController.cs
+++ BTCPayServer/Controllers/ServerController.cs
@@ -1,5 +1,6 @@
 ﻿using BTCPayServer.Models;
 using BTCPayServer.Models.ServerViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace BTCPayServer.Controllers
 {
+	[Authorize(Roles=Roles.ServerAdmin)]
 	public class ServerController : Controller
 	{
 		private UserManager<ApplicationUser> _UserManager;

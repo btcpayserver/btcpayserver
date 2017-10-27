59de852484cb7c9d05b6a939337d2437a65211ab
nicolas.dorier <nicolas.dorier@gmail.com>
2017-10-27 12:11:08 +0900

    remove warning

--- BTCPayServer/Services/BTCPayServerEnvironment.cs
+++ BTCPayServer/Services/BTCPayServerEnvironment.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using System.Reflection;
 using System.Text;
 
 namespace BTCPayServer.Services

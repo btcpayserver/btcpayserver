0074a8924c6bc3208b012d1d8f42ef1d6dd2561f
Esky33 <djescaba@me.com>
2018-01-18 23:49:22 +0000

    update
    roll back to  <Version>1.0.0.83</Version>

--- BTCPayServer.Tests/docker-litecoin-cli.ps1
+++
@@ -1,1 +0,0 @@
-docker exec -ti btcpayserver_dev_litecoind litecoin-cli -regtest -conf="/data/litecoin.conf" -datadir="/data" $args
--- publish-docker.ps1
+++
@@ -1,3 +0,0 @@
-$ver = [regex]::Match((Get-Content BTCPayServer\BTCPayServer.csproj), '<Version>([^<]+)<').Groups[1].Value
-git tag -a "v$ver" -m "$ver"
-git push --tags

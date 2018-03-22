ab8d3f5813f14547b872d4cdbaf5f1f5a4121b9e
lepipele <lepipele@users.noreply.github.com>
2018-03-22 12:02:55 -0500

    Extracting strings for translation

--- BTCPayServer/wwwroot/js/checkout/langs/en.js
+++ BTCPayServer/wwwroot/js/checkout/langs/en.js
@@ -7,5 +7,33 @@ const locales_en = {
     "Scan": "Scan",
     "Copy": "Copy",
     "Conversion": "Conversion",
-    "Open in wallet": "Open in wallet"
+    "Open in wallet": "Open in wallet",
+    "Contact and Refund Email": "Contact & Refund Email",
+    "Contact_Body": "Please provide an email address below. We’ll contact you at this address if there is an issue with your payment.",
+    "Your email": "Your email",
+    "Continue": "Continue",
+    "Please enter a valid email address": "Please enter a valid email address",
+    "Order Amount": "Order Amount",
+    "Network Cost": "Network Cost",
+    "Already Paid": "Already Paid",
+    "Due": "Due",
+    // Copy tab
+    "CompletePay_Body": "To complete your payment, please send {{btcDue}} {{cryptoCode}} to the address below.",
+    "Amount": "Amount",
+    "Address": "Address",
+    "Copied": "Copied",
+    // Conversion tab
+    "ConversionTab_BodyTop": "You can pay {{btcDue}} {{cryptoCode}} using altcoins other than the ones merchant directly supports.",
+    "ConversionTab_BodyDesc": "This service is provided by 3rd party. Please keep in mind that we have no control over how providers will forward your funds. Invoice will only be marked paid once funds are received on {{cryptoCode}} Blockchain.",
+    "Shapeshift_Button_Text": "Pay with Altcoins",
+    // Invoice expired
+    "Invoice expired": "Invoice expired",
+    "What happened?": "What happened?",
+    "InvoiceExpired_Body": "This invoice has expired. An invoice is only valid for 15 minutes. You can return to TestStore  \
+if you would like to submit your payment again. \
+If you tried to send a payment, it has not yet been accepted by the Bitcoin network. We have not yet received your funds.    \
+If the transaction is not accepted by the Bitcoin network, the funds will be spendable again in your wallet. Depending on your wallet, this may take 48-72 hours.",
+    "Invoice ID": "Invoice ID",
+    "Order ID": "Order ID",
+    "Return to StoreName": "Return to {{storeName}}"
 };

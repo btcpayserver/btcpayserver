97eedc2c9ff500aca4c49b52f72be3edc0d7eb45
lepipele <lepipele@users.noreply.github.com>
2018-03-22 23:33:47 -0500

    German test translation completed

--- BTCPayServer/wwwroot/js/checkout/langs/de.js
+++ BTCPayServer/wwwroot/js/checkout/langs/de.js
@@ -4,19 +4,45 @@ const locales_de = {
     },
     "Awaiting Payment...": "Warten auf Zahlung...",
     "Pay with": "Bezahlen mit",
-    "Contact and Refund Email": "Contact & Refund Email",
-    "Contact_Body": "Please provide an email address below. We’ll contact you at this address if there is an issue with your payment.",
-    "Your email": "Your email",
-    "Continue": "Continue",
-    "Please enter a valid email address": "Please enter a valid email address",
-    "Order Amount": "Order Amount",
-    "Network Cost": "Network Cost",
-    "Already Paid": "Already Paid",
-    "Due": "Due",
+    "Contact and Refund Email": "Kontakt und Rückerstattungs Email",
+    "Contact_Body": "Bitte geben Sie unten eine E-Mail-Adresse an. Wir werden Sie unter dieser Adresse kontaktieren, wenn ein Problem mit Ihrer Zahlung vorliegt.",
+    "Your email": "Deine Email",
+    "Continue": "Fortsetzen",
+    "Please enter a valid email address": "Bitte geben Sie eine gültige E-Mail-Adresse ein",
+    "Order Amount": "Bestellbetrag",
+    "Network Cost": "Netzwerkkosten",
+    "Already Paid": "Bereits bezahlt",
+    "Due": "Fällig",
     // Tabs
     "Scan": "Scan",
     "Copy": "Kopieren",
     "Conversion": "Umwandlung",
     // Scan tab
-    "Open in wallet": "In der Brieftasche öffnen"
+    "Open in wallet": "In der Brieftasche öffnen",
+    // Copy tab
+    "CompletePay_Body": "Um Ihre Zahlung abzuschließen, senden Sie bitte {{btcDue}} {{cryptoCode}} an die unten angegebene Adresse.",
+    "Amount": "Menge",
+    "Address": "Adresse",
+    "Copied": "Kopiert",
+    // Conversion tab
+    "ConversionTab_BodyTop": "Sie können {{btcDue}} {{cryptoCode}} mit altcoins bezahlen, die nicht direkt vom Händler unterstützt werden.",
+    "ConversionTab_BodyDesc": "Dieser Service wird von Drittanbietern bereitgestellt. Bitte beachten Sie, dass wir keine Kontrolle darüber haben, wie die Anbieter Ihre Gelder weiterleiten. Die Rechnung wird erst bezahlt, wenn das Geld in {{cryptoCode}} Blockchain eingegangen ist.",
+    "Shapeshift_Button_Text": "Bezahlen mit Altcoins",
+    "ConversionTab_Lightning": "Für BTC Lightning Network-Zahlungen sind keine Conversion-Anbieter verfügbar.",
+    // Invoice expired
+    "Invoice expiring soon...": "Die Rechnung läuft bald ab...",
+    "Invoice expired": "Die Rechnung ist abgelaufen",
+    "What happened?": "Was ist passiert?",
+    "InvoiceExpired_Body_1": "Diese Rechnung ist abgelaufen. Eine Rechnung ist nur für {{maxTimeMinutes}} Minuten gültig. \
+Sie können zu {{storeName}} zurückkehren, wenn Sie Ihre Zahlung erneut senden möchten.",
+    "InvoiceExpired_Body_2": "Wenn Sie versucht haben, eine Zahlung zu senden, wurde sie vom Bitcoin-Netzwerk noch nicht akzeptiert. Wir haben Ihre Gelder noch nicht erhalten.",
+    "InvoiceExpired_Body_3": "Wenn die Transaktion vom Bitcoin-Netzwerk nicht akzeptiert wird, ist das Geld wieder in Ihrer Brieftasche verfügbar. Abhängig von Ihrem Geldbeutel, kann dies 48-72 Stunden dauern.",
+    "Invoice ID": "Rechnungs ID",
+    "Order ID": "Auftrag ID",
+    "Return to StoreName": "Zurück zu {{storeName}}",
+    // Invoice paid
+    "This invoice has been paid": "Diese Rechnung wurde bezahlt",
+    // Invoice archived
+    "This invoice has been archived": "Diese Rechnung wurde archiviert",
+    "Archived_Body": "Bitte kontaktieren Sie den Shop für Bestellinformationen oder Hilfe"
 };

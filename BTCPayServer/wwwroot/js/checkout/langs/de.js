d14040c1425c17f80ad771d7dc14014b91b21925
lepipele <lepipele@users.noreply.github.com>
2018-03-22 23:04:42 -0500

    Foundation for future translations of supported languages

--- BTCPayServer/wwwroot/js/checkout/langs/de.js
+++ BTCPayServer/wwwroot/js/checkout/langs/de.js
@@ -4,8 +4,19 @@ const locales_de = {
     },
     "Awaiting Payment...": "Warten auf Zahlung...",
     "Pay with": "Bezahlen mit",
+    "Contact and Refund Email": "Contact & Refund Email",
+    "Contact_Body": "Please provide an email address below. We’ll contact you at this address if there is an issue with your payment.",
+    "Your email": "Your email",
+    "Continue": "Continue",
+    "Please enter a valid email address": "Please enter a valid email address",
+    "Order Amount": "Order Amount",
+    "Network Cost": "Network Cost",
+    "Already Paid": "Already Paid",
+    "Due": "Due",
+    // Tabs
     "Scan": "Scan",
     "Copy": "Kopieren",
     "Conversion": "Umwandlung",
+    // Scan tab
     "Open in wallet": "In der Brieftasche öffnen"
 };

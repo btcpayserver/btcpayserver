d626870e46860426f2ad6e5a96e5eebdb0e5d00d
Ricardo Andrade <ricardo@feudo.org>
2018-03-23 10:48:03 -0300

    Create pt.js

---
+++ BTCPayServer/wwwroot/js/checkout/langs/pt.js
@@ -0,0 +1,56 @@
+const locales_pt = {
+    nested: {
+        lang: 'Idioma',
+    },
+    "Awaiting Payment...": "Esperando o pagamento...",
+    "Pay with": "Pague com",
+    "Contact and Refund Email": "Email de contato e reembolso",
+    "Contact_Body": "Por favor, forneça um email abaixo. Nós iremos contactar você se algum problema ocorrer com o seu pagamento.",
+    "Your email": "Seu email",
+    "Continue": "Continue",
+    "Please enter a valid email address": "Por favor, entre com um email válido",
+    "Order Amount": "Valor do pedido",
+    "Network Cost": "Custo da rede",
+    "Already Paid": "Já foi pago",
+    "Due": "Devido",
+    // Tabs
+    "Scan": "Escaneie",
+    "Copy": "Copie",
+    "Conversion": "Conversão",
+    // Scan tab
+    "Open in wallet": "Abra na carteira",
+    // Copy tab
+    "CompletePay_Body": "Para completar seu pagamento, por favor envie {{btcDue}} {{cryptoCode}} para o endereço abaixo.",
+    "Amount": "Quantia",
+    "Address": "Endereço",
+    "Copied": "Copiado",
+    // Conversion tab
+    "ConversionTab_BodyTop": "Você pode pagar {{btcDue}} {{cryptoCode}} utilizando outras altcoins além das que a loja aceita diretamente.",
+    "ConversionTab_BodyDesc": "Esse serviço é oferecido por terceiros. Por favor, tenha em mente que não temos nenhum controle sobre como seus fundos serão utilizados. A fatura apenas será marcada como paga quando os fundos forem recebidos na Blockchain {{cryptoCode}}.",
+    "Shapeshift_Button_Text": "Pague com Altcoins",
+    "ConversionTab_Lightning": "Não há provedores de conversão disponíveis para pagamentos via Lightning Network de BTC.",
+    // Invoice expired
+    "Invoice expiring soon...": "A fatura está vencendo...",
+    "Invoice expired": "Fatura vencida",
+    "What happened?": "O que aconteceu?",
+    "InvoiceExpired_Body_1": "Essa fatura vence. Uma fatura é válida apenas por {{maxTimeMinutes}} minutos.  \
+Você pode retornar à {{storeName}} se desejar enviar seu pagamento novamente.",
+    "InvoiceExpired_Body_2": "Se você tentou enviar um pagamento, o mesmo não foi aceito pela rede Bitcoin. Nós não recebemos ainda o valor enviado.",
+    "InvoiceExpired_Body_3": "Se a transação não for aceita pela rede Bitcoin, o valor retornará à sua carteira. Dependendo da sua carteira, isso pode demorar de 48 a 72 horas.",
+    "Invoice ID": "Nº da Fatura",
+    "Order ID": "Nº do Pedido",
+    "Return to StoreName": "Retornar à {{storeName}}",
+    // Invoice paid
+    "This invoice has been paid": "Essa fatura foi paga",
+    // Invoice archived
+    "This invoice has been archived": "Essa fatura foi arquivada",
+    "Archived_Body": "Por favor, contate o estabelecimento para informações e suporte"
+};
+
+    © 2018 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+

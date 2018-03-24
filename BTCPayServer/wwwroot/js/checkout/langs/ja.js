2f8590ca7a6adeee103731dca2b7315ba640a391
junderw <junderwood@bitcoinbank.co.jp>
2018-03-24 22:06:03 +0900

    Fix Japanese

--- BTCPayServer/wwwroot/js/checkout/langs/ja.js
+++ BTCPayServer/wwwroot/js/checkout/langs/ja.js
@@ -2,47 +2,47 @@
     nested: {
         lang: 'Language'
     },
-    "Awaiting Payment...": "決済をお待ちください...",
-    "Pay with": "支払い方法",
-    "Contact and Refund Email": "コンタクトと返金メール",
-    "Contact_Body": "メールアドレスを下記にお送りください。何か決済に問題があれば、頂いたアドレスにコンタクト致します。",
-    "Your email": "メール",
+    "Awaiting Payment...": "お支払いをお待ちしております…",
+    "Pay with": "お支払い方法",
+    "Contact and Refund Email": "問題発生時の連絡先",
+    "Contact_Body": "決済においては、何か問題が発生したらこちらのメールアドレスに対してご連絡差し上げることもございますので、ご記入ください",
+    "Your email": "ご自分のメールアドレス",
     "Continue": "続ける",
-    "Please enter a valid email address": "正しいメールアドレスを記入してください",
-    "Order Amount": "注文総額",
-    "Network Cost": "ネットワークコスト",
-    "Already Paid": "支払われた額",
-    "Due": "合計",
+    "Please enter a valid email address": "正常なメールアドレスをご記入ください",
+    "Order Amount": "注文金額",
+    "Network Cost": "ネットワーク手数料",
+    "Already Paid": "支払い済み金額",
+    "Due": "未払い金額",
     // Tabs
     "Scan": "スキャン",
     "Copy": "コピー",
     "Conversion": "変換",
     // Scan tab
-    "Open in wallet": "ウォレットを開く",
+    "Open in wallet": "ウォレットで開く",
     // Copy tab
-    "CompletePay_Body": "決済をするために、こちらに{{btcDue}} {{cryptoCode}}お送りください",
-    "Amount": "額",
+    "CompletePay_Body": "決済をするために、下記のアドレスに {{btcDue}} {{cryptoCode}} をお送りください",
+    "Amount": "金額",
     "Address": "アドレス",
-    "Copied": "コピー",
+    "Copied": "コピーしました",
     // Conversion tab
-    "ConversionTab_BodyTop": "You can pay {{btcDue}} {{cryptoCode}} using altcoins other than the ones merchant directly supports.",
-    "ConversionTab_BodyDesc": "This service is provided by 3rd party. Please keep in mind that we have no control over how providers will forward your funds. Invoice will only be marked paid once funds are received on {{cryptoCode}} Blockchain.",
-    "Shapeshift_Button_Text": "アルトコインで支払う",
-    "ConversionTab_Lightning": "No conversion providers available for BTC Lightning Network payments.",
+    "ConversionTab_BodyTop": "代わりに、お店が受け付けていなくても {{btcDue}} {{cryptoCode}} での支払いもできます。",
+    "ConversionTab_BodyDesc": "ただし、この変換は第三者サービスによるものですので、お店が受け付けている通貨で着金するまでの間の処理に関しては何の保証もいたしません。変換後に受付中の通貨 ({{cryptoCode}}) がお店に着金してから支払い済みとなりますのでご了承ください。",
+    "Shapeshift_Button_Text": "他の仮想通貨で支払う",
+    "ConversionTab_Lightning": "BTCライトニングのペイメントでは現在変換サービスが存在しないためご利用いただけません。ご了承ください。",
     // Invoice expired
-    "Invoice expiring soon...": "請求書の期限が迫っています...",
-    "Invoice expired": "請求書の期限が切れました",
-    "What happened?": "何が起こったか？",
-    "InvoiceExpired_Body_1": "This invoice has expired. An invoice is only valid for {{maxTimeMinutes}} minutes.  \
-You can return to {{storeName}} if you would like to submit your payment again.",
-    "InvoiceExpired_Body_2": "If you tried to send a payment, it has not yet been accepted by the Bitcoin network. We have not yet received your funds.",
-    "InvoiceExpired_Body_3": "If the transaction is not accepted by the Bitcoin network, the funds will be spendable again in your wallet. Depending on your wallet, this may take 48-72 hours.",
-    "Invoice ID": "請求書ID",
-    "Order ID": "オーダーID",
-    "Return to StoreName": "{{storeName}}に戻る",
+    "Invoice expiring soon...": "お支払いの期限が迫っています...",
+    "Invoice expired": "お支払いの期限が切れました",
+    "What happened?": "え！？ナニコレ！？",
+    "InvoiceExpired_Body_1": "当件のお支払いの有効期限が過ぎてしまいました。最大 {{maxTimeMinutes}} 分以内に支払うことが義務付けられています。  \
+まだお支払いのご希望の場合 {{storeName}} に一旦戻っていただき、もう一度お支払いの手続きを最初からやり直してみてください。",
+    "InvoiceExpired_Body_2": "送金手続きを完了したつもりでも、ネットワークにて取り込まれて処理されるまでは決済となりません。現時点ではまだ着金しておりません。",
+    "InvoiceExpired_Body_3": "ビットコインネットワークにて取り込まれなかった送金はいずれ送金元のウォレットに戻りますが、ウォレットソフトによっては２〜３日かかる場合もございますのでご了承ください。",
+    "Invoice ID": "お支払い ID",
+    "Order ID": "ご注文 ID",
+    "Return to StoreName": "{{storeName}} に戻る",
     // Invoice paid
-    "This invoice has been paid":　"この請求は支払いが完了しました",
+    "This invoice has been paid":　"お支払いが完了しました",
     // Invoice archived
-    "This invoice has been archived": "This invoice has been archived",
-    "Archived_Body": "お店のオーダーインフォメーション、又はアシスタントにコンタクトしてください。"
+    "This invoice has been archived": "お支払いをアーカイブしました",
+    "Archived_Body": "ご注文に関わる詳細などでお困りの場合はお店の担当窓口へお問い合わせください。"
 };

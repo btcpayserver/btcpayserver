cd2e3350b0fcad8dececcb7c0c7002b24f7bfeef
nicolas.dorier <nicolas.dorier@gmail.com>
2018-03-24 20:15:42 +0900

    Japanese support WIP

--- BTCPayServer/wwwroot/js/checkout/langs/ja.js
+++ BTCPayServer/wwwroot/js/checkout/langs/ja.js
@@ -1,2 +1,48 @@
-const locales_ja = {
+﻿const locales_ja = {
+    nested: {
+        lang: 'Language'
+    },
+    "Awaiting Payment...": "決済をお待ちください...",
+    "Pay with": "支払い方法",
+    "Contact and Refund Email": "コンタクトと返金メール",
+    "Contact_Body": "メールアドレスを下記にお送りください。何か決済に問題があれば、頂いたアドレスにコンタクト致します。",
+    "Your email": "メール",
+    "Continue": "続ける",
+    "Please enter a valid email address": "正しいメールアドレスを記入してください",
+    "Order Amount": "注文総額",
+    "Network Cost": "ネットワークコスト",
+    "Already Paid": "支払われた額",
+    "Due": "合計",
+    // Tabs
+    "Scan": "スキャン",
+    "Copy": "コピー",
+    "Conversion": "変換",
+    // Scan tab
+    "Open in wallet": "ウォレットを開く",
+    // Copy tab
+    "CompletePay_Body": "決済をするために、こちらに{{btcDue}} {{cryptoCode}}お送りください",
+    "Amount": "額",
+    "Address": "アドレス",
+    "Copied": "コピー",
+    // Conversion tab
+    "ConversionTab_BodyTop": "You can pay {{btcDue}} {{cryptoCode}} using altcoins other than the ones merchant directly supports.",
+    "ConversionTab_BodyDesc": "This service is provided by 3rd party. Please keep in mind that we have no control over how providers will forward your funds. Invoice will only be marked paid once funds are received on {{cryptoCode}} Blockchain.",
+    "Shapeshift_Button_Text": "アルトコインで支払う",
+    "ConversionTab_Lightning": "No conversion providers available for BTC Lightning Network payments.",
+    // Invoice expired
+    "Invoice expiring soon...": "請求書の期限が迫っています...",
+    "Invoice expired": "請求書の期限が切れました",
+    "What happened?": "何が起こったか？",
+    "InvoiceExpired_Body_1": "This invoice has expired. An invoice is only valid for {{maxTimeMinutes}} minutes.  \
+You can return to {{storeName}} if you would like to submit your payment again.",
+    "InvoiceExpired_Body_2": "If you tried to send a payment, it has not yet been accepted by the Bitcoin network. We have not yet received your funds.",
+    "InvoiceExpired_Body_3": "If the transaction is not accepted by the Bitcoin network, the funds will be spendable again in your wallet. Depending on your wallet, this may take 48-72 hours.",
+    "Invoice ID": "請求書ID",
+    "Order ID": "オーダーID",
+    "Return to StoreName": "{{storeName}}に戻る",
+    // Invoice paid
+    "This invoice has been paid":　"この請求は支払いが完了しました",
+    // Invoice archived
+    "This invoice has been archived": "This invoice has been archived",
+    "Archived_Body": "お店のオーダーインフォメーション、又はアシスタントにコンタクトしてください。"
 };

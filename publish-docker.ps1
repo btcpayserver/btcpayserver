a0a2ab6fcdde03792124cc6a5430ca79cf9bda2f
nicolas.dorier <nicolas.dorier@gmail.com>
2018-03-26 11:54:10 +0900

    update publish-docker

--- publish-docker.ps1
+++ publish-docker.ps1
@@ -1,3 +1,6 @@
 $ver = [regex]::Match((Get-Content BTCPayServer\BTCPayServer.csproj), '<Version>([^<]+)<').Groups[1].Value
 git tag -a "v$ver" -m "$ver"
 git push --tags
+git tag -d "stable"
+git tag -a "stable" -m "stable"
+git push --tags --force

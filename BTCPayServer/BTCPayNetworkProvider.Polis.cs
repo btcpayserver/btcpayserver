bc8978182e73979c8becc7bf5a8c800b66b9bd37
Cronos <cronos@polispay.org>
2018-04-17 11:13:50 -0500

    Add Polis

---
+++ BTCPayServer/BTCPayNetworkProvider.Polis.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BTCPayServer.Services.Rates;
+using NBitcoin;
+using NBXplorer;
+
+namespace BTCPayServer
+{
+    public partial class BTCPayNetworkProvider
+    {
+        public void InitPolis()
+        {
+            NBitcoin.Altcoins.Polis.EnsureRegistered();
+
+            var nbxplorerNetwork = NBXplorerNetworkProvider.GetFromCryptoCode("POLIS");
+            Add(new BTCPayNetwork()
+            {
+                CryptoCode = nbxplorerNetwork.CryptoCode,
+                BlockExplorerLink = NBXplorerNetworkProvider.ChainType == ChainType.Main ? "https://insight.polispay.org/tx/{0}" : "https://insight.polispay.org/tx/f{0}",
+                NBitcoinNetwork = nbxplorerNetwork.NBitcoinNetwork,
+                NBXplorerNetwork = nbxplorerNetwork,
+                UriScheme = "polis",
+                DefaultRateProvider = new CoinAverageRateProviderDescription("POLIS"),
+                CryptoImagePath = "imlegacy/polis.png",
+                DefaultSettings = BTCPayDefaultSettings.GetDefaultSettings(NBXplorerNetworkProvider.ChainType),
+                CoinType = NBXplorerNetworkProvider.ChainType == ChainType.Main ? new KeyPath("2'") : new KeyPath("1'")
+            });
+        }
+    }
+}

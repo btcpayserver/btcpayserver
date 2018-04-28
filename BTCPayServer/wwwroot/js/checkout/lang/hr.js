94ff77f2b294af0ff6c7c3dc6f2db241970bc0b8
2pac1 <35058480+2pac1@users.noreply.github.com>
2018-04-28 18:44:55 +0200

    Update hr.js

--- BTCPayServer/wwwroot/js/checkout/lang/hr.js
+++ BTCPayServer/wwwroot/js/checkout/lang/hr.js
@@ -5,13 +5,13 @@ const locales_hr = {
     "Awaiting Payment...": "Čeka se uplata...",
     "Pay with": "Plati sa",
     "Contact and Refund Email": "E-mail za kontakt i povrat sredstava",
-    "Contact_Body": "Molimo upišite Vašu e-mail adresu. Kontaktirati ćemo Vas ako nastane problem s uplatom",
+    "Contact_Body": "Molimo upišite Vašu e-mail adresu. Kontaktirat ćemo Vas ukoliko bude potrebe.",
     "Your email": "Vaš e-mail",
     "Continue": "Dalje",
     "Please enter a valid email address": "Molimo unesite ispravnu e-mail adresu",
     "Order Amount": "Količina",
     "Network Cost": "Mrežni trošak",
-    "Already Paid": "Već plačeno",
+    "Already Paid": "Već plaćeno",
     "Due": "Rok",
     // Tabs
     "Scan": "Skeniraj",
@@ -26,23 +26,23 @@ const locales_hr = {
     "Copied": "Kopirano",
     // Conversion tab
     "ConversionTab_BodyTop": "Možete platiti {{btcDue}} {{cryptoCode}} pomoću altcoina koje prodavač ne podržava.",
-    "ConversionTab_BodyDesc": "Ovu usluga pruža treća strana. Vodite računa da nemamo kontroli nad načinom kako će Vam davatelji usluge prosljediti sredstva. Vodite računa da je račun plaćen tek kada su primljena sredstva na {{cryptoCode}} Blockchainu.",
-    "Shapeshift_Button_Text": "Plati s Altcoinovima",
+    "ConversionTab_BodyDesc": "Ovu usluga pruža treća strana. Vodite računa da nemamo kontroli nad načinom kako će Vam davatelji usluge proslijediti sredstva. Vodite računa da je račun plaćen tek kada su primljena sredstva na {{cryptoCode}} Blockchainu.",
+    "Shapeshift_Button_Text": "Plati s Alt-coinovima",
     "ConversionTab_Lightning": "Ne postoji treća strana koja bi konvertirala Lightning Network uplate.",
     // Invoice expired
-    "Invoice expiring soon...": "Račun uskoro istiće...",
+    "Invoice expiring soon...": "Račun uskoro ističe...",
     "Invoice expired": "Račun je istekao",
     "What happened?": "Što se dogodilo",
     "InvoiceExpired_Body_1": "Račun je istekao i nije više valjan. Račun vrijedi samo {{maxTimeMinutes}} minuta.  \
-Možete se vratiti na {{storeName}}, gdje možete ponovo inicirati plačanje.",
-    "InvoiceExpired_Body_2": "Ukoliko ste pokušali poslati uplatu, ista nije registrirana na Blockchainu. Nismo još zaprimili Vašu uplatu.",
-    "InvoiceExpired_Body_3": "Ukoliko poslana sredstva na budu potvrđena na Blockchainu, sredstva će biti ponovo dostupna u Vašem novčaniku.",
+Možete se vratiti na {{storeName}}, gdje možete ponovo inicirati plaćanje.",
+    "InvoiceExpired_Body_2": "Ako ste pokušali poslati uplatu, ista nije registrirana na Blockchainu. Nismo još zaprimili Vašu uplatu.",
+    "InvoiceExpired_Body_3": "Ako poslana sredstva na budu potvrđena na Blockchainu, sredstva će biti ponovo dostupna u Vašem novčaniku.",
     "Invoice ID": "Broj računa",
     "Order ID": "Broj narudžbe",
     "Return to StoreName": "Vrati se na {{storeName}}",
     // Invoice paid
-    "This invoice has been paid": "Račun je plačen",
+    "This invoice has been paid": "Račun je plaćen",
     // Invoice archived
     "This invoice has been archived": "Račun je arhiviran.",
-    "Archived_Body": "Kontaktirajte dučan za detalje oko naruđbe ili pomoć."
+    "Archived_Body": "Kontaktirajte dućan za detalje oko narudžbe ili pomoć."
 };

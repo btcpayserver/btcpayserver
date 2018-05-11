7fdf19ca220bd59c2910275fabe244d27b638ca5
cronos <cronos@polispay.org>
2018-05-11 11:07:42 -0500

    Remove cryptopia from CoinAverage

--- BTCPayServer/Services/Rates/CoinAverageSettings.cs
+++ BTCPayServer/Services/Rates/CoinAverageSettings.cs
@@ -120,7 +120,6 @@ namespace BTCPayServer.Services.Rates
                 (DisplayName: "Quoine", Name: "quoine"),
                 (DisplayName: "BTC Markets", Name: "btcmarkets"),
                 (DisplayName: "Bitso", Name: "bitso"),
-                (DisplayName: "Cryptopia", Name: "cryptopia"),
                 })
             {
                 AvailableExchanges.TryAdd(item.Name, new CoinAverageExchange(item.Name, item.DisplayName));

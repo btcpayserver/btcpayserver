3965281993ca4bbcf8560a89cc495f8fa9a011e5
kemoantemo <kemoantemo20@gmail.com>
2018-09-26 19:55:26 +0200

    Create ar.js

---
+++ BTCPayServer/wwwroot/checkout/js/langs/ar.js
@@ -0,0 +1,54 @@
+const locales_ar = {
+    nested: {
+        lang: 'اللغة'
+    },
+    "Awaiting Payment...": "في انتظار الدفع...",
+    "Pay with": "ادفع عن طريق",
+    "Contact and Refund Email": "البريد الإلكتروني الخاص بالتواصل & إعادة المال",
+    "Contact_Body": "من فضلك ادخل بريدك الإلكتروني ادناه. سوف نتواصل معك علي هذا البريد في حال حدوث مشاكل مع معاملتك.",
+    "Your email": "بريدك الإلكتروني",
+    "Continue": "استمرار",
+    "Please enter a valid email address": "ادخل بريد الكتروني صحيح من فضلك",
+    "Order Amount": "المبلغ المطلوب",
+    "Network Cost": "مصاريف الشبكة",
+    "Already Paid": "تم دفعه",
+    "Due": "المستحق",
+    // Tabs
+    "Scan": "مسح",
+    "Copy": "نسخ",
+    "Conversion": "تبديل",
+    // Scan tab
+    "Open in wallet": "الفتح في المحفظة",
+    // Copy tab
+    "CompletePay_Body": "لإكمال التحويل, من فضلك ارسل {{btcDue}} {{cryptoCode}} للعنوان ادناه.",
+    "Amount": "المبلع",
+    "Address": "العنوان",
+    "Copied": "تم النسخ",
+    // Conversion tab
+    "ConversionTab_BodyTop": "تستطيع دفع {{btcDue}} {{cryptoCode}} بإستخدام عملات اخري غير التي يقبلها التاجر.",
+    "ConversionTab_BodyDesc": "هذه الخدمة موفرة عن طريق طرف ثالث. يجب ان تضع في الإعتبار اننا غير مسؤلين عن كيف سيحول موفر الخدمة الأموال. الأموال ستكون ظاهر فقط في حين استلامها علي شبكة {{cryptoCode}}.",
+    "Shapeshift_Button_Text": "الدفع عن طريق عملات اخري",
+    "ConversionTab_Lightning": "لا يوجد خدمة دفع بديلة لتحويلات Lightning.",
+    // Invoice expired
+    "Invoice expiring soon...": "المعاملة تنتهي صلاحيتها قريبا...",
+    "Invoice expired": "المعاملة انتهت صلاحيتها",
+    "What happened?": "ماذا حدث?",
+    "InvoiceExpired_Body_1": "هذه المعاملة انتهت صلاحيتها المعاملة صالحة فقط ل {{maxTimeMinutes}} دقائق.  \
+تستطيع الرجوع الي {{storeName}} اذا كنت تريد عمل معاملة جديدة.",
+    "InvoiceExpired_Body_2": "اذا كنت قد حاولت ارسال عملات, فهي لم تقبل بعد من خلال الشبكة. نحن لم نستلم بعد عملاتك.",
+    "InvoiceExpired_Body_3": "اذا كانت التحويلة غير مقبولة من الشبكة, فعملاتك ستكون قابلة للدفع مجددا في محفظتك. بالإعتماد علي نوع محفظتك, هذا قد يستغرق 48-72 ساعة.",
+    "Invoice ID": "المعاملة ID",
+    "Order ID": "الطلب ID",
+    "Return to StoreName": "الرجوع الي {{storeName}}",
+    // Invoice paid
+    "This invoice has been paid": "هذه المعاملة تم دفعها بنجاح",
+    // Invoice archived
+    "This invoice has been archived": "تمت ارشفة هذه المعاملة",
+    "Archived_Body": "من فضلك تحدث مع البائع للمعلومات بخصوص المعاملة او المساعدة",
+    // Lightning
+    "BOLT 11 Invoice": "BOLT 11 معاملة",
+    "Node Info": "Node معلومات",
+    //
+    "txCount": "{{count}} تحويلة",
+    "txCount_plural": "{{count}} تحويلات"
+};

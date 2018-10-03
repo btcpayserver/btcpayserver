cbaf2259cb39839b38e92727316871c0fe045bb7
britttttk <39231115+britttttk@users.noreply.github.com>
2018-10-03 07:57:54 -0600

    Nepali Language

---
+++ BTCPayServer/wwwroot/checkout/js/langs/LanguageService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BTCPayServer.Services
+{
+    public class Language
+    {
+        public Language(string code, string displayName)
+        {
+            DisplayName = displayName;
+            Code = code;
+        }
+        public string Code { get; set; }
+        public string DisplayName { get; set; }
+    }
+    public class LanguageService
+    {
+        public Language[] GetLanguages()
+        {
+            return new[]
+            {
+                new Language("en-US", "English"),
+                new Language("de-DE", "Deutsch"),
+                new Language("ja-JP", "日本語"),
+                new Language("fr-FR", "Français"),
+                new Language("es-ES", "Spanish"),
+                new Language("pt-PT", "Portuguese"),
+                new Language("pt-BR", "Portuguese (Brazil)"),
+                new Language("nl-NL", "Dutch"),
+                new Language("np-NP", "नेपाली"),
+                new Language("cs-CZ", "Česky"),
+                new Language("is-IS", "Íslenska"),
+                new Language("hr-HR", "Croatian"),
+            };
+        }
+    }
+}

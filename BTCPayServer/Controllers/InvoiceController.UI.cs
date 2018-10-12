c2bbc04c4c6ceab62622da8f1a4038c822e1e324
Rockstar Developer <5191402+rockstardev@users.noreply.github.com>
2018-10-11 20:09:13 -0500

    Various bugfixes (#308)
    * NotifyEmail field on Invoice, sending email when triggered
    
    * Styling invoices page
    
    * Exporting Invoices in JSON
    
    * Recoding based on feedback
    
    * Fixing image breaking responsive layout on mobile
    
    * Reducing amount of data sent in email notification
    
    * Turning bundling on by default

--- BTCPayServer/Controllers/InvoiceController.UI.cs
+++ BTCPayServer/Controllers/InvoiceController.UI.cs
@@ -58,6 +58,7 @@ namespace BTCPayServer.Controllers
                 OrderId = invoice.OrderId,
                 BuyerInformation = invoice.BuyerInformation,
                 Fiat = _CurrencyNameTable.DisplayFormatCurrency((decimal)dto.Price, dto.Currency),
+                NotificationEmail = invoice.NotificationEmail,
                 NotificationUrl = invoice.NotificationURL,
                 RedirectUrl = invoice.RedirectURL,
                 ProductInformation = invoice.ProductInformation,
@@ -228,7 +229,7 @@ namespace BTCPayServer.Controllers
                 if (!isDefaultCrypto)
                     return null;
                 var paymentMethodTemp = invoice.GetPaymentMethods(_NetworkProvider)
-                                               .Where(c=> paymentMethodId.CryptoCode == c.GetId().CryptoCode)
+                                               .Where(c => paymentMethodId.CryptoCode == c.GetId().CryptoCode)
                                                .FirstOrDefault();
                 if (paymentMethodTemp == null)
                     paymentMethodTemp = invoice.GetPaymentMethods(_NetworkProvider).First();
@@ -405,23 +406,17 @@ namespace BTCPayServer.Controllers
         [BitpayAPIConstraint(false)]
         public async Task<IActionResult> ListInvoices(string searchTerm = null, int skip = 0, int count = 50)
         {
-            var model = new InvoicesModel();
-            var filterString = new SearchString(searchTerm);
-            foreach (var invoice in await _InvoiceRepository.GetInvoices(new InvoiceQuery()
+            var model = new InvoicesModel
             {
-                TextSearch = filterString.TextSearch,
-                Count = count,
+                SearchTerm = searchTerm,
                 Skip = skip,
-                UserId = GetUserId(),
-                Unusual = !filterString.Filters.ContainsKey("unusual") ? null
-                          : !bool.TryParse(filterString.Filters["unusual"].First(), out var r) ? (bool?)null
-                          : r,
-                Status = filterString.Filters.ContainsKey("status") ? filterString.Filters["status"].ToArray() : null,
-                ExceptionStatus = filterString.Filters.ContainsKey("exceptionstatus") ? filterString.Filters["exceptionstatus"].ToArray() : null,
-                StoreId = filterString.Filters.ContainsKey("storeid") ? filterString.Filters["storeid"].ToArray() : null
-            }))
+                Count = count,
+                StatusMessage = StatusMessage
+            };
+
+            var list = await ListInvoicesProcess(searchTerm, skip, count);
+            foreach (var invoice in list)
             {
-                model.SearchTerm = searchTerm;
                 model.Invoices.Add(new InvoiceModel()
                 {
                     Status = invoice.Status + (invoice.ExceptionStatus == null ? string.Empty : $" ({invoice.ExceptionStatus})"),
@@ -433,10 +428,27 @@ namespace BTCPayServer.Controllers
                     AmountCurrency = $"{invoice.ProductInformation.Price.ToString(CultureInfo.InvariantCulture)} {invoice.ProductInformation.Currency}"
                 });
             }
-            model.Skip = skip;
-            model.Count = count;
-            model.StatusMessage = StatusMessage;
             return View(model);
+        }
+
+        private async Task<InvoiceEntity[]> ListInvoicesProcess(string searchTerm = null, int skip = 0, int count = 50)
+        {
+            var filterString = new SearchString(searchTerm);
+            var list = await _InvoiceRepository.GetInvoices(new InvoiceQuery()
+            {
+                TextSearch = filterString.TextSearch,
+                Count = count,
+                Skip = skip,
+                UserId = GetUserId(),
+                Unusual = !filterString.Filters.ContainsKey("unusual") ? null
+                          : !bool.TryParse(filterString.Filters["unusual"].First(), out var r) ? (bool?)null
+                          : r,
+                Status = filterString.Filters.ContainsKey("status") ? filterString.Filters["status"].ToArray() : null,
+                ExceptionStatus = filterString.Filters.ContainsKey("exceptionstatus") ? filterString.Filters["exceptionstatus"].ToArray() : null,
+                StoreId = filterString.Filters.ContainsKey("storeid") ? filterString.Filters["storeid"].ToArray() : null
+            });
+
+            return list;
         }
 
         [HttpGet]
@@ -501,6 +513,7 @@ namespace BTCPayServer.Controllers
                     PosData = model.PosData,
                     OrderId = model.OrderId,
                     //RedirectURL = redirect + "redirect",
+                    NotificationEmail = model.NotificationEmail,
                     NotificationURL = model.NotificationUrl,
                     ItemDesc = model.ItemDesc,
                     FullNotifications = true,
@@ -515,6 +528,21 @@ namespace BTCPayServer.Controllers
                 ModelState.TryAddModelError(nameof(model.Currency), $"Error: {ex.Message}");
                 return View(model);
             }
+        }
+
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = Policies.CookieAuthentication)]
+        [BitpayAPIConstraint(false)]
+        public async Task<IActionResult> Export(string format, string searchTerm = null)
+        {
+            var model = new ExportInvoicesModel
+            {
+                Format = format,
+                Invoices = await ListInvoicesProcess(searchTerm, 0, int.MaxValue)
+            };
+
+            return Content(model.Process(), "application/" + format);
         }
 
         [HttpPost]
---
+++ BTCPayServer/Models/InvoicingModels/ExportInvoicesModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTCPayServer.Services.Invoices;
+using Newtonsoft.Json;
+
+namespace BTCPayServer.Models.InvoicingModels
+{
+    public class ExportInvoicesModel
+    {
+        public InvoiceEntity[] Invoices { get; set; }
+        public string Format { get; set; }
+
+        public string Process()
+        {
+            if (String.Equals(Format, "json", StringComparison.OrdinalIgnoreCase))
+                return processJson();
+            else
+                throw new Exception("Export format not supported");
+        }
+
+        private string processJson()
+        {
+            foreach (var i in Invoices)
+            {
+                // removing error causing complex circular dependencies
+                i.Payments?.ForEach(a =>
+                {
+                    a.Output = null;
+                    a.Outpoint = null;
+                });
+            }
+
+            var serializerSett = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+            var json = JsonConvert.SerializeObject(new { Invoices }, Formatting.Indented, serializerSett);
+
+            return json;
+        }
+    }
+}

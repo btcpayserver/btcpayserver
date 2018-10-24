1ff0a98d30005a02103b1c6033d07c006b110523
britttttk <39231115+britttttk@users.noreply.github.com>
2018-10-24 00:18:31 -0600

    Adding Ukrainian Translation (#352)

--- BTCPayServer/Services/LanguageService.cs
+++ BTCPayServer/Services/LanguageService.cs
@@ -36,6 +36,7 @@ namespace BTCPayServer.Services
                 new Language("it-IT", "Italiano"),
                 new Language("kk-KZ", "Қазақша"),
                 new Language("ru-RU", "русский"),
+                new Language("uk-UA", "Українська"),
                 new Language("vi-VN", "Tiếng Việt"),
                 new Language("zh-SP", "中文（简体）"),
             };

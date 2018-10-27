2e0f20671adca509e9d1eb968380c9d8453eb341
Kukks <evilkukka@gmail.com>
2018-10-27 18:07:32 +0200

    start working on loading locales async and as json

--- BTCPayServer/Services/LanguageService.cs
+++ BTCPayServer/Services/LanguageService.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace BTCPayServer.Services
 {
@@ -12,34 +12,59 @@ namespace BTCPayServer.Services
             DisplayName = displayName;
             Code = code;
         }
+
         public string Code { get; set; }
         public string DisplayName { get; set; }
     }
+
     public class LanguageService
     {
+        private readonly IEnumerable<Language> _languages;
+
+        public LanguageService()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "locales");
+            var files = Directory.GetFiles(path, "*.json");
+            var result = new List<Language>();
+            foreach (var file in files)
+            {
+                using (var stream = new StreamReader(file))
+                {
+                    var json = stream.ReadToEnd();
+
+                    var name = JObject.Parse(json).GetValue("currentLanguage").ToString();
+                    var code  =  Path.GetFileNameWithoutExtension(file);
+                    result.Add(new Language(code, name));
+                }
+            }
+
+            _languages = result;
+        }
+
         public Language[] GetLanguages()
         {
-            return new[]
-            {
-                new Language("en-US", "English"),
-                new Language("de-DE", "Deutsch"),
-                new Language("ja-JP", "日本語"),
-                new Language("fr-FR", "Français"),
-                new Language("es-ES", "Spanish"),
-                new Language("pt-PT", "Portuguese"),
-                new Language("pt-BR", "Portuguese (Brazil)"),
-                new Language("nl-NL", "Dutch"),
-                new Language("np-NP", "नेपाली"),
-                new Language("cs-CZ", "Česky"),
-                new Language("is-IS", "Íslenska"),
-                new Language("hr-HR", "Croatian"),
-                new Language("it-IT", "Italiano"),
-                new Language("kk-KZ", "Қазақша"),
-                new Language("ru-RU", "русский"),
-                new Language("uk-UA", "Українська"),
-                new Language("vi-VN", "Tiếng Việt"),
-                new Language("zh-SP", "中文（简体）"),
-            };
+            return _languages.ToArray();
+//            return new[]
+//            {
+//                new Language("en-US", "English"),
+//                new Language("de-DE", "Deutsch"),
+//                new Language("ja-JP", "日本語"),
+//                new Language("fr-FR", "Français"),
+//                new Language("es-ES", "Spanish"),
+//                new Language("pt-PT", "Portuguese"),
+//                new Language("pt-BR", "Portuguese (Brazil)"),
+//                new Language("nl-NL", "Dutch"),
+//                new Language("np-NP", "नेपाली"),
+//                new Language("cs-CZ", "Česky"),
+//                new Language("is-IS", "Íslenska"),
+//                new Language("hr-HR", "Croatian"),
+//                new Language("it-IT", "Italiano"),
+//                new Language("kk-KZ", "Қазақша"),
+//                new Language("ru-RU", "русский"),
+//                new Language("uk-UA", "Українська"),
+//                new Language("vi-VN", "Tiếng Việt"),
+//                new Language("zh-SP", "中文（简体）"),
+//            };
         }
     }
 }

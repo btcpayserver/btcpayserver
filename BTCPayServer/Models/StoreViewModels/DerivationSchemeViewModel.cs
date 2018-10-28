a5da7a1919a445726b4c9c77dce495c5afcf192a
Kukks <evilkukka@gmail.com>
2018-10-28 18:55:25 +0100

    Revert "add in ui"
    This reverts commit 9614721fa8a439f7097adca69772b5d41f6585d6.

--- BTCPayServer/Models/StoreViewModels/DerivationSchemeViewModel.cs
+++ BTCPayServer/Models/StoreViewModels/DerivationSchemeViewModel.cs
@@ -1,79 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using NBitcoin;
 
 namespace BTCPayServer.Models.StoreViewModels
 {
     public class DerivationSchemeViewModel
     {
-        private string _derivationScheme;
-
         public DerivationSchemeViewModel()
         {
         }
-
         public string DerivationScheme
         {
-            get => _derivationScheme;
-            set
-            {
-                _derivationScheme = value;
-                var split = _derivationScheme.Split("-");
-                ExtPubKey = split[0];
-                if (split.Length <= 1) return;
-                var suffix = "";
-                if (split.Length > 1)
-                {
-                    suffix = "-" + split[1];
-                }
-
-                var type = AddressTypes.SingleOrDefault(addressType => addressType.Suffix == suffix);
-                AddressType = type?.Type;
-            }
+            get; set;
         }
 
-
-        public List<(string KeyPath, string Address)> AddressSamples { get; set; } =
-            new List<(string KeyPath, string Address)>();
+        public List<(string KeyPath, string Address)> AddressSamples
+        {
+            get; set;
+        } = new List<(string KeyPath, string Address)>();
 
         public string CryptoCode { get; set; }
-        [Display(Name = "Hint address")] public string HintAddress { get; set; }
+        [Display(Name = "Hint address")]
+        public string HintAddress { get; set; }
         public bool Confirmation { get; set; }
         public bool Enabled { get; set; } = true;
 
         public string ServerUrl { get; set; }
         public string StatusMessage { get; internal set; }
         public KeyPath RootKeyPath { get; set; }
-
-        [Display(Name = "xpub / ypub key")]
-        public string ExtPubKey { get; set; }
-        public string AddressType { get; set; }
-
-
-        public IEnumerable<AddressType> AddressTypes = new List<AddressType>()
-        {
-            new AddressType()
-            {
-                Suffix = "",
-                Type = "P2WPKH / Multi-sig P2WSH"
-            },
-            new AddressType()
-            {
-                Suffix = "-[p2sh]",
-                Type = "P2SH-P2WPKH / Multi-sig P2SH-P2WSH"
-            },
-            new AddressType()
-            {
-                Suffix = "-[legacy]",
-                Type = "P2PKH / Multi-sig P2SH"
-            }
-        };
-    }
-
-    public class AddressType
-    {
-        public string Type { get; set; }
-        public string Suffix { get; set; }
     }
 }

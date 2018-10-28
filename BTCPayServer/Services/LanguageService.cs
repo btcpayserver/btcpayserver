aced990d5e7af9b25b376a0a2ae18dd0e4845192
Kukks <evilkukka@gmail.com>
2018-10-28 16:02:45 +0100

    finish off langs and UI

--- BTCPayServer/Services/LanguageService.cs
+++ BTCPayServer/Services/LanguageService.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace BTCPayServer.Services
 {
@@ -13,13 +15,15 @@ namespace BTCPayServer.Services
             Code = code;
         }
 
+        [JsonProperty("code")]
         public string Code { get; set; }
+        [JsonProperty("currentLanguage")]
         public string DisplayName { get; set; }
     }
 
     public class LanguageService
     {
-        private readonly IEnumerable<Language> _languages;
+        private readonly Language[] _languages;
 
         public LanguageService()
         {
@@ -31,40 +35,16 @@ namespace BTCPayServer.Services
                 using (var stream = new StreamReader(file))
                 {
                     var json = stream.ReadToEnd();
-
-                    var name = JObject.Parse(json).GetValue("currentLanguage").ToString();
-                    var code  =  Path.GetFileNameWithoutExtension(file);
-                    result.Add(new Language(code, name));
+                    result.Add(JObject.Parse(json).ToObject<Language>());
                 }
             }
 
-            _languages = result;
+            _languages = result.ToArray();
         }
 
         public Language[] GetLanguages()
         {
-            return _languages.ToArray();
-//            return new[]
-//            {
-//                new Language("en-US", "English"),
-//                new Language("de-DE", "Deutsch"),
-//                new Language("ja-JP", "日本語"),
-//                new Language("fr-FR", "Français"),
-//                new Language("es-ES", "Spanish"),
-//                new Language("pt-PT", "Portuguese"),
-//                new Language("pt-BR", "Portuguese (Brazil)"),
-//                new Language("nl-NL", "Dutch"),
-//                new Language("np-NP", "नेपाली"),
-//                new Language("cs-CZ", "Česky"),
-//                new Language("is-IS", "Íslenska"),
-//                new Language("hr-HR", "Croatian"),
-//                new Language("it-IT", "Italiano"),
-//                new Language("kk-KZ", "Қазақша"),
-//                new Language("ru-RU", "русский"),
-//                new Language("uk-UA", "Українська"),
-//                new Language("vi-VN", "Tiếng Việt"),
-//                new Language("zh-SP", "中文（简体）"),
-//            };
+            return _languages;
         }
     }
 }

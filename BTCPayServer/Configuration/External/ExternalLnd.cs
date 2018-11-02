c6b12233fea97dd77d4056b36fb1ab913606ade5
Esky33 <support@btcpayjungle.com>
2018-11-02 01:37:00 +0000

    update

---
+++ BTCPayServer/Configuration/External/ExternalLnd.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BTCPayServer.Lightning;
+
+namespace BTCPayServer.Configuration.External
+{
+    public abstract class ExternalLnd : ExternalService
+    {
+        public ExternalLnd(LightningConnectionString connectionString, LndTypes type)
+        {
+            ConnectionString = connectionString;
+            Type = type;
+        }
+
+        public LndTypes Type { get; set; }
+        public LightningConnectionString ConnectionString { get; set; }
+    }
+
+    public enum LndTypes
+    {
+        gRPC, Rest
+    }
+
+    public class ExternalLndGrpc : ExternalLnd
+    {
+        public ExternalLndGrpc(LightningConnectionString connectionString) : base(connectionString, LndTypes.gRPC) { }
+    }
+
+    public class ExternalLndRest : ExternalLnd
+    {
+        public ExternalLndRest(LightningConnectionString connectionString) : base(connectionString, LndTypes.Rest) { }
+    }
+}
---
+++ BTCPayServer/Models/WalletViewModels/RescanWalletModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using NBXplorer.Models;
+
+namespace BTCPayServer.Models.WalletViewModels
+{
+    public class RescanWalletModel
+    {
+        public bool IsServerAdmin { get; set; }
+        public bool IsSupportedByCurrency { get; set; }
+        public bool IsFullySync { get; set; }
+        public bool Ok => IsServerAdmin && IsSupportedByCurrency && IsFullySync;
+
+        [Range(1000, 10_000)]
+        public int BatchSize { get; set; } = 3000;
+        [Range(0, 10_000_000)]
+        public int StartingIndex { get; set; } = 0;
+
+        [Range(100, 100000)]
+        public int GapLimit { get; set; } = 10000;
+
+        public int? Progress { get; set; }
+        public string PreviousError { get; set; }
+        public ScanUTXOProgress LastSuccess { get; internal set; }
+        public string TimeOfScan { get; internal set; }
+        public string RemainingTime { get; internal set; }
+    }
+}

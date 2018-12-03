156f52b76fc3f588a2ea8c0133018ea84386ce47
nicolas.dorier <nicolas.dorier@gmail.com>
2018-12-03 23:57:43 +0900

    Add bitpay translator

---
+++ BTCPayServer/Models/BitpayTranslatorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BTCPayServer.Models
+{
+    public class BitpayTranslatorViewModel
+    {
+        [Display(Name = "Bitpay's invoice URL or obsolete invoice url")]
+        public string BitpayLink { get; set; }
+        public string Address { get; set; }
+        public string Amount { get; set; }
+    }
+}

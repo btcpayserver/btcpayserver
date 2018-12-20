30a3a84ec9bcc55a75da52027506e05f558a3e90
Andrew Camilleri <anca@hesehus.dk>
2018-12-20 14:33:31 +0100

    fix final issues with integration

--- BTCPayServer/wwwroot/checkout/js/coinswitchComponent.js
+++ BTCPayServer/wwwroot/checkout/js/coinswitchComponent.js
@@ -2,8 +2,14 @@
     {
         props: ["toCurrency", "toCurrencyDue", "toCurrencyAddress", "merchantId", "autoload", "mode"],
         data: function () {
+            return {
+                opened: false
+            };
         },
         computed: {
+            showInlineIFrame: function(){
+                return this.url && this.opened;
+            },
             url: function () {
                 return window.location.origin + "/checkout/coinswitch.html?" +
                     "&toCurrency=" +
@@ -12,6 +18,8 @@
                     this.toCurrencyAddress +
                     "&toCurrencyDue=" +
                     this.toCurrencyDue +
+                    "&mode=" +
+                    this.mode +
                     (this.merchantId ? "&merchant_id=" + this.merchantId : "");
             }
         },
@@ -21,17 +29,37 @@
                     e.preventDefault();
                 }
 
-                var coinSwitchWindow = window.open(
-                    this.url,
-                    'CoinSwitch',
-                    'width=600,height=470,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=0,left=0,top=0');
-                coinSwitchWindow.opener = null;
-                coinSwitchWindow.focus();
-                
+                if(this.mode === 'inline'){
+                    this.opened = true;
+                    
+                }else if(this.mode ==="popup"){
+                    var coinSwitchWindow = window.open(
+                        this.url,
+                        'CoinSwitch',
+                        'width=360,height=650,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=0,left=0,top=0');
+                    coinSwitchWindow.opener = null;
+                    coinSwitchWindow.focus();
+                }
+            },
+            closeDialog(){
+                if(this.mode === 'inline'){
+                    this.opened = false;
+                }
+            },
+            onLoadIframe(event){
+                $("#prettydropdown-DefaultLang").hide();
+                var c = this.closeDialog.bind(this);
+                event.currentTarget.contentWindow.addEventListener("message", function(evt){
+                    if(evt && evt.data == "popup-closed"){
+                        c();
+
+                        $("#prettydropdown-DefaultLang").show();
+                    }
+                });
             }
         },
         mounted: function () {
-            if(this.autoload && this.mode === 'popup'){
+            if(this.autoload){
                 this.openDialog();
             }
         }

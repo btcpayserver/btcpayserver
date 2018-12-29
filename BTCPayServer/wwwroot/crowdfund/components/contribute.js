8bd7ea5bbc9f62428033aab9b56b442d29b634d4
Kukks <evilkukka@gmail.com>
2018-12-29 20:21:23 +0100

    start ux for perks

--- BTCPayServer/wwwroot/crowdfund/components/contribute.js
+++ BTCPayServer/wwwroot/crowdfund/components/contribute.js
@@ -1,5 +1,6 @@
 Vue.component('contribute', {
-    props: ["targetCurrency", "active", "inModal"],
+    props: ["targetCurrency", "active", "perks", "inModal"],
+    template: "#contribute-template",
     data: function () {
         return {
             email: "",
@@ -17,27 +18,19 @@ Vue.component('contribute', {
             }
             eventAggregator.$emit("contribute", {email: this.email, amount: this.amount, choiceKey: this.choiceKey});
         }
-    },
-
-    template: "<div>" +
-        "<h3 v-if='!inModal'>Contribute</h3>" +
-        "" +
-        "                            <form v-on:submit=\"onContributeFormSubmit\">" +
-        "" +
-        "                                <div class=\"form-group\">" +
-        "                                    <label ></label>" +
-        "                                    <input type=\"email\" class=\"form-control\" v-model=\"email\" placeholder='Email'/>" +
-        "                                </div>" +
-        "                                <div class=\"form-group\">" +
-        "                                    <label ></label>" +
-        "                                    <div class=\"input-group mb-3\">" +
-        "                                        <input type=\"number\" step=\"any\" class=\"form-control\" v-model=\"amount\" placeholder='Contribution Amount'/>" +
-        "                                        <div class=\"input-group-append\">" +
-        "                                            <span class=\"input-group-text\">{{targetCurrency}}</span>" +
-        "                                        </div>" +
-        "                                    </div>" +
-        "                                </div>" +
-        "                                <button type=\"submit\" class=\"btn btn-primary\" :disabled='!active' v-if='!inModal'>Contribute</button>" +
-        "                            </form>" +
-        "</div>"
+    }
 });
+
+Vue.component('perks-display', {
+    props: ["perks"],
+    template: "#perks-template",
+    data: function () {
+        return {
+        }
+    },
+    methods: {
+        
+    }
+});
+
+

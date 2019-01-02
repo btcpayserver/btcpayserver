8a646d85c6b7061d63a0bafd9f2beab64ca755ed
Kukks <evilkukka@gmail.com>
2019-01-02 09:03:20 +0100

    fix bundles

--- BTCPayServer/wwwroot/crowdfund/app.js
+++ BTCPayServer/wwwroot/crowdfund/app.js
@@ -16,6 +16,50 @@ function addLoadEvent(func) {
 }
 addLoadEvent(function (ev) {
     Vue.use(Toasted);
+
+    Vue.component('contribute', {
+        props: ["targetCurrency", "active", "perks", "inModal"],
+        template: "#contribute-template"
+    });
+
+    Vue.component('perks', {
+        props: ["perks", "targetCurrency", "active", "inModal"],
+        template: "#perks-template"
+    });
+
+    Vue.component('perk', {
+        props: ["perk", "targetCurrency", "active", "inModal"],
+        template: "#perk-template",
+        data: function () {
+            return {
+                amount: null,
+                expanded: false
+            }
+        },
+        methods: {
+            onContributeFormSubmit: function (e) {
+                if (e) {
+                    e.preventDefault();
+                }
+                if(!this.active){
+                    return;
+                }
+                eventAggregator.$emit("contribute", {amount: this.amount, choiceKey: this.choiceKey});
+            },
+            expand: function(){
+                if(this.active){
+                    this.expanded = true;
+                }
+            }
+
+
+        },
+        mounted: function(){
+            this.amount = this.perk.price.value;
+
+        }
+    });
+    
     app = new Vue({
         el: '#app',
         data: function(){

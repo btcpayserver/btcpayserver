79e4a77836586a61af88216ed08ff2e09aef6705
Kukks <evilkukka@gmail.com>
2019-01-14 22:43:29 +0100

    Add payment requests

--- BTCPayServer/Migrations/20190121133309_AddPaymentRequests.Designer.cs
+++ BTCPayServer/Migrations/20190121133309_AddPaymentRequests.Designer.cs
@@ -3,18 +3,20 @@ using System;
 using BTCPayServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BTCPayServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190121133309_AddPaymentRequests")]
+    partial class AddPaymentRequests
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.8-servicing-32085");
+                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024");
 
             modelBuilder.Entity("BTCPayServer.Data.AddressInvoiceData", b =>
                 {
@@ -62,8 +64,6 @@ namespace BTCPayServer.Migrations
                     b.Property<string>("Settings");
 
                     b.Property<string>("StoreDataId");
-
-                    b.Property<bool>("TagAllInvoices");
 
                     b.HasKey("Id");
 

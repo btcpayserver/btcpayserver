575b6ca2226bcc8966440b8b47f66e0ac0f50fd8
nicolas.dorier <nicolas.dorier@gmail.com>
2019-01-18 19:15:31 +0900

    Improve error messages when the store has no payment method configured

--- BTCPayServer/Controllers/StoresController.cs
+++ BTCPayServer/Controllers/StoresController.cs
@@ -96,6 +96,11 @@ namespace BTCPayServer.Controllers
         {
             get; set;
         }
+        [TempData]
+        public bool StoreNotConfigured
+        {
+            get; set;
+        }
 
         [HttpGet]
         [Route("{storeId}/users")]
@@ -567,6 +572,7 @@ namespace BTCPayServer.Controllers
             var model = new TokensViewModel();
             var tokens = await _TokenRepository.GetTokensByStoreIdAsync(StoreData.Id);
             model.StatusMessage = StatusMessage;
+            model.StoreNotConfigured = StoreNotConfigured;
             model.Tokens = tokens.Select(t => new TokenViewModel()
             {
                 Facade = t.Facade,
@@ -794,6 +800,10 @@ namespace BTCPayServer.Controllers
             var pairingResult = await _TokenRepository.PairWithStoreAsync(pairingCode, store.Id);
             if (pairingResult == PairingResult.Complete || pairingResult == PairingResult.Partial)
             {
+                var excludeFilter = store.GetStoreBlob().GetExcludedPaymentMethods();
+                StoreNotConfigured = store.GetSupportedPaymentMethods(_NetworkProvider)
+                                          .Where(p => !excludeFilter.Match(p.PaymentId))
+                                          .Count() == 0;
                 StatusMessage = "Pairing is successful";
                 if (pairingResult == PairingResult.Partial)
                     StatusMessage = "Server initiated pairing code: " + pairingCode;

080a648de03c1c1164992a55c090d03484e1feb2
Kukks <evilkukka@gmail.com>
2019-01-28 17:12:28 +0100

    Part 4: OpenIddict: Add API Client UI

---
+++ BTCPayServer/Validation/StringRangeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace BTCPayServer.Validation
+{
+    public class StringRangeAttribute : ValidationAttribute
+    {
+        public string[] AllowableValues { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (AllowableValues?.Contains(value?.ToString()) == true)
+            {
+                return ValidationResult.Success;
+            }
+
+            var msg = $"Please enter one of the allowable values: {string.Join(", ", (AllowableValues ?? new string[] { "No allowable values found" }))}.";
+            return new ValidationResult(msg);
+        }
+    }
+}

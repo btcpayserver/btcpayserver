010c6539959ed8a3c844eb2c226377de4544698d
nicolas.dorier <nicolas.dorier@gmail.com>
2019-02-20 12:27:10 +0900

    Create EventHostedServiceBase and make AppHubStreamer use this

--- BTCPayServer/Services/Apps/AppHubStreamer.cs
+++ BTCPayServer/Services/Apps/AppHubStreamer.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using BTCPayServer.Controllers;
 using BTCPayServer.Data;
 using BTCPayServer.Events;
+using BTCPayServer.HostedServices;
 using BTCPayServer.Logging;
 using BTCPayServer.Models.AppViewModels;
 using BTCPayServer.Payments;
@@ -23,67 +24,48 @@ using NBitcoin;
 
 namespace BTCPayServer.Services.Apps
 {
-    public class AppHubStreamer : IHostedService
+    public class AppHubStreamer : EventHostedServiceBase
     {
-        private readonly EventAggregator _EventAggregator;
-        private readonly IHubContext<AppHub> _HubContext;
         private readonly AppService _appService;
-        private List<IEventAggregatorSubscription> _Subscriptions;
-        private CancellationTokenSource _Cts;
+        private IHubContext<AppHub> _HubContext;
 
         public AppHubStreamer(EventAggregator eventAggregator,
-            IHubContext<AppHub> hubContext,
-            AppService appService)
+           IHubContext<AppHub> hubContext,
+           AppService appService) : base(eventAggregator)
         {
-            _EventAggregator = eventAggregator;
-            _HubContext = hubContext;
             _appService = appService;
+            _HubContext = hubContext;
         }
 
-        private async Task NotifyClients(string appId, InvoiceEvent invoiceEvent, CancellationToken cancellationToken)
+        protected override void SubscibeToEvents()
         {
-            if (invoiceEvent.Name == InvoiceEvent.ReceivedPayment)
+            Subscribe<InvoiceEvent>();
+            Subscribe<AppsController.AppUpdated>();
+        }
+
+        protected override  async Task ProcessEvent(object evt, CancellationToken cancellationToken)
+        {
+            if (evt is InvoiceEvent invoiceEvent)
             {
-                var data = invoiceEvent.Payment.GetCryptoPaymentData();
-                await _HubContext.Clients.Group(appId).SendCoreAsync(AppHub.PaymentReceived, new object[]
+                foreach (var appId in AppService.GetAppInternalTags(invoiceEvent.Invoice.InternalTags))
+                {
+                    if (invoiceEvent.Name == InvoiceEvent.ReceivedPayment)
                     {
+                        var data = invoiceEvent.Payment.GetCryptoPaymentData();
+                        await _HubContext.Clients.Group(appId).SendCoreAsync(AppHub.PaymentReceived, new object[]
+                            {
                         data.GetValue(),
                         invoiceEvent.Payment.GetCryptoCode(),
                         Enum.GetName(typeof(PaymentTypes),
                             invoiceEvent.Payment.GetPaymentMethodId().PaymentType)
-                    }, cancellationToken);
-            }
-            await InfoUpdated(appId);
-        }
-
-        Channel<object> _Events = Channel.CreateUnbounded<object>();
-        public async Task ProcessEvents(CancellationToken cancellationToken)
-        {
-            while (await _Events.Reader.WaitToReadAsync(cancellationToken))
-            {
-                if (_Events.Reader.TryRead(out var evt))
-                {
-                    try
-                    {
-                        if (evt is InvoiceEvent invoiceEvent)
-                        {
-                            foreach (var appId in AppService.GetAppInternalTags(invoiceEvent.Invoice.InternalTags))
-                                await NotifyClients(appId, invoiceEvent, cancellationToken);
-                        }
-                        else if (evt is AppsController.AppUpdated app)
-                        {
-                            await InfoUpdated(app.AppId);
-                        }
+                            }, cancellationToken);
                     }
-                    catch when (cancellationToken.IsCancellationRequested)
-                    {
-                        throw;
-                    }
-                    catch (Exception ex)
-                    {
-                        Logs.PayServer.LogWarning(ex, "Unhandled exception in CrowdfundHubStream");
-                    }
+                    await InfoUpdated(appId);
                 }
+            }
+            else if (evt is AppsController.AppUpdated app)
+            {
+                await InfoUpdated(app.AppId);
             }
         }
 
@@ -91,31 +73,6 @@ namespace BTCPayServer.Services.Apps
         {
             var info = await _appService.GetAppInfo(appId);
             await _HubContext.Clients.Group(appId).SendCoreAsync(AppHub.InfoUpdated, new object[] { info });
-        }
-
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _Subscriptions = new List<IEventAggregatorSubscription>()
-            {
-                _EventAggregator.Subscribe<InvoiceEvent>(e => _Events.Writer.TryWrite(e)),
-                _EventAggregator.Subscribe<AppsController.AppUpdated>(e => _Events.Writer.TryWrite(e))
-            };
-            _Cts = new CancellationTokenSource();
-            _ProcessingEvents = ProcessEvents(_Cts.Token);
-            return Task.CompletedTask;
-        }
-        Task _ProcessingEvents = Task.CompletedTask;
-
-        public async Task StopAsync(CancellationToken cancellationToken)
-        {
-            _Subscriptions?.ForEach(subscription => subscription.Dispose());
-            _Cts?.Cancel();
-            try
-            {
-                await _ProcessingEvents;
-            }
-            catch (OperationCanceledException)
-            { }
         }
     }
 }

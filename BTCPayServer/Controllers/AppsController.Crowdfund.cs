a7f9899ada8113f19dd39b7919336d958b95ba79
Kukks <evilkukka@gmail.com>
2019-02-22 08:32:10 +0100

    fix crowdfund invoice filter link

--- BTCPayServer/Controllers/AppsController.Crowdfund.cs
+++ BTCPayServer/Controllers/AppsController.Crowdfund.cs
@@ -54,7 +54,7 @@ namespace BTCPayServer.Controllers
                 ResetEvery = Enum.GetName(typeof(CrowdfundResetEvery), settings.ResetEvery),
                 UseAllStoreInvoices = app.TagAllInvoices,
                 AppId = appId,
-                SearchTerm = app.TagAllInvoices ? $"storeid:{app.StoreDataId}" : $"orderid:{AppService.GetCrowdfundOrderId(appId)}",
+                SearchTerm = app.TagAllInvoices ? $"storeid:{app.StoreDataId}" : $"appid:{appId}",
                 DisplayPerksRanking = settings.DisplayPerksRanking,
                 SortPerksByPopularity = settings.SortPerksByPopularity
             };

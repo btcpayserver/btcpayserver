bc23f1d5763bf5074759be63ef97f5e9b5176239
Kukks <evilkukka@gmail.com>
2019-04-09 10:32:17 +0200

    more tests

--- BTCPayServer.Tests/StorageTests.cs
+++ BTCPayServer.Tests/StorageTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using BTCPayServer.Controllers;
 using BTCPayServer.Models;
 using BTCPayServer.Models.ServerViewModels;
@@ -18,8 +19,7 @@ namespace BTCPayServer.Tests
 {
     public class StorageTests
     {
-    
-          public StorageTests(ITestOutputHelper helper)
+        public StorageTests(ITestOutputHelper helper)
         {
             Logs.Tester = new XUnitLog(helper) {Name = "Tests"};
             Logs.LogProvider = new XUnitLogProvider(helper);
@@ -38,7 +38,13 @@ namespace BTCPayServer.Tests
 
                 //Initially, there is no configuration, make sure we display the choices available to configure
                 Assert.IsType<StorageSettings>(Assert.IsType<ViewResult>(await controller.Storage()).Model);
-                
+
+                //also the file list should tell us it's not configured:
+                var viewFilesViewModelInitial =
+                    Assert.IsType<ViewFilesViewModel>(Assert.IsType<ViewResult>(await controller.Files()).Model);
+                Assert.False(viewFilesViewModelInitial.StorageConfigured);
+
+
                 //Once we select a provider, redirect to its view
                 var localResult = Assert
                     .IsType<RedirectToActionResult>(controller.Storage(new StorageSettings()
@@ -47,9 +53,8 @@ namespace BTCPayServer.Tests
                     }));
                 Assert.Equal(nameof(ServerController.StorageProvider), localResult.ActionName);
                 Assert.Equal(StorageProvider.FileSystem.ToString(), localResult.RouteValues["provider"]);
-                
-                
-                
+
+
                 var AmazonS3result = Assert
                     .IsType<RedirectToActionResult>(controller.Storage(new StorageSettings()
                     {
@@ -57,7 +62,7 @@ namespace BTCPayServer.Tests
                     }));
                 Assert.Equal(nameof(ServerController.StorageProvider), AmazonS3result.ActionName);
                 Assert.Equal(StorageProvider.AmazonS3.ToString(), AmazonS3result.RouteValues["provider"]);
-                
+
                 var GoogleResult = Assert
                     .IsType<RedirectToActionResult>(controller.Storage(new StorageSettings()
                     {
@@ -65,8 +70,8 @@ namespace BTCPayServer.Tests
                     }));
                 Assert.Equal(nameof(ServerController.StorageProvider), GoogleResult.ActionName);
                 Assert.Equal(StorageProvider.GoogleCloudStorage.ToString(), GoogleResult.RouteValues["provider"]);
-                
-                
+
+
                 var AzureResult = Assert
                     .IsType<RedirectToActionResult>(controller.Storage(new StorageSettings()
                     {
@@ -74,64 +79,46 @@ namespace BTCPayServer.Tests
                     }));
                 Assert.Equal(nameof(ServerController.StorageProvider), AzureResult.ActionName);
                 Assert.Equal(StorageProvider.AzureBlobStorage.ToString(), AzureResult.RouteValues["provider"]);
-                
+
                 //Cool, we get redirected to the config pages
                 //Let's configure this stuff
-                
+
                 //Let's try and cheat and go to an invalid storage provider config
                 Assert.Equal(nameof(Storage), (Assert
                     .IsType<RedirectToActionResult>(await controller.StorageProvider("I am not a real provider"))
                     .ActionName));
-                
+
                 //ok no more messing around, let's configure this shit. 
-                var fileSystemStorageConfiguration =   Assert.IsType<FileSystemStorageConfiguration>(Assert
+                var fileSystemStorageConfiguration = Assert.IsType<FileSystemStorageConfiguration>(Assert
                     .IsType<ViewResult>(await controller.StorageProvider(StorageProvider.FileSystem.ToString()))
                     .Model);
-                
+
                 //local file system does not need config, easy days!
-                Assert.IsType<ViewResult>(await controller.EditFileSystemStorageProvider(fileSystemStorageConfiguration));
-                
+                Assert.IsType<ViewResult>(
+                    await controller.EditFileSystemStorageProvider(fileSystemStorageConfiguration));
+
                 //ok cool, let's see if this got set right
                 var shouldBeRedirectingToLocalStorageConfigPage =
                     Assert.IsType<RedirectToActionResult>(await controller.Storage());
                 Assert.Equal(nameof(StorageProvider), shouldBeRedirectingToLocalStorageConfigPage.ActionName);
-                Assert.Equal(StorageProvider.FileSystem, shouldBeRedirectingToLocalStorageConfigPage.RouteValues["provider"]);
-                
-                
-                //ok we're set up with local file storage. let's change to azure
-                
+                Assert.Equal(StorageProvider.FileSystem,
+                    shouldBeRedirectingToLocalStorageConfigPage.RouteValues["provider"]);
+
+
                 //if we tell the settings page to force, it should allow us to select a new provider
                 Assert.IsType<StorageSettings>(Assert.IsType<ViewResult>(await controller.Storage(true)).Model);
-                
-                var azureBlobStorageConfiguration =   Assert.IsType<AzureBlobStorageConfiguration>(Assert
-                    .IsType<ViewResult>(await controller.StorageProvider(StorageProvider.AzureBlobStorage.ToString()))
-                    .Model);
 
-                azureBlobStorageConfiguration.ConnectionString = GetFromSecrets("AzureBlobStorageConnectionString");
-                Assert.IsType<ViewResult>(await controller.EditAzureBlobStorageStorageProvider(azureBlobStorageConfiguration));
-                
-                
-                var shouldBeRedirectingToAzureStorageConfigPage =
-                    Assert.IsType<RedirectToActionResult>(await controller.Storage());
-                Assert.Equal(nameof(StorageProvider), shouldBeRedirectingToLocalStorageConfigPage.ActionName);
-                Assert.Equal(StorageProvider.AzureBlobStorage, shouldBeRedirectingToLocalStorageConfigPage.RouteValues["provider"]);
-                
-               //seems like everything worked, let's see if the conn string was actually saved
-               
-               Assert.Equal(azureBlobStorageConfiguration.ConnectionString, Assert.IsType<AzureBlobStorageConfiguration>(Assert
-                   .IsType<ViewResult>(await controller.StorageProvider(StorageProvider.AzureBlobStorage.ToString()))
-                   .Model).ConnectionString);
-               
-               //awesome, now let's see if the files result says we're all set up
-               var viewFilesViewModel = Assert.IsType<ViewFilesViewModel>(Assert.IsType<ViewResult>(await controller.Files()).Model);
-               Assert.True(viewFilesViewModel.StorageConfigured);
-               Assert.Empty(viewFilesViewModel.Files);
+                //awesome, now let's see if the files result says we're all set up
+                var viewFilesViewModel =
+                    Assert.IsType<ViewFilesViewModel>(Assert.IsType<ViewResult>(await controller.Files()).Model);
+                Assert.True(viewFilesViewModel.StorageConfigured);
+                Assert.Empty(viewFilesViewModel.Files);
             }
         }
 
         [Fact]
         [Trait("Integration", "Integration")]
-        public async void CanUploadAndViewAndDeleteFiles()
+        public async void CanUseLocalProviderFiles()
         {
             using (var tester = ServerTester.Create())
             {
@@ -139,50 +126,99 @@ namespace BTCPayServer.Tests
                 var user = tester.NewAccount();
                 user.GrantAccess();
                 var controller = tester.PayTester.GetController<ServerController>(user.UserId, user.StoreId);
-                
-                
+
+
                 var shouldBeRedirectingToLocalStorageConfigPage =
                     Assert.IsType<RedirectToActionResult>(await controller.Storage());
                 Assert.Equal(nameof(StorageProvider), shouldBeRedirectingToLocalStorageConfigPage.ActionName);
-                Assert.Equal(StorageProvider.FileSystem, shouldBeRedirectingToLocalStorageConfigPage.RouteValues["provider"]);
-                
-                
+                Assert.Equal(StorageProvider.FileSystem,
+                    shouldBeRedirectingToLocalStorageConfigPage.RouteValues["provider"]);
 
-                var filename = "uploadtestfile.txt";
-                var fileContent = "content";
-                File.WriteAllText(filename, fileContent);
 
-                var fileInfo = new FileInfo(filename);
-                var formFile = new FormFile(
-                    new FileStream(filename, FileMode.OpenOrCreate),
-                    0,
-                    fileInfo.Length, fileInfo.Name, fileInfo.FullName);
-                var uploadFormFileResult = Assert.IsType<RedirectToActionResult>(await controller.CreateFile(formFile));
-                Assert.True(uploadFormFileResult.RouteValues.ContainsKey("fileId"));
-                var fileId = uploadFormFileResult.RouteValues["fileId"].ToString();
-                Assert.Equal("Files", uploadFormFileResult.ActionName);
-
-                var viewFilesViewModel =
-                    Assert.IsType<ViewFilesViewModel>(Assert.IsType<ViewResult>(await controller.Files(fileId)).Model);
-
-                Assert.NotEmpty(viewFilesViewModel.Files);
-                Assert.Equal(fileId, viewFilesViewModel.SelectedFileId);
-                Assert.NotEmpty(viewFilesViewModel.DirectFileUrl);
-                
-                
-                var net = new System.Net.WebClient();
-                var data = await net.DownloadStringTaskAsync(new Uri(viewFilesViewModel.DirectFileUrl));
-                Assert.Equal(fileContent, data);
-
-                Assert.Equal(StatusMessageModel.StatusSeverity.Success, new StatusMessageModel(Assert.IsType<RedirectToActionResult>(await controller.DeleteFile(fileId))
-                    .RouteValues["statusMessage"].ToString()).Severity);
-                
-                viewFilesViewModel =
-                    Assert.IsType<ViewFilesViewModel>(Assert.IsType<ViewResult>(await controller.Files(fileId)).Model);
-
-                Assert.Empty(viewFilesViewModel.Files);
+                await CanUploadRemoveFiles(controller);
             }
         }
+
+        [Fact]
+        [Trait("Integration", "Integration")]
+        public async Task CanUseAzureBlobStorage()
+        {
+            using (var tester = ServerTester.Create())
+            {
+                tester.Start();
+                var user = tester.NewAccount();
+                user.GrantAccess();
+                var controller = tester.PayTester.GetController<ServerController>(user.UserId, user.StoreId);
+                var azureBlobStorageConfiguration = Assert.IsType<AzureBlobStorageConfiguration>(Assert
+                    .IsType<ViewResult>(await controller.StorageProvider(StorageProvider.AzureBlobStorage.ToString()))
+                    .Model);
+
+                azureBlobStorageConfiguration.ConnectionString = GetFromSecrets("AzureBlobStorageConnectionString");
+                Assert.IsType<ViewResult>(
+                    await controller.EditAzureBlobStorageStorageProvider(azureBlobStorageConfiguration));
+
+
+                var shouldBeRedirectingToAzureStorageConfigPage =
+                    Assert.IsType<RedirectToActionResult>(await controller.Storage());
+                Assert.Equal(nameof(StorageProvider), shouldBeRedirectingToAzureStorageConfigPage.ActionName);
+                Assert.Equal(StorageProvider.AzureBlobStorage,
+                    shouldBeRedirectingToAzureStorageConfigPage.RouteValues["provider"]);
+
+                //seems like azure config worked, let's see if the conn string was actually saved
+
+                Assert.Equal(azureBlobStorageConfiguration.ConnectionString, Assert
+                    .IsType<AzureBlobStorageConfiguration>(Assert
+                        .IsType<ViewResult>(
+                            await controller.StorageProvider(StorageProvider.AzureBlobStorage.ToString()))
+                        .Model).ConnectionString);
+                
+                
+
+                await CanUploadRemoveFiles(controller);
+            }
+        }
+        
+        
+        private async Task CanUploadRemoveFiles(ServerController controller)
+        {
+            var filename = "uploadtestfile.txt";
+            var fileContent = "content";
+            File.WriteAllText(filename, fileContent);
+
+            var fileInfo = new FileInfo(filename);
+            var formFile = new FormFile(
+                new FileStream(filename, FileMode.OpenOrCreate),
+                0,
+                fileInfo.Length, fileInfo.Name, fileInfo.FullName);
+            var uploadFormFileResult = Assert.IsType<RedirectToActionResult>(await controller.CreateFile(formFile));
+            Assert.True(uploadFormFileResult.RouteValues.ContainsKey("fileId"));
+            var fileId = uploadFormFileResult.RouteValues["fileId"].ToString();
+            Assert.Equal("Files", uploadFormFileResult.ActionName);
+
+            var viewFilesViewModel =
+                Assert.IsType<ViewFilesViewModel>(Assert.IsType<ViewResult>(await controller.Files(fileId)).Model);
+
+            Assert.NotEmpty(viewFilesViewModel.Files);
+            Assert.Equal(fileId, viewFilesViewModel.SelectedFileId);
+            Assert.NotEmpty(viewFilesViewModel.DirectFileUrl);
+
+
+            var net = new System.Net.WebClient();
+            var data = await net.DownloadStringTaskAsync(new Uri(viewFilesViewModel.DirectFileUrl));
+            Assert.Equal(fileContent, data);
+
+            Assert.Equal(StatusMessageModel.StatusSeverity.Success, new StatusMessageModel(Assert
+                .IsType<RedirectToActionResult>(await controller.DeleteFile(fileId))
+                .RouteValues["statusMessage"].ToString()).Severity);
+
+            viewFilesViewModel =
+                Assert.IsType<ViewFilesViewModel>(Assert.IsType<ViewResult>(await controller.Files(fileId)).Model);
+
+            Assert.Empty(viewFilesViewModel.Files);
+        }
+
+      
+
         private static string GetFromSecrets(string key)
         {
             var builder = new ConfigurationBuilder();

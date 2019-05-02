3382270929327b9a11150f032cb3a1eecc4d06b1
Kukks <evilkukka@gmail.com>
2019-05-02 13:44:31 +0200

    debug sh

--- BTCPayServer.Tests/docker-entrypoint.sh
+++ BTCPayServer.Tests/docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 dotnet test --filter Fast=Fast --no-build
 dotnet test --filter Integration=Integration --no-build -v n
+echo "Running external tests set to: $TESTS_RUN_EXTERNAL_INTEGRATION"
+
 if [[ "$TESTS_RUN_EXTERNAL_INTEGRATION" == "true" ]]; then
     dotnet test --filter ExternalIntegration=ExternalIntegration --no-build -v n
 fi

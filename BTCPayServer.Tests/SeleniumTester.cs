8ffb81cdf3c1004c2ed5f24119042faf90798c98
nicolas.dorier <nicolas.dorier@gmail.com>
2019-05-13 18:42:20 +0900

    Refactor Selenium tests to properly cleanup resources

---
+++ BTCPayServer.Tests/SeleniumTester.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using NBitcoin;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+using System.IO;
+
+namespace BTCPayServer.Tests
+{
+    public class SeleniumTester : IDisposable
+    {
+        public IWebDriver Driver { get; set; }
+        public ServerTester Server { get; set; }
+
+        string _Directory;
+        public static SeleniumTester Create([CallerMemberNameAttribute] string scope = null)
+        {
+            
+            var server = ServerTester.Create(scope);
+            return new SeleniumTester()
+            {
+                _Directory = scope,
+                Server = server
+            };
+        }
+
+        public void Start()
+        {
+            Server.Start();
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("headless"); // Comment to view browser
+            options.AddArguments("window-size=1200x600"); // Comment to view browser
+            Driver = new ChromeDriver(Directory.GetCurrentDirectory(), options);
+            Driver.Navigate().GoToUrl(Server.PayTester.ServerUri);
+        }
+
+
+        public string RegisterNewUser(bool isAdmin = false)
+        {
+            var usr = RandomUtils.GetUInt256().ToString() + "@a.com";
+            Driver.FindElement(By.Id("Register")).Click();
+            Driver.FindElement(By.Id("Email")).SendKeys(usr);
+            Driver.FindElement(By.Id("Password")).SendKeys("123456");
+            Driver.FindElement(By.Id("ConfirmPassword")).SendKeys("123456");
+            Driver.FindElement(By.Id("IsAdmin")).Click();
+            Driver.FindElement(By.Id("RegisterButton")).Click();
+            Driver.AssertNoError();
+            return usr;
+        }
+
+        public string CreateNewStore()
+        {
+            var usr = "Store" + RandomUtils.GetUInt64().ToString();
+            Driver.FindElement(By.Id("Stores")).Click();
+            Driver.FindElement(By.Id("CreateStore")).Click();
+            Driver.FindElement(By.Id("Name")).SendKeys(usr);
+            Driver.FindElement(By.Id("Create")).Click();
+            return usr;
+        }
+
+        public void AddDerivationScheme()
+        {
+            Driver.FindElement(By.Id("ModifyBTC")).Click();
+            Driver.FindElement(By.Id("DerivationScheme")).SendKeys("xpub661MyMwAqRbcGABgHMUXDzPzH1tU7eZaAaJQXhDXsSxsqyQzQeU6kznNfSuAyqAK9UaWSaZaMFdNiY5BCF4zBPAzSnwfUAwUhwttuAKwfRX-[legacy]");
+            Driver.FindElement(By.Id("Continue")).Click();
+            Driver.FindElement(By.Id("Confirm")).Click();
+            Driver.FindElement(By.Id("Save")).Click();
+            return;
+        }
+
+        public void ClickOnAllSideMenus()
+        {
+            var links = Driver.FindElements(By.CssSelector(".nav-pills .nav-link")).Select(c => c.GetAttribute("href")).ToList();
+            Assert.NotEmpty(links);
+            foreach (var l in links)
+            {
+                Driver.Navigate().GoToUrl(l);
+                Driver.AssertNoError();
+            }
+        }
+
+        public void CreateInvoice(string random)
+        {
+            Driver.FindElement(By.Id("Invoices")).Click();
+            Driver.FindElement(By.Id("CreateNewInvoice")).Click();
+            Driver.FindElement(By.CssSelector("input#Amount.form-control")).SendKeys("100");
+            Driver.FindElement(By.Name("StoreId")).SendKeys("Deriv" + random + Keys.Enter);
+            Driver.FindElement(By.Id("Create")).Click();
+            return;
+        }
+
+
+        public void Dispose()
+        {
+            if (Driver != null)
+            {
+                try
+                {
+                    Driver.Close();
+                }
+                catch { }
+                Driver.Dispose();
+            }
+            if (Server != null)
+                Server.Dispose();
+        }
+    }
+}
--- BTCPayServer.Tests/SeleniumTests.cs
+++ BTCPayServer.Tests/SeleniumTests.cs
@@ -10,293 +10,195 @@ using NBitcoin;
 
 namespace BTCPayServer.Tests
 {
-    public class Base
+    [Trait("Selenium", "Selenium")]
+    public class ChromeTests
     {
-        public IWebDriver Driver { get; set; }
-
-        public Base(ITestOutputHelper helper)
+        public ChromeTests(ITestOutputHelper helper)
         {
             Logs.Tester = new XUnitLog(helper) { Name = "Tests" };
             Logs.LogProvider = new XUnitLogProvider(helper);
         }
 
-        protected void Wrap(Action<string> func)
-        {
-            using (var tester = ServerTester.Create())
-            {
-                tester.Start();
-                var logOut = Driver.FindElements(By.Id("Logout"));
-                if (logOut.Count != 0)
-                    logOut[0].Click();
-                func.Invoke(tester.PayTester.ServerUri.ToString());
-            }
-        }
-    }
-
-    public class Browsers : Base
-    {
-        public Browsers(ITestOutputHelper helper) : base(helper)
-        {
-            ChromeOptions options = new ChromeOptions();
-            options.AddArguments("headless"); // Comment to view browser
-            options.AddArguments("window-size=1200x600"); // Comment to view browser
-            Driver = new ChromeDriver(Environment.CurrentDirectory, options);
-        }
-
-        public string RegisterNewUser(bool isAdmin = false)
-        {
-            var usr = RandomUtils.GetUInt256().ToString() + "@a.com";
-            Driver.FindElement(By.Id("Register")).Click();
-            Driver.FindElement(By.Id("Email")).SendKeys(usr);
-            Driver.FindElement(By.Id("Password")).SendKeys("123456");
-            Driver.FindElement(By.Id("ConfirmPassword")).SendKeys("123456");
-            Driver.FindElement(By.Id("IsAdmin")).Click();
-            Driver.FindElement(By.Id("RegisterButton")).Click();
-            Driver.AssertNoError();
-            return usr;
-        }
-
-        public string CreateNewStore()
-        {
-            var usr = "Store" + RandomUtils.GetUInt64().ToString();
-            Driver.FindElement(By.Id("Stores")).Click();
-            Driver.FindElement(By.Id("CreateStore")).Click();
-            Driver.FindElement(By.Id("Name")).SendKeys(usr);
-            Driver.FindElement(By.Id("Create")).Click();
-            return usr;
-        }
-
-        public void AddDerivationScheme()
-        {
-            Driver.FindElement(By.Id("ModifyBTC")).Click();
-            Driver.FindElement(By.Id("DerivationScheme")).SendKeys("xpub661MyMwAqRbcGABgHMUXDzPzH1tU7eZaAaJQXhDXsSxsqyQzQeU6kznNfSuAyqAK9UaWSaZaMFdNiY5BCF4zBPAzSnwfUAwUhwttuAKwfRX-[legacy]");
-            Driver.FindElement(By.Id("Continue")).Click();
-            Driver.FindElement(By.Id("Confirm")).Click();
-            Driver.FindElement(By.Id("Save")).Click();
-            return;
-        }
-
-        public void CreateInvoice(string random)
-        {
-            Driver.FindElement(By.Id("Invoices")).Click();
-            Driver.FindElement(By.Id("CreateNewInvoice")).Click();
-            Driver.FindElement(By.CssSelector("input#Amount.form-control")).SendKeys("100");
-            Driver.FindElement(By.Name("StoreId")).SendKeys("Deriv" + random + Keys.Enter);
-            Driver.FindElement(By.Id("Create")).Click();
-            return;
-        }
-        
-    }
-    
-    [Trait("Selenium", "Selenium")]
-    public class ChromeTests : Browsers
-    {
-        [Fact]
-        public void AccessRequiresLogin()
-        {
-            Wrap(s =>
-            {
-                Driver.Navigate().GoToUrl(s);
-                Assert.Contains("Login", Driver.PageSource);
-                Driver.Quit();
-            });
-        }
-
         [Fact]
         public void CanNavigateServerSettings()
         {
-            Wrap(s =>
+            using (var s = SeleniumTester.Create())
             {
-                Driver.Navigate().GoToUrl(s);
-                RegisterNewUser(true);
-                Driver.FindElement(By.Id("ServerSettings")).Click();
-                Driver.AssertNoError();
-                ClickOnAllSideMenus();
-                Driver.Quit();
-            });
+                s.Start();
+                s.RegisterNewUser(true);
+                s.Driver.FindElement(By.Id("ServerSettings")).Click();
+                s.Driver.AssertNoError();
+                s.ClickOnAllSideMenus();
+                s.Driver.Quit();
+            }
         }
 
         [Fact]
         public void NewUserLogin()
         {
-            Wrap(s =>
+            using (var s = SeleniumTester.Create())
             {
+                s.Start();
                 //Register & Log Out
-                Driver.Navigate().GoToUrl(s);
-                var email = RegisterNewUser();
-                Driver.FindElement(By.Id("Logout")).Click();
-                Driver.AssertNoError();
+                var email = s.RegisterNewUser();
+                s.Driver.FindElement(By.Id("Logout")).Click();
+                s.Driver.AssertNoError();
 
                 //Same User Can Log Back In
-                Driver.FindElement(By.Id("Login")).Click();
-                Driver.FindElement(By.Id("Email")).SendKeys(email);
-                Driver.FindElement(By.Id("Password")).SendKeys("123456");
-                Driver.FindElement(By.Id("LoginButton")).Click();
-                Driver.AssertNoError();
+                s.Driver.FindElement(By.Id("Login")).Click();
+                s.Driver.FindElement(By.Id("Email")).SendKeys(email);
+                s.Driver.FindElement(By.Id("Password")).SendKeys("123456");
+                s.Driver.FindElement(By.Id("LoginButton")).Click();
+                s.Driver.AssertNoError();
 
                 //Change Password & Log Out
-                Driver.FindElement(By.Id("MySettings")).Click();
-                Driver.FindElement(By.Id("ChangePassword")).Click();
-                Driver.FindElement(By.Id("OldPassword")).SendKeys("123456");
-                Driver.FindElement(By.Id("NewPassword")).SendKeys("abc???");
-                Driver.FindElement(By.Id("ConfirmPassword")).SendKeys("abc???");
-                Driver.FindElement(By.Id("UpdatePassword")).Click();
-                Driver.FindElement(By.Id("Logout")).Click();
-                Driver.AssertNoError();
+                s.Driver.FindElement(By.Id("MySettings")).Click();
+                s.Driver.FindElement(By.Id("ChangePassword")).Click();
+                s.Driver.FindElement(By.Id("OldPassword")).SendKeys("123456");
+                s.Driver.FindElement(By.Id("NewPassword")).SendKeys("abc???");
+                s.Driver.FindElement(By.Id("ConfirmPassword")).SendKeys("abc???");
+                s.Driver.FindElement(By.Id("UpdatePassword")).Click();
+                s.Driver.FindElement(By.Id("Logout")).Click();
+                s.Driver.AssertNoError();
 
                 //Log In With New Password
-                Driver.FindElement(By.Id("Login")).Click();
-                Driver.FindElement(By.Id("Email")).SendKeys(email);
-                Driver.FindElement(By.Id("Password")).SendKeys("abc???");
-                Driver.FindElement(By.Id("LoginButton")).Click();
-                Assert.True(Driver.PageSource.Contains("Stores"), "Can't Access Stores");
-                Driver.Quit();
-            });
+                s.Driver.FindElement(By.Id("Login")).Click();
+                s.Driver.FindElement(By.Id("Email")).SendKeys(email);
+                s.Driver.FindElement(By.Id("Password")).SendKeys("abc???");
+                s.Driver.FindElement(By.Id("LoginButton")).Click();
+                Assert.True(s.Driver.PageSource.Contains("Stores"), "Can't Access Stores");
+                s.Driver.Quit();
+            }
         }
         
 
         [Fact]
         public void CanCreateStores()
         {
-            Wrap(s =>
+            using (var s = SeleniumTester.Create())
             {
-                Driver.Navigate().GoToUrl(s);
-                RegisterNewUser();
-                var store = CreateNewStore();
-                Driver.AssertNoError();
-                Assert.Contains(store, Driver.PageSource);
+                s.Start();
+                s.RegisterNewUser();
+                var store = s.CreateNewStore();
+                s.Driver.AssertNoError();
+                Assert.Contains(store, s.Driver.PageSource);
 
-                ClickOnAllSideMenus();
+                s.ClickOnAllSideMenus();
 
-                Driver.Quit();
-            });
-        }
-
-        private void ClickOnAllSideMenus()
-        {
-            var links = Driver.FindElements(By.CssSelector(".nav-pills .nav-link")).Select(c => c.GetAttribute("href")).ToList();
-            Assert.NotEmpty(links);
-            foreach (var l in links)
-            {
-                Driver.Navigate().GoToUrl(l);
-                Driver.AssertNoError();
+                s.Driver.Quit();
             }
         }
 
         [Fact]
         public void CanCreateInvoice()
         {
-            Wrap(s =>
+            using (var s = SeleniumTester.Create())
             {
-                Driver.Navigate().GoToUrl(s);
-                RegisterNewUser();
-                var store = CreateNewStore();
-                AddDerivationScheme();
+                s.Start();
+                s.RegisterNewUser();
+                var store = s.CreateNewStore();
+                s.AddDerivationScheme();
 
-                Driver.FindElement(By.Id("Invoices")).Click();
-                Driver.FindElement(By.Id("CreateNewInvoice")).Click();
-                Driver.FindElement(By.CssSelector("input#Amount.form-control")).SendKeys("100");
-                Driver.FindElement(By.Name("StoreId")).SendKeys(store + Keys.Enter);
-                Driver.FindElement(By.Id("Create")).Click();
-                Assert.True(Driver.PageSource.Contains("just created!"), "Unable to create Invoice");
-                Driver.Quit();
-            });
+                s.Driver.FindElement(By.Id("Invoices")).Click();
+                s.Driver.FindElement(By.Id("CreateNewInvoice")).Click();
+                s.Driver.FindElement(By.CssSelector("input#Amount.form-control")).SendKeys("100");
+                s.Driver.FindElement(By.Name("StoreId")).SendKeys(store + Keys.Enter);
+                s.Driver.FindElement(By.Id("Create")).Click();
+                Assert.True(s.Driver.PageSource.Contains("just created!"), "Unable to create Invoice");
+                s.Driver.Quit();
+            }
         }
         
         [Fact]
         public void CanCreateAppPoS()
         {
-            Wrap(s =>
+            using (var s = SeleniumTester.Create())
             {
-                Driver.Navigate().GoToUrl(s);
-                RegisterNewUser();
-                var store = CreateNewStore();
+                s.Start();
+                s.RegisterNewUser();
+                var store = s.CreateNewStore();
 
-                Driver.FindElement(By.Id("Apps")).Click();
-                Driver.FindElement(By.Id("CreateNewApp")).Click();
-                Driver.FindElement(By.Name("Name")).SendKeys("PoS" + store);
-                Driver.FindElement(By.CssSelector("select#SelectedAppType.form-control")).SendKeys("PointOfSale" + Keys.Enter);
-                Driver.FindElement(By.CssSelector("select#SelectedStore.form-control")).SendKeys(store + Keys.Enter);
-                Driver.FindElement(By.Id("Create")).Click();
-                Driver.FindElement(By.CssSelector("input#EnableShoppingCart.form-check")).Click();
-                Driver.FindElement(By.Id("SaveSettings")).Click();
-                Assert.True(Driver.PageSource.Contains("App updated"), "Unable to create PoS");
-                Driver.Quit();
-            });
+                s.Driver.FindElement(By.Id("Apps")).Click();
+                s.Driver.FindElement(By.Id("CreateNewApp")).Click();
+                s.Driver.FindElement(By.Name("Name")).SendKeys("PoS" + store);
+                s.Driver.FindElement(By.CssSelector("select#SelectedAppType.form-control")).SendKeys("PointOfSale" + Keys.Enter);
+                s.Driver.FindElement(By.CssSelector("select#SelectedStore.form-control")).SendKeys(store + Keys.Enter);
+                s.Driver.FindElement(By.Id("Create")).Click();
+                s.Driver.FindElement(By.CssSelector("input#EnableShoppingCart.form-check")).Click();
+                s.Driver.FindElement(By.Id("SaveSettings")).Click();
+                Assert.True(s.Driver.PageSource.Contains("App updated"), "Unable to create PoS");
+                s.Driver.Quit();
+            }
         }
 
         [Fact]
         public void CanCreateAppCF()
         {
-            Wrap(s =>
+            using (var s = SeleniumTester.Create())
             {
-                Driver.Navigate().GoToUrl(s);
-                RegisterNewUser();
-                var store = CreateNewStore();
-                AddDerivationScheme();
+                s.Start();
+                s.RegisterNewUser();
+                var store = s.CreateNewStore();
+                s.AddDerivationScheme();
 
-                Driver.FindElement(By.Id("Apps")).Click();
-                Driver.FindElement(By.Id("CreateNewApp")).Click();
-                Driver.FindElement(By.Name("Name")).SendKeys("CF" + store);
-                Driver.FindElement(By.CssSelector("select#SelectedAppType.form-control")).SendKeys("Crowdfund" + Keys.Enter);
-                Driver.FindElement(By.CssSelector("select#SelectedStore.form-control")).SendKeys(store + Keys.Enter);
-                Driver.FindElement(By.Id("Create")).Click();
-                Driver.FindElement(By.Id("Title")).SendKeys("Kukkstarter");
-                Driver.FindElement(By.CssSelector("div.note-editable.card-block")).SendKeys("1BTC = 1BTC");
-                Driver.FindElement(By.Id("TargetCurrency")).SendKeys("JPY");
-                Driver.FindElement(By.Id("TargetAmount")).SendKeys("700");
-                Driver.FindElement(By.Id("SaveSettings")).Submit();
-                Driver.FindElement(By.Id("ViewApp")).Click();
-                Driver.SwitchTo().Window(Driver.WindowHandles.Last());
-                Assert.True(Driver.PageSource.Contains("Currently Active!"), "Unable to create CF");
-                Driver.Quit();
-            }); 
+                s.Driver.FindElement(By.Id("Apps")).Click();
+                s.Driver.FindElement(By.Id("CreateNewApp")).Click();
+                s.Driver.FindElement(By.Name("Name")).SendKeys("CF" + store);
+                s.Driver.FindElement(By.CssSelector("select#SelectedAppType.form-control")).SendKeys("Crowdfund" + Keys.Enter);
+                s.Driver.FindElement(By.CssSelector("select#SelectedStore.form-control")).SendKeys(store + Keys.Enter);
+                s.Driver.FindElement(By.Id("Create")).Click();
+                s.Driver.FindElement(By.Id("Title")).SendKeys("Kukkstarter");
+                s.Driver.FindElement(By.CssSelector("div.note-editable.card-block")).SendKeys("1BTC = 1BTC");
+                s.Driver.FindElement(By.Id("TargetCurrency")).SendKeys("JPY");
+                s.Driver.FindElement(By.Id("TargetAmount")).SendKeys("700");
+                s.Driver.FindElement(By.Id("SaveSettings")).Submit();
+                s.Driver.FindElement(By.Id("ViewApp")).Click();
+                s.Driver.SwitchTo().Window(s.Driver.WindowHandles.Last());
+                Assert.True(s.Driver.PageSource.Contains("Currently Active!"), "Unable to create CF");
+                s.Driver.Quit();
+            } 
         }
 
         [Fact]
         public void CanCreatePayRequest()
         {
-            Wrap(s =>
+            using (var s = SeleniumTester.Create())
             {
-                Driver.Navigate().GoToUrl(s);
-                RegisterNewUser();
-                CreateNewStore();
-                AddDerivationScheme();
+                s.Start();
+                s.RegisterNewUser();
+                s.CreateNewStore();
+                s.AddDerivationScheme();
 
-                Driver.FindElement(By.Id("PaymentRequests")).Click();
-                Driver.FindElement(By.Id("CreatePaymentRequest")).Click();
-                Driver.FindElement(By.Id("Title")).SendKeys("Pay123");
-                Driver.FindElement(By.Id("Amount")).SendKeys("700");
-                Driver.FindElement(By.Id("Currency")).SendKeys("BTC");
-                Driver.FindElement(By.Id("SaveButton")).Submit();
-                Driver.FindElement(By.Name("ViewAppButton")).SendKeys(Keys.Return);
-                Driver.SwitchTo().Window(Driver.WindowHandles.Last());
-                Assert.True(Driver.PageSource.Contains("Amount due"), "Unable to create Payment Request");
-                Driver.Quit();
-            });
+                s.Driver.FindElement(By.Id("PaymentRequests")).Click();
+                s.Driver.FindElement(By.Id("CreatePaymentRequest")).Click();
+                s.Driver.FindElement(By.Id("Title")).SendKeys("Pay123");
+                s.Driver.FindElement(By.Id("Amount")).SendKeys("700");
+                s.Driver.FindElement(By.Id("Currency")).SendKeys("BTC");
+                s.Driver.FindElement(By.Id("SaveButton")).Submit();
+                s.Driver.FindElement(By.Name("ViewAppButton")).SendKeys(Keys.Return);
+                s.Driver.SwitchTo().Window(s.Driver.WindowHandles.Last());
+                Assert.True(s.Driver.PageSource.Contains("Amount due"), "Unable to create Payment Request");
+                s.Driver.Quit();
+            }
         }
 
         [Fact]
         public void CanManageWallet()
         {
-            Wrap(s =>
+            using (var s = SeleniumTester.Create())
             {
-                Driver.Navigate().GoToUrl(s);
-                RegisterNewUser();
-                CreateNewStore();
-                AddDerivationScheme();
+                s.Start();
+                s.RegisterNewUser();
+                s.CreateNewStore();
+                s.AddDerivationScheme();
 
-                Driver.FindElement(By.Id("Wallets")).Click();
-                Driver.FindElement(By.LinkText("Manage")).Click();
+                s.Driver.FindElement(By.Id("Wallets")).Click();
+                s.Driver.FindElement(By.LinkText("Manage")).Click();
 
-                ClickOnAllSideMenus();
+                s.ClickOnAllSideMenus();
 
-                Driver.Quit();
-            });
-        }
-
-        public ChromeTests(ITestOutputHelper helper) : base(helper)
-        {
+                s.Driver.Quit();
+            }
         }
     }
 }

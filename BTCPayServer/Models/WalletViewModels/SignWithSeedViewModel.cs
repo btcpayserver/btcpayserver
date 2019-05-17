55a48ff84a3351a194c5b4649c1e5ceda21989fc
nicolas.dorier <nicolas.dorier@gmail.com>
2019-05-17 14:42:28 +0900

    Add some notice in sign with seed

--- BTCPayServer/Models/WalletViewModels/SignWithSeedViewModel.cs
+++ BTCPayServer/Models/WalletViewModels/SignWithSeedViewModel.cs
@@ -9,7 +9,7 @@ namespace BTCPayServer.Models.WalletViewModels
     {
         [Required]
         public string PSBT { get; set; }
-        [Required][Display(Name = "Seed(12/24 word mnemonic seed) Or HD private key(xprv...)")]
+        [Required][Display(Name = "BIP39 Seed (12/24 word mnemonic phrase) or HD private key (xprv...)")]
         public string SeedOrKey { get; set; }
 
         [Display(Name = "Optional seed passphrase")]

82c965d3d808a3fd3b84be9a1173c9e9dc708c27
Kukks <evilkukka@gmail.com>
2019-05-28 12:49:00 +0200

    fix serializer when network not provided

--- BTCPayServer/Services/Invoices/InvoiceRepository.cs
+++ BTCPayServer/Services/Invoices/InvoiceRepository.cs
@@ -717,6 +717,10 @@ retry:
 
         private string ToString<T>(T data, BTCPayNetworkBase network)
         {
+            if (network == null)
+            {
+                return JsonConvert.SerializeObject(data);
+            }
             return network.ToString(data);
         }
 

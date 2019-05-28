90e0bbf8e7930d20afd91ce5a72d158c335af1ff
Kukks <evilkukka@gmail.com>
2019-05-28 11:12:03 +0200

    some type fixes

--- BTCPayServer.Tests/TestAccount.cs
+++ BTCPayServer.Tests/TestAccount.cs
@@ -87,7 +87,7 @@ namespace BTCPayServer.Tests
             parent.Stores.Add(StoreId);
         }
 
-        public BTCPayNetwork SupportedNetwork { get; set; }
+        public BitcoinSpecificBTCPayNetwork SupportedNetwork { get; set; }
 
         public WalletId RegisterDerivationScheme(string crytoCode, bool segwit = false)
         {
@@ -95,7 +95,7 @@ namespace BTCPayServer.Tests
         }
         public async Task<WalletId> RegisterDerivationSchemeAsync(string cryptoCode, bool segwit = false)
         {
-            SupportedNetwork = parent.NetworkProvider.GetNetwork(cryptoCode);
+            SupportedNetwork = parent.NetworkProvider.GetNetwork<BitcoinSpecificBTCPayNetwork>(cryptoCode);
             var store = parent.PayTester.GetController<StoresController>(UserId, StoreId);
             ExtKey = new ExtKey().GetWif(SupportedNetwork.NBitcoinNetwork);
             DerivationScheme = new DerivationStrategyFactory(SupportedNetwork.NBitcoinNetwork).Parse(ExtKey.Neuter().ToString() + (segwit ? "" : "-[legacy]"));

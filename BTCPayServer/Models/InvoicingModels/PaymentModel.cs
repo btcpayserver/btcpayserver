ccc1728dcb902e121e9e31343904e68860a4e011
Kukks <evilkukka@gmail.com>
2019-07-17 11:20:59 +0200

    revert "modular ui" code

--- BTCPayServer/Models/InvoicingModels/PaymentModel.cs
+++ BTCPayServer/Models/InvoicingModels/PaymentModel.cs
@@ -76,6 +76,5 @@ namespace BTCPayServer.Models.InvoicingModels
         public string RootPath { get; set; }
         public decimal CoinSwitchAmountMarkupPercentage { get; set; }
         public bool RedirectAutomatically { get; set; }
-        public string CheckoutBodyPartial { get; set; }
     }
 }

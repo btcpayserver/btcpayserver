90bac2e1ce72e07631e126d632dae8f2a40df049
Kukks <evilkukka@gmail.com>
2019-07-25 15:58:57 +0200

    fix test

--- BTCPayServer.Tests/UnitTest1.cs
+++ BTCPayServer.Tests/UnitTest1.cs
@@ -2602,7 +2602,8 @@ donation:
             var fetcher = new RateFetcher(factory);
             var pairs =
                     provider.GetAll()
-                    .Select(c => new CurrencyPair(c.CryptoCode, "USD"))
+                        .Where(c => !string.IsNullOrEmpty(c.CryptoCode))
+                        .Select(c => new CurrencyPair(c.CryptoCode, "USD"))
                     .ToHashSet();
 
             var rules = new StoreBlob().GetDefaultRateRules(provider);

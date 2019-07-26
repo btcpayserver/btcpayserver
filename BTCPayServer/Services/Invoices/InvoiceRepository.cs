4ad62230d1c90fd692bbcb27e9634837a8d316c3
Kukks <evilkukka@gmail.com>
2019-07-26 15:50:59 +0200

    remove junk spaces

--- BTCPayServer/Services/Invoices/InvoiceRepository.cs
+++ BTCPayServer/Services/Invoices/InvoiceRepository.cs
@@ -174,7 +174,7 @@ retry:
 
                 foreach (var paymentMethod in invoice.GetPaymentMethods())
                 {
-                    if (paymentMethod.Network == null )
+                    if (paymentMethod.Network == null)
                         throw new InvalidOperationException("CryptoCode unsupported");
                     var paymentDestination = paymentMethod.GetPaymentMethodDetails().GetPaymentDestination();
 

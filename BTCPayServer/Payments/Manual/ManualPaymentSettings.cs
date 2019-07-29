3475d182f60dfa26f73df7b54471c685a32f6aa4
Kukks <evilkukka@gmail.com>
2019-07-29 18:26:37 +0200

    start adding tests

---
+++ BTCPayServer/Payments/Manual/ManualPaymentSettings.cs
@@ -0,0 +1,14 @@
+namespace BTCPayServer.Payments.Bitcoin
+{
+    public class ManualPaymentSettings : ISupportedPaymentMethod
+    {
+        public PaymentMethodId PaymentId { get; } = StaticPaymentId;
+        public static PaymentMethodId StaticPaymentId { get; } = new PaymentMethodId(string.Empty, PaymentTypes.Manual);
+
+        public string DisplayText { get; set; } = string.Empty;
+        public bool AllowCustomerToMarkPaid { get; set; } = false;
+        public bool AllowPartialPaymentInput { get; set; } = false;
+        public bool AllowPaymentNote { get; set; } = false;
+        public bool SetPaymentAsConfirmed { get; set; } = true;
+    }
+}

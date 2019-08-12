b68135434699877754874b1f60ab507f3e10cc96
Kukks <evilkukka@gmail.com>
2019-08-12 15:26:34 +0200

    start working on store config UI

--- BTCPayServer/Monero/UI/MoneroLikeStoreController.cs
+++ BTCPayServer/Monero/UI/MoneroLikeStoreController.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BTCPayServer.Data;
+using BTCPayServer.Payments;
 using BTCPayServer.Payments.Monero;
 using BTCPayServer.Security;
 using BTCPayServer.Services.Stores;
@@ -15,14 +17,18 @@ namespace BTCPayServer.Controllers
     [Authorize(Policy = Policies.CanModifyServerSettings.Key, AuthenticationSchemes = Policies.CookieAuthentication)]
     public class MoneroLikeStoreController : Controller
     {
+        private readonly MoneroLikeConfiguration _MoneroLikeConfiguration;
         private readonly StoreRepository _StoreRepository;
+        private readonly MoneroRPCProvider _MoneroRpcProvider;
         private readonly BTCPayNetworkProvider _BtcPayNetworkProvider;
 
         public MoneroLikeStoreController(MoneroLikeConfiguration moneroLikeConfiguration,
             StoreRepository storeRepository, MoneroRPCProvider moneroRpcProvider,
             BTCPayNetworkProvider btcPayNetworkProvider)
         {
+            _MoneroLikeConfiguration = moneroLikeConfiguration;
             _StoreRepository = storeRepository;
+            _MoneroRpcProvider = moneroRpcProvider;
             _BtcPayNetworkProvider = btcPayNetworkProvider;
         }
 
@@ -35,12 +41,39 @@ namespace BTCPayServer.Controllers
                 .OfType<MoneroSupportedPaymentMethod>();
             
             var excludeFilters = StoreData.GetStoreBlob().GetExcludedPaymentMethods();
-            
-            
-            
-            
+
+
+            return View(new MoneroLikePaymentMethodListViewModel()
+            {
+                Items = _MoneroLikeConfiguration.MoneroLikeConfigurationItems.Select(pair =>
+                {
+                    var settings = monero.SingleOrDefault(method => method.CryptoCode == pair.Key);
+                    _MoneroRpcProvider.Summaries.TryGetValue(pair.Key, out var summary);
+                    return new MoneroLikePaymentMethodViewModel()
+                    {
+                        Enabled =
+                            settings != null &&
+                            !excludeFilters.Match(new PaymentMethodId(pair.Key, MoneroPaymentType.Instance)),
+                        Summary = summary,
+                        CryptoCode = pair.Key,
+                        AccountIndex = settings?.AccountIndex?? 0
+                    };
+                })
+            });
+
+        }
+
+        public class MoneroLikePaymentMethodListViewModel
+        {
+            public IEnumerable<MoneroLikePaymentMethodViewModel> Items { get; set; }
         }
         
+        public class MoneroLikePaymentMethodViewModel: MoneroSupportedPaymentMethod
+        {
+            public MoneroRPCProvider.MoneroLikeSummary Summary { get; set; }
+            public string CryptoCode { get; set; }
+            public bool Enabled { get; set; } 
+        }
         
     }
 }

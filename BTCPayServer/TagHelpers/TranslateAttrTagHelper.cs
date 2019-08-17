b047b4fd2606f7a9ba5b2455dc4a2eb61cfd8afa
Wouter Samaey <wouter.samaey@storefront.be>
2019-08-17 20:24:35 +0200

    Improved translation of attributes

--- BTCPayServer/TagHelpers/TranslateAttrTagHelper.cs
+++ BTCPayServer/TagHelpers/TranslateAttrTagHelper.cs
@@ -1,27 +1,25 @@
+using System.Globalization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.Extensions.Localization;
 
 namespace BTCPayServer.TagHelpers
 {
-    
-    
-    
-    
     /**
-     * TagHelper that allows you to translate HTML attributes. For example if you want to translate the "title" attribute, use translate-attr="title".
-     * If you want to translate multiple attributes in 1 tag, you can enter more than 1 attribute by separating them with a comma, like this: translate-attr="title,alt"
+     * TagHelper that allows you to translate HTML attributes. For example if you want to translate the "title" attribute, use translate-attr="title:key-for-title".
+     * If you want to translate multiple attributes in 1 tag, you can enter more than 1 attribute by separating them with a comma, like this: translate-attr="title:key-for-title,alt:key-for-alt"
      */
     [HtmlTargetElement(Attributes = "translate-attr")]
     public class TranslateAttrTagHelper : TagHelper
     {
-        
+        private readonly IStringLocalizer<TranslateAttrTagHelper> _localizer;
+
+
         // Can be passed via <xxx translate-attr="..." />
         // PascalCase gets translated into kebab-case.
         public string TranslateAttr { get; set; }
 
-        private readonly IStringLocalizer<TranslateAttrTagHelper> _localizer;
-        
+
         public TranslateAttrTagHelper(IStringLocalizer<TranslateAttrTagHelper> localizer)
         {
             _localizer = localizer;
@@ -42,22 +40,33 @@ namespace BTCPayServer.TagHelpers
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            var attrsToTranslate = TranslateAttr.Split(',');
-            foreach (var attrToTranslate in attrsToTranslate)
+            CultureInfo currentCulture = CultureInfo.CurrentCulture;
+            if (!currentCulture.TwoLetterISOLanguageName.Equals("en"))
             {
-                var attrs = output.Attributes;
-
-                for (int i = 0; i < attrs.Count; i++)
+                var attrsToTranslate = TranslateAttr.Split(',');
+                foreach (var attrAndKeyToTranslate in attrsToTranslate)
                 {
-                    TagHelperAttribute attr = attrs[i];
-                    if (attr.Name.Equals(attrToTranslate))
+                    var parts = attrAndKeyToTranslate.Trim().Split(":");
+                    if (parts.Length == 2)
                     {
-                        var translatedAttr = _localizer[attr.Value.ToString()];
-                        output.Attributes.SetAttribute(attrToTranslate, translatedAttr);
+                        var attrToTranslate = parts[0];
+                        var key = parts[1];
+
+                        var attrs = output.Attributes;
+
+                        for (int i = 0; i < attrs.Count; i++)
+                        {
+                            TagHelperAttribute attr = attrs[i];
+                            if (attr.Name.Equals(attrToTranslate))
+                            {
+                                var originalContent = attr.Value.ToString();
+                                var translation = _localizer[key];
+                                output.Attributes.SetAttribute(attrToTranslate, translation);
+                            }
+                        }
                     }
                 }
             }
         }
-
     }
 }

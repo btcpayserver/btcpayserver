0537ef1ec53e4024aa0f2e8684948ef8cb542d89
Wouter Samaey <wouter.samaey@storefront.be>
2019-08-17 20:24:44 +0200

    Only translate non-English cultures

--- BTCPayServer/TagHelpers/TranslateTagHelper.cs
+++ BTCPayServer/TagHelpers/TranslateTagHelper.cs
@@ -1,4 +1,7 @@
+using System.Globalization;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.Extensions.Localization;
 
@@ -7,18 +10,20 @@ namespace BTCPayServer.TagHelpers
     [HtmlTargetElement(Attributes = "translate")]
     public class TranslateTagHelper : TagHelper
     {
-
         private readonly IStringLocalizer<TranslateTagHelper> _localizer;
 
 
         // Can be passed via <xxx translate="..." />
         // PascalCase gets translated into kebab-case.
         public string Translate { get; set; }
-        
+
+
         public TranslateTagHelper(IStringLocalizer<TranslateTagHelper> localizer)
         {
             _localizer = localizer;
         }
+
+
         public override int Order
         {
             get
@@ -42,16 +47,16 @@ namespace BTCPayServer.TagHelpers
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            var originalContent = output.Content.IsModified
-                ? output.Content.GetContent()
-                : (await output.GetChildContentAsync()).GetContent();
+            CultureInfo currentCulture = CultureInfo.CurrentCulture;
+            if (!currentCulture.TwoLetterISOLanguageName.Equals("en"))
+            {
+                var originalContent = output.Content.IsModified
+                    ? output.Content.GetContent()
+                    : (await output.GetChildContentAsync()).GetContent();
 
-            var newContent = _localizer[Translate];
-            
-            
-
-            output.Content.SetHtmlContent(newContent);
+                var newContent = _localizer[Translate];
+                output.Content.SetHtmlContent(newContent);
+            }
         }
-        
     }
 }

1067d77910153be90fa4e74687de608f80d469f7
Wouter Samaey <wouter.samaey@storefront.be>
2019-08-17 19:44:43 +0200

    Switching locales now works with a cookie

--- BTCPayServer/Controllers/LocaleController.cs
+++ BTCPayServer/Controllers/LocaleController.cs
@@ -7,17 +7,17 @@ namespace BTCPayServer.Controllers
 {
     public class LocaleController : Controller
     {
-
-        
         [HttpPost]
         [Route("setlocale")]
         public IActionResult SetLocaleInCookie(string culture, string returnUrl)
         {
             CookieOptions option = new CookieOptions();
             option.Expires = DateTime.Now.AddYears(1);
+            // TODO the path could be set better, dynamically. Not sure how to do that.
             option.Path = "/";
 
-            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, culture, option);
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, "c=" + culture + "|uic=" + culture,
+                option);
 
             return LocalRedirect(returnUrl);
         }

3b6465c9c189a3e8ad035ff5b8846d7de9ac735a
Kukks <evilkukka@gmail.com>
2019-09-30 14:40:09 +0200

    validate xmr endpoint hit before proceeding

--- BTCPayServer/Altcoins/Monero/RPC/MoneroDaemonCallbackController.cs
+++ BTCPayServer/Altcoins/Monero/RPC/MoneroDaemonCallbackController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using BTCPayServer.Altcoins.Monero.Configuration;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BTCPayServer.Altcoins.Monero.RPC
@@ -6,31 +9,46 @@ namespace BTCPayServer.Altcoins.Monero.RPC
     public class MoneroLikeDaemonCallbackController : Controller
     {
         private readonly EventAggregator _eventAggregator;
+        private readonly MoneroLikeConfiguration _MoneroLikeConfiguration;
 
-        public MoneroLikeDaemonCallbackController(EventAggregator eventAggregator)
+        public MoneroLikeDaemonCallbackController(EventAggregator eventAggregator,
+            MoneroLikeConfiguration moneroLikeConfiguration)
         {
             _eventAggregator = eventAggregator;
+            _MoneroLikeConfiguration = moneroLikeConfiguration;
         }
+
         [HttpGet("block")]
         public IActionResult OnBlockNotify(string hash, string cryptoCode)
         {
-            _eventAggregator.Publish(new MoneroEvent()
-            {
-                BlockHash = hash,
-                CryptoCode = cryptoCode.ToUpperInvariant()
-            });
+            var result = Guard(cryptoCode, hash);
+            if (result != null) return result;
+            _eventAggregator.Publish(new MoneroEvent() {BlockHash = hash, CryptoCode = cryptoCode.ToUpperInvariant()});
             return Ok();
         }
+
         [HttpGet("tx")]
         public IActionResult OnTransactionNotify(string hash, string cryptoCode)
         {
+            var result = Guard(cryptoCode, hash);
+            if (result != null) return result;
             _eventAggregator.Publish(new MoneroEvent()
             {
-                TransactionHash = hash,
-                CryptoCode = cryptoCode.ToUpperInvariant()
+                TransactionHash = hash, CryptoCode = cryptoCode.ToUpperInvariant()
             });
             return Ok();
         }
 
+        private IActionResult Guard(string cryptoCode, string hash)
+        {
+            if (string.IsNullOrEmpty(cryptoCode) || string.IsNullOrEmpty(hash) ||
+                !_MoneroLikeConfiguration.MoneroLikeConfigurationItems.Any(pair =>
+                    pair.Key.Equals(cryptoCode, StringComparison.InvariantCultureIgnoreCase)))
+            {
+                return NotFound();
+            }
+
+            return null;
+        }
     }
 }

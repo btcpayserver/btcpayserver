82fd811e7a5a0930686f239709ccca2c56ff74e0
esky33 <djescaba@cloud.com>
2019-11-06 15:51:59 +0000

    Create LNDGRPCServicesViewModel.cs

---
+++ BTCPayServer/Models/ServerViewModels/LNDGRPCServicesViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BTCPayServer.Models.ServerViewModels
+{
+    public class LndGrpcServicesViewModel
+    {
+        public string Host { get; set; }
+        public bool SSL { get; set; }
+        public string Macaroon { get; set; }
+        public string AdminMacaroon { get; set; }
+        public string ReadonlyMacaroon { get; set; }
+        public string InvoiceMacaroon { get; set; }
+        public string CertificateThumbprint { get; set; }
+        [Display(Name = "GRPC SSL Cipher suite (GRPC_SSL_CIPHER_SUITES)")]
+        public string GRPCSSLCipherSuites { get; set; }
+        public string QRCode { get; set; }
+        public string QRCodeLink { get; set; }
+        [Display(Name = "REST Uri")]
+        public string Uri { get; set; }
+        public string ConnectionType { get; internal set; }
+    }
+}

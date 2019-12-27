11affb996892c02968c72b2dd07edba88222342b
Umar Bolatov <bolatovumar@gmail.com>
2019-12-26 16:46:29 -0800

    Clean-up ConfigurationExtensions class

--- BTCPayServer/Configuration/ConfigurationExtensions.cs
+++ BTCPayServer/Configuration/ConfigurationExtensions.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Primitives;
 using NBitcoin;
 
 namespace BTCPayServer.Configuration

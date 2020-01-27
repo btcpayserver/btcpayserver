bb12d37416ddb58def44ced0c3eeb6e14f848050
rockstardev <5191402+rockstardev@users.noreply.github.com>
2020-01-27 04:57:46 -0600

    Displaying sats in a more user-friendly way (space as group separator) (#1306)
    Fix: #1146

--- BTCPayServer/Payments/Lightning/LightningLikePaymentHandler.cs
+++ BTCPayServer/Payments/Lightning/LightningLikePaymentHandler.cs
@@ -180,11 +180,15 @@ namespace BTCPayServer.Payments.Lightning
             model.LightningAmountInSatoshi = storeBlob.LightningAmountInSatoshi;
             if (storeBlob.LightningAmountInSatoshi && model.CryptoCode == "BTC" )
             {
+                var satoshiCulture = new CultureInfo(CultureInfo.InvariantCulture.Name);
+                satoshiCulture.NumberFormat.NumberGroupSeparator = " ";
+
                 model.CryptoCode = "Sats";
-                model.BtcDue = Money.Parse(model.BtcDue).ToUnit(MoneyUnit.Satoshi).ToString(CultureInfo.InvariantCulture);
-                model.BtcPaid =  Money.Parse(model.BtcPaid).ToUnit(MoneyUnit.Satoshi).ToString(CultureInfo.InvariantCulture);
+                model.BtcDue = Money.Parse(model.BtcDue).ToUnit(MoneyUnit.Satoshi).ToString("N0", satoshiCulture);
+                model.BtcPaid =  Money.Parse(model.BtcPaid).ToUnit(MoneyUnit.Satoshi).ToString("N0", satoshiCulture);
+                model.OrderAmount = Money.Parse(model.OrderAmount).ToUnit(MoneyUnit.Satoshi).ToString("N0", satoshiCulture);
+
                 model.NetworkFee = new Money(model.NetworkFee, MoneyUnit.BTC).ToUnit(MoneyUnit.Satoshi);
-                model.OrderAmount = Money.Parse(model.OrderAmount).ToUnit(MoneyUnit.Satoshi).ToString(CultureInfo.InvariantCulture);
             }
         }
         public override string GetCryptoImage(PaymentMethodId paymentMethodId)

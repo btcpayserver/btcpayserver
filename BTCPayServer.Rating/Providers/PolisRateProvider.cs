ed9a0eeb7ed16f39a884948de819d979db363353
Hans <hans@hodl.blue>
2020-01-30 14:06:08 -0800

    Fix Polis Rate Fetcher

--- BTCPayServer.Rating/Providers/PolisRateProvider.cs
+++ BTCPayServer.Rating/Providers/PolisRateProvider.cs
@@ -7,21 +7,20 @@ using Newtonsoft.Json.Linq;
 
 namespace BTCPayServer.Services.Rates
 {
-    public class PolisRateProvider : IRateProvider, IHasExchangeName
+    public class PolisRateProvider : IRateProvider
     {
         private readonly HttpClient _httpClient;
         public PolisRateProvider(HttpClient httpClient)
         {
             _httpClient = httpClient ?? new HttpClient();
         }
-        public string ExchangeName => "polispay";
 
-        public async Task<ExchangeRates> GetRatesAsync(CancellationToken cancellationToken)
+        public async Task<PairRate[]> GetRatesAsync(CancellationToken cancellationToken)
         {
             var response = await _httpClient.GetAsync("https://obol.polispay.com/complex/btc/polis", cancellationToken);
             var jobj = await response.Content.ReadAsAsync<JObject>(cancellationToken);
             var value = jobj["data"].Value<decimal>();
-            return new ExchangeRates(new[] { new ExchangeRate(ExchangeName, new CurrencyPair("POLIS", "BTC"), new BidAsk(value)) });
+            return new[] { new PairRate(new CurrencyPair("POLIS", "BTC"), new BidAsk(value)) };
         }
     }
 }

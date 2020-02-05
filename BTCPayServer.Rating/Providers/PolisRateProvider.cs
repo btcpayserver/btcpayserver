44b642cf90c6eb5e1071f5b5efcbff47f0c537b9
Hans <hans@hodl.blue>
2020-02-05 11:02:17 -0800

    Add small doc to re-trigger testing

--- BTCPayServer.Rating/Providers/PolisRateProvider.cs
+++ BTCPayServer.Rating/Providers/PolisRateProvider.cs
@@ -17,7 +17,7 @@ namespace BTCPayServer.Services.Rates
 
         public async Task<PairRate[]> GetRatesAsync(CancellationToken cancellationToken)
         {
-            var response = await _httpClient.GetAsync("https://obol.polispay.com/complex/btc/polis", cancellationToken);
+            var response = await _httpClient.GetAsync("https://obol.polispay.com/complex/btc/polis", cancellationToken);  //Returns complex rate from BTC to POLIS
             var jobj = await response.Content.ReadAsAsync<JObject>(cancellationToken);
             var value = jobj["data"].Value<decimal>();
             return new[] { new PairRate(new CurrencyPair("BTC", "POLIS"), new BidAsk(value)) };

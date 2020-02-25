ec80787120fc5f82e92ceb5b886cd45d1d40e9f5
Kukks <evilkukka@gmail.com>
2020-02-25 15:33:04 +0100

    fix

--- BTCPayServer/Controllers/StoresController.BTCLike.cs
+++ BTCPayServer/Controllers/StoresController.BTCLike.cs
@@ -142,7 +142,7 @@ namespace BTCPayServer.Controllers
 
         [HttpPost]
         [Route("{storeId}/derivations/{cryptoCode}")]
-        public async Task<IActionResult> AddDerivationScheme(string storeId, [FromBody] DerivationSchemeViewModel vm,
+        public async Task<IActionResult> AddDerivationScheme(string storeId, [FromForm] DerivationSchemeViewModel vm,
             string cryptoCode)
         {
             vm.CryptoCode = cryptoCode;

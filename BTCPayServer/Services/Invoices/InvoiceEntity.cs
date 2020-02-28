f0f05acdfde075213c4f9d0cf4e0ecba13662d39
Kukks <evilkukka@gmail.com>
2020-02-28 10:10:07 +0100

    Let 0 amount invoices pass through and allow email to be set when required but paid
    @ketominer @Askuwheteau @IAskuwheteau

--- BTCPayServer/Services/Invoices/InvoiceEntity.cs
+++ BTCPayServer/Services/Invoices/InvoiceEntity.cs
@@ -888,7 +888,11 @@ namespace BTCPayServer.Services.Invoices
             accounting.Due = Money.Max(accounting.TotalDue - accounting.Paid, Money.Zero);
             accounting.DueUncapped = accounting.TotalDue - accounting.Paid;
             accounting.NetworkFee = accounting.TotalDue - totalDueNoNetworkCost;
-            var minimumTotalDueSatoshi = Math.Max(1.0m, accounting.TotalDue.Satoshi * (1.0m - ((decimal)ParentEntity.PaymentTolerance / 100.0m)));
+            // If the total due is 0, there is no payment tolerance to calculate
+            var minimumTotalDueSatoshi = accounting.TotalDue.Satoshi == 0
+                ? 0
+                : Math.Max(1.0m,
+                    accounting.TotalDue.Satoshi * (1.0m - ((decimal)ParentEntity.PaymentTolerance / 100.0m)));
             accounting.MinimumTotalDue = Money.Satoshis(minimumTotalDueSatoshi);
             return accounting;
         }

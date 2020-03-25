5565d8dae53ebe5ce00a76b16d5280c75f800f2a
nicolas.dorier <nicolas.dorier@gmail.com>
2020-03-26 01:21:16 +0900

    Catch error with bitflyer

--- BTCPayServer.Rating/Providers/BitflyerRateProvider.cs
+++ BTCPayServer.Rating/Providers/BitflyerRateProvider.cs
@@ -21,6 +21,10 @@ namespace BTCPayServer.Services.Rates
         {
             var response = await _httpClient.GetAsync("https://api.bitflyer.jp/v1/ticker", cancellationToken);
             var jobj = await response.Content.ReadAsAsync<JObject>(cancellationToken);
+            if (jobj.Property("error_message")?.Value?.Value<string>() is string err)
+            {
+                throw new Exception($"Error from bitflyer: {err}");
+            }
             var bid = jobj.Property("best_bid").Value.Value<decimal>();
             var ask = jobj.Property("best_ask").Value.Value<decimal>();
             var rates = new PairRate[1];

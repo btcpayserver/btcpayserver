fe451525296c6e7d4b2b69eff4a96197cf793c29
Dennis Reimann <mail@dennisreimann.de>
2020-04-07 18:13:47 +0200

    Add bitcoin generate shell script
    The shell equivalent to the existing powershell script.

---
+++ BTCPayServer.Tests/docker-bitcoin-generate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bitcoind_container_id="$(docker ps -q --filter label=com.docker.compose.project=btcpayservertests --filter label=com.docker.compose.service=bitcoind)"
+address=$(docker exec -ti $bitcoind_container_id bitcoin-cli -datadir="/data" getnewaddress)
+docker exec -ti $bitcoind_container_id bitcoin-cli -datadir="/data" generatetoaddress "$@" "$address"

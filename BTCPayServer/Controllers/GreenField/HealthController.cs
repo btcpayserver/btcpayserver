a6a56e4791afc3ab42836b8663cbc3b6cc11c211
Kukks <evilkukka@gmail.com>
2020-04-21 17:09:17 +0200

    fix controller

--- BTCPayServer/Controllers/GreenField/HealthController.cs
+++ BTCPayServer/Controllers/GreenField/HealthController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BTCPayServer.Controllers.GreenField
 {
-    [ApiController]
+    [Controller]
     public class HealthController : ControllerBase
     {
         [AllowAnonymous]

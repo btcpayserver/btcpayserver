e9c809bbffc39f4e08a76c98a4d822facb481293
Remco <4744907+RemcoMi@users.noreply.github.com>
2020-04-25 22:42:28 +0200

    Typo fix

--- BTCPayServer/Controllers/WalletsController.PSBT.cs
+++ BTCPayServer/Controllers/WalletsController.PSBT.cs
@@ -346,7 +346,7 @@ namespace BTCPayServer.Controllers
                                 Html =
                                     $"This transaction has been coordinated between the receiver and you to create a <a href='https://en.bitcoin.it/wiki/PayJoin' target='_blank'>payjoin transaction</a> by adding inputs from the receiver.<br/>" +
                                     $"The amount being sent may appear higher but is in fact almost same.<br/><br/>" +
-                                    $"If you cancel refuse to sign this transaction, the payment will proceed without payjoin"
+                                    $"If you cancel or refuse to sign this transaction, the payment will proceed without payjoin"
                             });
                             return ViewVault(walletId, proposedPayjoin, vm.PayJoinEndpointUrl, psbt);
                         }

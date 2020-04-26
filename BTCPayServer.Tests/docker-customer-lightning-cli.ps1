7e0f9e1d28edbcbd6ab444521f23bbda7773a582
nicolas.dorier <nicolas.dorier@gmail.com>
2020-04-27 03:59:09 +0900

    Fix docker-customer-lightning-cli.ps1 pay doesn't work (Fix #1509)

--- BTCPayServer.Tests/docker-customer-lightning-cli.ps1
+++ BTCPayServer.Tests/docker-customer-lightning-cli.ps1
@@ -1,2 +1,2 @@
 $customer_lightning_container_id=$(docker ps -q --filter label=com.docker.compose.project=btcpayservertests --filter label=com.docker.compose.service=customer_lightningd)
-docker exec -ti $customer_lightning_container_id lightning-cli $args
+docker exec -ti $customer_lightning_container_id lightning-cli --rpc-file=/root/.lightning/lightning-rpc $args

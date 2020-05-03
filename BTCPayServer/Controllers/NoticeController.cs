1ab016d21ebc9199b16a477af93b2b503f7df9cf
rockstardev <rockstardev@users.noreply.github.com>
2020-05-03 17:42:01 -0600

    Foundation for notices grid

---
+++ BTCPayServer/Controllers/NoticeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTCPayServer.Filters;
+using BTCPayServer.Models.NoticeViewModels;
+using BTCPayServer.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTCPayServer.Controllers
+{
+    [BitpayAPIConstraint(false)]
+    public class NoticeController : Controller
+    {
+        [HttpGet]
+        [Route("notices")]
+        [Authorize(AuthenticationSchemes = AuthenticationSchemes.Cookie)]
+        public async Task<IActionResult> Index(int skip = 0, int count = 50, int timezoneOffset = 0)
+        {
+            var model = new IndexViewModel()
+            {
+                Items = new List<IndexViewModel.NoticeDataHolder>
+                {
+                    new IndexViewModel.NoticeDataHolder
+                    {
+                        Id = 1,
+                        Body = "Hello world",
+                        Level = "Admin",
+                        Created = new DateTime(2020, 01, 05)
+                    },
+                    new IndexViewModel.NoticeDataHolder
+                    {
+                        Id = 1,
+                        Body = "Notification number 2",
+                        Level = "Store",
+                        Created = new DateTime(2020, 02, 07)
+                    },
+                    new IndexViewModel.NoticeDataHolder
+                    {
+                        Id = 1,
+                        Body = "Up unpacked friendly ecstatic so possible humoured do. Ample end might folly quiet one set spoke her. We no am former valley assure. Four need spot ye said we find mile.",
+                        Level = "User",
+                        Created = new DateTime(2020, 03, 01)
+                    },
+                    new IndexViewModel.NoticeDataHolder
+                    {
+                        Id = 1,
+                        Body = "New version of BTCPayServer is detected.",
+                        Level = "Admin",
+                        Created = new DateTime(2020, 06, 22)
+                    },
+                    new IndexViewModel.NoticeDataHolder
+                    {
+                        Id = 1,
+                        Body = "The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.",
+                        Level = "Store",
+                        Created = new DateTime(2020, 04, 17)
+                    },
+                    new IndexViewModel.NoticeDataHolder
+                    {
+                        Id = 1,
+                        Body = "New invoice paid",
+                        Level = "User",
+                        Created = new DateTime(2020, 05, 07)
+                    }
+                }
+            };
+            model.Items = model.Items.OrderByDescending(a => a.Created).ToList();
+            return View(model);
+        }
+    }
+}
---
+++ BTCPayServer/Models/NoticeViewModels/IndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCPayServer.Models.NoticeViewModels
+{
+    public class IndexViewModel
+    {
+        public List<NoticeDataHolder> Items { get; set; }
+
+        public class NoticeDataHolder
+        {
+            public int Id { get; set; }
+            public string Body { get; set; }
+            public string Level { get; set; }
+            public DateTime Created { get; set; }
+        }
+    }
+}

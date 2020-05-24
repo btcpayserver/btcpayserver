80563de5874a430aa2129a4f3c655e2f35bd9207
Andrew Camilleri <evilkukka@gmail.com>
2020-05-24 12:11:26 +0200

    Add support for Bitpay invoice create property "paymentCurrencies" (#1589)

--- BTCPayServer/Models/CreateInvoiceRequest.cs
+++ BTCPayServer/Models/CreateInvoiceRequest.cs
@@ -82,5 +82,9 @@ namespace BTCPayServer.Models
 
         [JsonProperty(PropertyName = "redirectAutomatically", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public bool? RedirectAutomatically { get; set; }
+        
+        //Bitpay compatibility: create invoice in btcpay uses this instead of supportedTransactionCurrencies
+        [JsonProperty(PropertyName = "paymentCurrencies", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public IEnumerable<string> PaymentCurrencies { get; set; }
     }
 }

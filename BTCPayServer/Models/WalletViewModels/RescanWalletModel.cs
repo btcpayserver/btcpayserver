69b9acf669eb1367f118c205389396ff98aa2240
Dennis Reimann <mail@dennisreimann.de>
2020-07-23 14:48:58 +0200

    Improve wallet views
    CSS fix

--- BTCPayServer/Models/WalletViewModels/RescanWalletModel.cs
+++ BTCPayServer/Models/WalletViewModels/RescanWalletModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using NBXplorer.Models;
 
@@ -12,11 +13,14 @@ namespace BTCPayServer.Models.WalletViewModels
         public bool Ok => IsServerAdmin && IsSupportedByCurrency && IsFullySync;
 
         [Range(1000, 10_000)]
+        [DisplayName("Batch size")]
         public int BatchSize { get; set; } = 3000;
         [Range(0, 10_000_000)]
+        [DisplayName("Starting index")]
         public int StartingIndex { get; set; } = 0;
 
         [Range(100, 100000)]
+        [DisplayName("Gap limit")]
         public int GapLimit { get; set; } = 10000;
 
         public int? Progress { get; set; }

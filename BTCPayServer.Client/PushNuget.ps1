3c3e2f80da7dc7a1a3f63bdc2da16f7527c8b9d0
nicolas.dorier <nicolas.dorier@gmail.com>
2020-09-01 11:51:11 +0900

    Push only one tag

--- BTCPayServer.Client/PushNuget.ps1
+++ BTCPayServer.Client/PushNuget.ps1
@@ -4,4 +4,4 @@ $package=(ls .\bin\Release\*.nupkg).FullName
 dotnet nuget push $package --source "https://api.nuget.org/v3/index.json"
 $ver = ((ls .\bin\release\*.nupkg)[0].Name -replace '.*(\d+\.\d+\.\d+)\.nupkg','$1')
 git tag -a "BTCPayServer.Client/v$ver" -m "BTCPayServer.Client/$ver"
-git push --tags
+git push origin "BTCPayServer.Client/v$ver"

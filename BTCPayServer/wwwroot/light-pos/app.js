a8c6f693e6785688c539e1c3c26758346d309bd7
Kukks <evilkukka@gmail.com>
2020-09-14 17:00:01 +0200

    review fixes

--- BTCPayServer/wwwroot/light-pos/app.js
+++ BTCPayServer/wwwroot/light-pos/app.js
@@ -1,5 +1,4 @@
 var app = null;
-var eventAggregator = new Vue();
 
 function addLoadEvent(func) {
     var oldonload = window.onload;
@@ -15,27 +14,38 @@ function addLoadEvent(func) {
     }
 }
 addLoadEvent(function (ev) {
-    Vue.component('keypad', {
-        props: ['keys'],
-        template: '#keypad-template'
-    });
 
-    Vue.component('controls', {
-        props: ['keys'],
-        template: '#controls-template',
-        methods: {
-            onClickClear: function() {
-                eventAggregator.$emit("clearClicked");
-            }
-        }
-    });
 
-    Vue.component('display', {
-        props: ['value', 'currency'],
-        template: '#display-template',
-        data: function() {
+    app = new Vue({
+        el: '#app',
+        data: function(){
             return {
-                fontSize: 80
+                srvModel: window.srvModel,
+                payTotal: '0',
+                payTotalNumeric: 0,
+                fontSize: 80,
+                keys: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0', 'C']
+            }
+        },
+        computed: {
+            Currency: function(){
+                return this.srvModel.Currency.toUpperCase();
+            },
+        },
+        watch: {
+            payTotal: function(val) {
+                var self = this;
+
+                // This must be timeouted because the updated width is not available yet
+                this.$nextTick(function(){
+                    var displayWidth = self.getWidth(self.$refs.display),
+                        amountWidth = self.getWidth(self.$refs.amount);
+
+                    if (displayWidth <= amountWidth) {
+                        var gamma = displayWidth / amountWidth || 0;
+                        self.fontSize = Math.floor(self.fontSize * gamma);
+                    }
+                });
             }
         },
         methods: {
@@ -46,70 +56,14 @@ addLoadEvent(function (ev) {
                     padR = parseFloat(styles.paddingRight);
 
                 return width - padL - padR;
-            }
-        },
-        watch: {
-            value: function(val) {
-                var self = this;
-
-                // This must be timeouted because the updated width is not available yet
-                setTimeout(function(){
-                    var displayWidth = self.getWidth(self.$refs.display),
-                        amountWidth = self.getWidth(self.$refs.amount);
-
-                    if (displayWidth <= amountWidth) {
-                        var gamma = displayWidth / amountWidth || 0;
-                        self.fontSize = Math.floor(self.fontSize * gamma);
-                    }
-                }, 10);
-            }
-        }
-    });
-
-    Vue.component('keypad-button', {
-        props: ['text', 'newLine'],
-        template: '#keypad-button-template',
-        computed: {
-            classObject: function () {
-              return {
-                    'btn btn-primary': (isNaN(this.text) === false) || this.text === '.',
-                    'btn btn-dark': isNaN(this.text) && this.text !== '.',
-              }
-            }
-        },
-        methods: {
-            onClickButton: function() {
-                eventAggregator.$emit("buttonClicked", this.text);
-            }
-        }
-    });
-    
-    app = new Vue({
-        el: '#app',
-        data: function(){
-            return {
-                srvModel: window.srvModel,
-                payTotal: '0',
-                payTotalNumeric: 0,
-                keys: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0', 'C']
-            }
-        },
-        computed: {
-            Currency: function(){
-                return this.srvModel.Currency.toUpperCase();
             },
-        },
-        methods: {
             clearTotal: function() {
                 this.payTotal = '0';
                 this.payTotalNumeric = 0;
-            }
-        },
-        mounted: function () {
-            var self = this;
-
-            eventAggregator.$on("buttonClicked", function(key) {
-                var payTotal = self.payTotal;
+            },
+            buttonClicked: function(key) {
+                
+                var payTotal = this.payTotal;
 
                 if (key === 'C') {
                     payTotal = payTotal.substring(0, payTotal.length - 1);
@@ -124,15 +78,18 @@ addLoadEvent(function (ev) {
                         payTotal = '';
                     }
                     payTotal += key;
+
+                    var divsibility = this.srvModel.currencyInfo.divisibility;
+                    var decimalIndex = payTotal.indexOf('.')
+                    if (decimalIndex !== -1 && (payTotal.length - decimalIndex-1  > divsibility)) {
+                        payTotal= payTotal.replace(".","");
+                        payTotal =  payTotal.substr(0,payTotal.length - divsibility ) + "." + payTotal.substr(payTotal.length - divsibility);
+                    }
                 }
 
-                self.payTotal = payTotal;
-                self.payTotalNumeric = parseFloat(payTotal);
-            });
-
-            eventAggregator.$on("clearClicked", function(key) {
-                self.clearTotal();
-            });
+                this.payTotal = payTotal;
+                this.payTotalNumeric = parseFloat(payTotal);
+            }
         }
     });
 });

7093feed957227ed12474cc64f985dd952863ccc
Civa <44102586+civa@users.noreply.github.com>
2020-09-17 16:40:45 +0300

    Update EmailSenderExtensions.cs

--- BTCPayServer/Extensions/EmailSenderExtensions.cs
+++ BTCPayServer/Extensions/EmailSenderExtensions.cs
@@ -8,7 +8,7 @@ namespace BTCPayServer.Services
         public static void SendEmailConfirmation(this IEmailSender emailSender, string email, string link)
         {
             emailSender.SendEmail(email, "Confirm your email",
-                $"Hello <br> Thank you for registering your Mycryptopay Account, Please click <a href='{HtmlEncoder.Default.Encode(link)}'>here</a> to verify your account . <br> Nice to have you onboard");
+                $"Hello <br><br> Thank you for registering your Mycryptopay Account,<br><br> Please click <a href='{HtmlEncoder.Default.Encode(link)}'>here</a> to verify your account . <br> <br><br>Nice to have you onboard<br>MyCryptoPay Team");
         }
     }
 }

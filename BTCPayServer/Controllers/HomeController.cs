282d0abb62ef2f10191157093a2465be660e76f1
Nicolas Dorier <nicolas.dorier@gmail.com>
2020-12-12 15:17:44 +0900

    Remove bitpay translator (#2130)

--- BTCPayServer/Controllers/HomeController.cs
+++ BTCPayServer/Controllers/HomeController.cs
@@ -115,11 +115,6 @@ namespace BTCPayServer.Controllers
                 return RedirectToAction(nameof(AccountController.Login), "Account");
         }
 
-        [Route("translate")]
-        public IActionResult BitpayTranslator()
-        {
-            return View(new BitpayTranslatorViewModel());
-        }
         [Route("misc/lang")]
         [Authorize(AuthenticationSchemes = AuthenticationSchemes.Cookie)]
         public IActionResult Languages()
@@ -150,58 +145,6 @@ namespace BTCPayServer.Controllers
         public IActionResult SwaggerDocs()
         {
             return View();
-        }
-
-        [HttpPost]
-        [Route("translate")]
-        public async Task<IActionResult> BitpayTranslator(BitpayTranslatorViewModel vm)
-        {
-            if (!ModelState.IsValid)
-                return View(vm);
-            vm.BitpayLink = vm.BitpayLink ?? string.Empty;
-            vm.BitpayLink = vm.BitpayLink.Trim();
-            if (!vm.BitpayLink.StartsWith("bitcoin:", StringComparison.OrdinalIgnoreCase))
-            {
-                var invoiceId = vm.BitpayLink.Substring(vm.BitpayLink.LastIndexOf("=", StringComparison.OrdinalIgnoreCase) + 1);
-                vm.BitpayLink = $"bitcoin:?r=https://bitpay.com/i/{invoiceId}";
-            }
-
-            try
-            {
-                BitcoinUrlBuilder urlBuilder = new BitcoinUrlBuilder(vm.BitpayLink, Network.Main);
-#pragma warning disable CS0618 // Type or member is obsolete
-                if (!urlBuilder.PaymentRequestUrl.DnsSafeHost.EndsWith("bitpay.com", StringComparison.OrdinalIgnoreCase))
-                {
-                    throw new Exception("This tool only work with bitpay");
-                }
-
-                var client = HttpClientFactory.CreateClient();
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, urlBuilder.PaymentRequestUrl);
-#pragma warning restore CS0618 // Type or member is obsolete
-                request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/payment-request"));
-                var result = await client.SendAsync(request);
-                // {"network":"main","currency":"BTC","requiredFeeRate":29.834,"outputs":[{"amount":255900,"address":"1PgPo5d4swD6pKfCgoXtoW61zqTfX9H7tj"}],"time":"2018-12-03T14:39:47.162Z","expires":"2018-12-03T14:54:47.162Z","memo":"Payment request for BitPay invoice HHfG8cprRMzZG6MErCqbjv for merchant VULTR Holdings LLC","paymentUrl":"https://bitpay.com/i/HHfG8cprRMzZG6MErCqbjv","paymentId":"HHfG8cprRMzZG6MErCqbjv"}
-                var str = await result.Content.ReadAsStringAsync();
-                try
-                {
-                    var jobj = JObject.Parse(str);
-                    vm.Address = ((JArray)jobj["outputs"])[0]["address"].Value<string>();
-                    var amount = Money.Satoshis(((JArray)jobj["outputs"])[0]["amount"].Value<long>());
-                    vm.Amount = amount.ToString();
-                    vm.BitcoinUri = $"bitcoin:{vm.Address}?amount={amount.ToString()}";
-                }
-                catch (JsonReaderException)
-                {
-                    ModelState.AddModelError(nameof(vm.BitpayLink), $"Invalid or expired bitpay invoice");
-                    return View(vm);
-                }
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError(nameof(vm.BitpayLink), $"Error while requesting {ex.Message}");
-                return View(vm);
-            }
-            return View(vm);
         }
 
         [Route("recovery-seed-backup")]

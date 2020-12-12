a294ad41cb40ff744b0f7e36cd1854fa505a612c
Umar Bolatov <bolatovumar@gmail.com>
2020-12-11 22:23:59 -0800

    Ensure campaign status is shown correctly in minimal crowdfund app (#2062)

--- BTCPayServer/Models/AppViewModels/ViewCrowdfundViewModel.cs
+++ BTCPayServer/Models/AppViewModels/ViewCrowdfundViewModel.cs
@@ -70,7 +70,7 @@ namespace BTCPayServer.Models.AppViewModels
 
         public bool Started => !StartDate.HasValue || DateTime.Now.ToUniversalTime() > StartDate;
 
-        public bool Ended => !EndDate.HasValue || DateTime.Now.ToUniversalTime() > EndDate;
+        public bool Ended => EndDate.HasValue && DateTime.Now.ToUniversalTime() > EndDate;
         public bool DisplayPerksRanking { get; set; }
         public bool Enabled { get; set; }
         public string ResetEvery { get; set; }

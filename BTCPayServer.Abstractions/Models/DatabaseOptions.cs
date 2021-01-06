30c234f6ecaa2c57bf9ef08f58168eafe4396854
Kukks <evilkukka@gmail.com>
2021-01-06 09:49:18 +0100

    Move COnfigure logic for Options to the Startup

--- BTCPayServer.Abstractions/Models/DatabaseOptions.cs
+++ BTCPayServer.Abstractions/Models/DatabaseOptions.cs
@@ -1,45 +1,8 @@
-using System;
-using System.IO;
-using BTCPayServer.Configuration;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
-
 namespace BTCPayServer.Abstractions.Models
 {
     public class DatabaseOptions
     {
         public DatabaseType DatabaseType { get; set; }
         public string ConnectionString { get; set; }
-
-        public void Configure(IConfiguration configuration, IOptions<DataDirectories> dataDirectories)
-        {
-            var postgresConnectionString = configuration["postgres"];
-            var mySQLConnectionString = configuration["mysql"];
-            var sqliteFileName = configuration["sqlitefile"];
-
-            if (!string.IsNullOrEmpty(postgresConnectionString))
-            {
-                DatabaseType = DatabaseType.Postgres;
-                ConnectionString = postgresConnectionString;
-            }
-            else if (!string.IsNullOrEmpty(mySQLConnectionString))
-            {
-                DatabaseType = DatabaseType.MySQL;
-                ConnectionString = mySQLConnectionString;
-            }
-            else if (!string.IsNullOrEmpty(sqliteFileName))
-            {
-                var connStr = "Data Source=" + (Path.IsPathRooted(sqliteFileName)
-                    ? sqliteFileName
-                    : Path.Combine(dataDirectories.Value.DataDir, sqliteFileName));
-
-                DatabaseType = DatabaseType.Sqlite;
-                ConnectionString = sqliteFileName;
-            }
-            else
-            {
-                throw new InvalidOperationException("No database option was configured.");
-            }
-        }
     }
 }
--- BTCPayServer/Hosting/BTCPayServerServices.cs
+++ BTCPayServer/Hosting/BTCPayServerServices.cs
@@ -11,6 +11,7 @@ using BTCPayServer.Configuration;
 using BTCPayServer.Controllers;
 using BTCPayServer.Data;
 using BTCPayServer.HostedServices;
+using BTCPayServer.Lightning;
 using BTCPayServer.Logging;
 using BTCPayServer.PaymentRequest;
 using BTCPayServer.Payments;
@@ -128,22 +129,116 @@ namespace BTCPayServer.Hosting
             services.AddOptions<DatabaseOptions>().Configure<IOptions<DataDirectories>>(
                 (options, datadirs) =>
                 {
-                    options.Configure(configuration, datadirs);
+                    var postgresConnectionString = configuration["postgres"];
+                    var mySQLConnectionString = configuration["mysql"];
+                    var sqliteFileName = configuration["sqlitefile"];
+
+                    if (!string.IsNullOrEmpty(postgresConnectionString))
+                    {
+                        options.DatabaseType = DatabaseType.Postgres;
+                        options.ConnectionString = postgresConnectionString;
+                    }
+                    else if (!string.IsNullOrEmpty(mySQLConnectionString))
+                    {
+                        options.DatabaseType = DatabaseType.MySQL;
+                        options.ConnectionString = mySQLConnectionString;
+                    }
+                    else if (!string.IsNullOrEmpty(sqliteFileName))
+                    {
+                        var connStr = "Data Source=" + (Path.IsPathRooted(sqliteFileName)
+                            ? sqliteFileName
+                            : Path.Combine(datadirs.Value.DataDir, sqliteFileName));
+
+                        options.DatabaseType = DatabaseType.Sqlite;
+                        options.ConnectionString = sqliteFileName;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("No database option was configured.");
+                    }
                 });
             services.AddOptions<NBXplorerOptions>().Configure<BTCPayNetworkProvider>(
                 (options, btcPayNetworkProvider) =>
                 {
-                    options.Configure(configuration, btcPayNetworkProvider);
+                    foreach (BTCPayNetwork btcPayNetwork in btcPayNetworkProvider.GetAll().OfType<BTCPayNetwork>())
+                    {
+                        NBXplorerConnectionSetting setting =
+                            new NBXplorerConnectionSetting
+                            {
+                                CryptoCode = btcPayNetwork.CryptoCode,
+                                ExplorerUri = configuration.GetOrDefault<Uri>(
+                                    $"{btcPayNetwork.CryptoCode}.explorer.url",
+                                    btcPayNetwork.NBXplorerNetwork.DefaultSettings.DefaultUrl),
+                                CookieFile = configuration.GetOrDefault<string>(
+                                    $"{btcPayNetwork.CryptoCode}.explorer.cookiefile",
+                                    btcPayNetwork.NBXplorerNetwork.DefaultSettings.DefaultCookieFile)
+                            };
+                        options.NBXplorerConnectionSettings.Add(setting);
+                    }
                 });
             services.AddOptions<LightningNetworkOptions>().Configure<BTCPayNetworkProvider>(
                 (options, btcPayNetworkProvider) =>
                 {
-                    options.Configure(configuration, btcPayNetworkProvider);
+                    foreach (var net in btcPayNetworkProvider.GetAll().OfType<BTCPayNetwork>())
+                    {
+                        var lightning = configuration.GetOrDefault<string>($"{net.CryptoCode}.lightning", string.Empty);
+                        if (lightning.Length != 0)
+                        {
+                            if (!LightningConnectionString.TryParse(lightning, true, out var connectionString,
+                                out var error))
+                            {
+                                Logs.Configuration.LogWarning($"Invalid setting {net.CryptoCode}.lightning, " +
+                                                              Environment.NewLine +
+                                                              $"If you have a c-lightning server use: 'type=clightning;server=/root/.lightning/lightning-rpc', " +
+                                                              Environment.NewLine +
+                                                              $"If you have a lightning charge server: 'type=charge;server=https://charge.example.com;api-token=yourapitoken'" +
+                                                              Environment.NewLine +
+                                                              $"If you have a lnd server: 'type=lnd-rest;server=https://lnd:lnd@lnd.example.com;macaroon=abf239...;certthumbprint=2abdf302...'" +
+                                                              Environment.NewLine +
+                                                              $"              lnd server: 'type=lnd-rest;server=https://lnd:lnd@lnd.example.com;macaroonfilepath=/root/.lnd/admin.macaroon;certthumbprint=2abdf302...'" +
+                                                              Environment.NewLine +
+                                                              $"If you have an eclair server: 'type=eclair;server=http://eclair.com:4570;password=eclairpassword;bitcoin-host=bitcoind:37393;bitcoin-auth=bitcoinrpcuser:bitcoinrpcpassword" +
+                                                              Environment.NewLine +
+                                                              $"               eclair server: 'type=eclair;server=http://eclair.com:4570;password=eclairpassword;bitcoin-host=bitcoind:37393" +
+                                                              Environment.NewLine +
+                                                              $"Error: {error}" + Environment.NewLine +
+                                                              "This service will not be exposed through BTCPay Server");
+                            }
+                            else
+                            {
+                                if (connectionString.IsLegacy)
+                                {
+                                    Logs.Configuration.LogWarning(
+                                        $"Setting {net.CryptoCode}.lightning is a deprecated format, it will work now, but please replace it for future versions with '{connectionString.ToString()}'");
+                                }
+                                options.InternalLightningByCryptoCode.Add(net.CryptoCode, connectionString);
+                            }
+                        }
+                    }
                 });
             services.AddOptions<ExternalServicesOptions>().Configure<BTCPayNetworkProvider>(
                 (options, btcPayNetworkProvider) =>
                 {
-                    options.Configure(configuration, btcPayNetworkProvider);
+                    foreach (var net in btcPayNetworkProvider.GetAll().OfType<BTCPayNetwork>())
+                    {
+                        options.ExternalServices.Load(net.CryptoCode, configuration);
+                    }
+
+                    options.ExternalServices.LoadNonCryptoServices(configuration);
+
+                    var services = configuration.GetOrDefault<string>("externalservices", null);
+                    if (services != null)
+                    {
+                        foreach (var service in services.Split(new[] {';', ','}, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(p => (p, SeparatorIndex: p.IndexOf(':', StringComparison.OrdinalIgnoreCase)))
+                            .Where(p => p.SeparatorIndex != -1)
+                            .Select(p => (Name: p.p.Substring(0, p.SeparatorIndex),
+                                Link: p.p.Substring(p.SeparatorIndex + 1))))
+                        {
+                            if (Uri.TryCreate(service.Link, UriKind.RelativeOrAbsolute, out var uri))
+                                options.OtherExternalServices.AddOrReplace(service.Name, uri);
+                        }
+                    }
                 });
             services.TryAddSingleton(o => configuration.ConfigureNetworkProvider());
 

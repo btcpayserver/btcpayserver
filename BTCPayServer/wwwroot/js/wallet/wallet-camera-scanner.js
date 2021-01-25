4638f781f93adade92f580033a5d9bcfeff65ea8
Dennis Reimann <mail@dennisreimann.de>
2021-01-25 14:11:11 +0100

    Fix JS error

--- BTCPayServer/wwwroot/js/wallet/wallet-camera-scanner.js
+++ BTCPayServer/wwwroot/js/wallet/wallet-camera-scanner.js
@@ -1,6 +1,8 @@
 $(function () {
-    initCameraScanningApp("Scan address/ payment link", function(data){
-        $("#BIP21").val(data);
-        $("form").submit();
-    },"scanModal");
+    if (typeof(initCameraScanningApp) === "function") {
+        initCameraScanningApp("Scan address/ payment link", function(data) {
+            $("#BIP21").val(data);
+            $("form").submit();
+        },"scanModal");
+    }
 });

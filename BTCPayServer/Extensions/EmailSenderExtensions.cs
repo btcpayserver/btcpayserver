b077f05a8760160596f0ae9472dd30dfd2d463df
Salie Hendricks <saliehendricks@gmail.com>
2021-01-26 12:21:04 +0200

    Compacted the html and inline styles and removed logo

--- BTCPayServer/Extensions/EmailSenderExtensions.cs
+++ BTCPayServer/Extensions/EmailSenderExtensions.cs
@@ -5,44 +5,14 @@ namespace BTCPayServer.Services
 {
     public static class EmailSenderExtensions
     {
-        private static string EMAIL_STYLE = @"<style>
-        body {
-            font-family: Open Sans, Helvetica Neue,Arial,sans-serif;
-            font-color: #292929;
-        }
-        .btn-primary {
-            color: #fff;
-            background-color: #51b13e;
-            border-color: #51b13e;
-        }
-        .btn {
-            min-width: 2em;
-            min-height: 20px;
-            text-decoration-line: none;
-            cursor: pointer;
-            display: inline-block;
-            font-weight: 400;
-            color: #fff;
-            text-align: center;
-            vertical-align: middle;
-            user-select: none;
-            background-color: #51b13e;
-            border: 1px solid transparent;
-            padding: 0.375rem 0.75rem;
-            font-size: 1rem;
-            line-height: 1.5;
-            border-radius: 0.25rem;
-            transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-        }
-        </style>";
-        private static string BTCPAYSERVER_LOGO = @"<h2><img src='/img/btcpay-logo.svg' alt='BTCPay Server' style='height: 1.2em; min-width: 0.5em; margin-right: .8rem; vertical-align: top;'>BTCPay Server</h2>";
-        //TODO Base64 the logo so that there is no dependency on the logo.
-        private static string EMAIL_BUTTON = "<a href='{button_link}' type='submit' class='btn btn-primary'>{button_description}</a>";
+        private static string BODY_STYLE = "font-family: Open Sans, Helvetica Neue,Arial,sans-serif; font-color: #292929;";
+        private static string HEADER_HTML = "<h1 style='font-size:1.2rem'>BTCPay Server</h1><br/>";
+        private static string BUTTON_HTML = "<a href='{button_link}' type='submit' style='min-width: 2em;min-height: 20px;text-decoration-line: none;cursor: pointer;display: inline-block;font-weight: 400;color: #fff;text-align: center;vertical-align: middle;user-select: none;background-color: #51b13e;border-color: #51b13e;border: 1px solid transparent;padding: 0.375rem 0.75rem;font-size: 1rem;line-height: 1.5;border-radius: 0.25rem;transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;'>{button_description}</a>";
 
         private static string CallToAction(string actionName, string actionLink)
         {
-            string button = $"{EMAIL_BUTTON}".Replace("{button_description}", actionName, System.StringComparison.InvariantCulture);
-            button = button.Replace("{button_link}", actionLink, System.StringComparison.InvariantCulture);
+            string button = $"{BUTTON_HTML}".Replace("{button_description}", actionName, System.StringComparison.InvariantCulture);
+            button = button.Replace("{button_link}", actionLink, System.StringComparison.InvariantCulture);            
             return button;
         }
 
@@ -55,10 +25,10 @@ namespace BTCPayServer.Services
         public static void SendSetPasswordConfirmation(this IEmailSender emailSender, string email, string link, bool newPassword)
         {
             var subject = $"{(newPassword ? "Set" : "Update")}  Password";
-            var body = $"A request has been made to update your BTCPay Server password. Please confirm your password by clicking below. <br/><br/> {CallToAction("Update Password", HtmlEncoder.Default.Encode(link))}";
+            var body = $"A request has been made to {(newPassword ? "set" : "update")} your BTCPay Server password. Please confirm your password by clicking below. <br/><br/> {CallToAction(subject, HtmlEncoder.Default.Encode(link))}";
             emailSender.SendEmail(email,
                 subject,
-                $"<html><head>{EMAIL_STYLE}</head>{BTCPAYSERVER_LOGO}<body>{body}</body></html>");
+                $"<html><body style='{BODY_STYLE}'>{HEADER_HTML}{body}</body></html>");
         }
     }
 }

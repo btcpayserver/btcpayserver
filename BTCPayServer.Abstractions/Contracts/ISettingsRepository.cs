97dd10edc01a522a0a61a7a618a37c2efb1f4f41
nicolas.dorier <nicolas.dorier@gmail.com>
2021-03-06 13:36:36 +0900

    Fix some NRE in the FileService

--- BTCPayServer.Abstractions/Contracts/ISettingsRepository.cs
+++ BTCPayServer.Abstractions/Contracts/ISettingsRepository.cs
@@ -1,3 +1,4 @@
+#nullable enable
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -5,8 +6,8 @@ namespace BTCPayServer.Abstractions.Contracts
 {
     public interface ISettingsRepository
     {
-        Task<T> GetSettingAsync<T>(string name = null);
-        Task UpdateSetting<T>(T obj, string name = null);
-        Task<T> WaitSettingsChanged<T>(CancellationToken cancellationToken = default);
+        Task<T?> GetSettingAsync<T>(string? name = null) where T : class;
+        Task UpdateSetting<T>(T obj, string? name = null) where T : class;
+        Task<T> WaitSettingsChanged<T>(CancellationToken cancellationToken = default) where T : class;
     }
 }

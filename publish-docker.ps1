1b2399745d4782128a8b6fac702cfe5987f6f645
nicolas.dorier <nicolas.dorier@gmail.com>
2021-04-01 12:05:54 +0900

    Remove stable and latest branch

--- publish-docker.ps1
+++ publish-docker.ps1
@@ -1,8 +1,4 @@
 $ver = [regex]::Match((Get-Content Build/Version.csproj), '<Version>([^<]+)<').Groups[1].Value
 git tag -a "v$ver" -m "$ver"
-git checkout latest
-git merge master
 git checkout master
-git tag -d "stable"
-git tag -a "stable" -m "stable"
-git push origin latest master "v$ver" --force
+git push origin "v$ver" --force

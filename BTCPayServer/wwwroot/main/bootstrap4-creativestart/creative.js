ff8378cd68977a30c8f676598202c45ac25e27e0
Dennis Reimann <mail@dennisreimann.de>
2021-05-17 12:52:06 +0200

    Remove unused JS

--- BTCPayServer/wwwroot/main/bootstrap4-creativestart/creative.js
+++ BTCPayServer/wwwroot/main/bootstrap4-creativestart/creative.js
@@ -30,38 +30,4 @@
             $("#mainNav").removeClass("navbar-shrink");
         }
     });
-
-    // Scroll reveal calls
-    window.sr = ScrollReveal();
-    sr.reveal('.sr-icons', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
-    sr.reveal('.sr-button', {
-        duration: 1000,
-        delay: 200
-    });
-    sr.reveal('.sr-contact', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 300);
-
-    // Magnific popup calls
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1]
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    });
-
 })(jQuery); // End of use strict

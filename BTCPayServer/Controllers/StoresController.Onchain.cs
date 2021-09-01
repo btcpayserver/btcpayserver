791872ecc81e684a67a97b3ecf8ab6303516f2aa
Manan Sharma <manan180796@gmail.com>
2021-09-01 18:45:15 +0530

    Do not show Taproot when not supported

--- BTCPayServer/Controllers/StoresController.Onchain.cs
+++ BTCPayServer/Controllers/StoresController.Onchain.cs
@@ -55,6 +55,7 @@ namespace BTCPayServer.Controllers
             vm.RootKeyPath = network.GetRootKeyPath();
             vm.CanUseHotWallet = hotWallet;
             vm.CanUseRPCImport = rpcImport;
+            vm.CanUseTaproot = TaprootSupported(vm.CryptoCode);
 
             if (vm.Method == null)
             {
@@ -208,6 +209,7 @@ namespace BTCPayServer.Controllers
 
             vm.CanUseHotWallet = hotWallet;
             vm.CanUseRPCImport = rpcImport;
+            vm.CanUseTaproot = TaprootSupported(vm.CryptoCode);
             vm.RootKeyPath = network.GetRootKeyPath();
             vm.Network = network;
 
@@ -262,6 +264,12 @@ namespace BTCPayServer.Controllers
                 CanUseRPCImport = rpcImport
             };
 
+            if (request.ScriptPubKeyType == ScriptPubKeyType.TaprootBIP86 && !TaprootSupported(cryptoCode) )
+            {
+                ModelState.AddModelError(nameof(request.ScriptPubKeyType), $"Taproot not supported");
+                return View(vm.ViewName, vm);
+            }
+            
             if (isImport && string.IsNullOrEmpty(request.ExistingMnemonic))
             {
                 ModelState.AddModelError(nameof(request.ExistingMnemonic), "Please provide your existing seed");

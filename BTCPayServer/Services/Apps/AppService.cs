c267cf0e9cc9573866eb7f6a227eb5e87d9f9046
Umar Bolatov <bolatovumar@gmail.com>
2021-09-06 13:09:22 -0700

    Rename "hidden" to "unavailable"

--- BTCPayServer/Services/Apps/AppService.cs
+++ BTCPayServer/Services/Apps/AppService.cs
@@ -290,7 +290,7 @@ namespace BTCPayServer.Services.Apps
                     itemNode.Add("image", new YamlScalarNode(item.Image));
                 }
                 itemNode.Add("custom", new YamlScalarNode(item.Custom.ToStringLowerInvariant()));
-                itemNode.Add("hidden", new YamlScalarNode(item.Hidden.ToStringLowerInvariant()));
+                itemNode.Add("unavailable", new YamlScalarNode(item.Unavailable.ToStringLowerInvariant()));
                 if (item.Inventory.HasValue)
                 {
                     itemNode.Add("inventory", new YamlScalarNode(item.Inventory.ToString()));
@@ -339,7 +339,7 @@ namespace BTCPayServer.Services.Apps
                     BuyButtonText = c.GetDetailString("buyButtonText"),
                     Inventory = string.IsNullOrEmpty(c.GetDetailString("inventory")) ? (int?)null : int.Parse(c.GetDetailString("inventory"), CultureInfo.InvariantCulture),
                     PaymentMethods = c.GetDetailStringList("payment_methods"),
-                    Hidden = c.GetDetailString("hidden") == "true"
+                    Unavailable = c.GetDetailString("unavailable") == "true"
                 })
                 .ToArray();
         }

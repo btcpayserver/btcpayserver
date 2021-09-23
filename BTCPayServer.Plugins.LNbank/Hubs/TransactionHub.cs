50e1d471d296898bbbfa44d369e63f6726ababa1
Dennis Reimann <mail@dennisreimann.de>
2021-09-23 13:38:29 +0200

    Allow unauthenticated connection to hub (for share page)

--- BTCPayServer.Plugins.LNbank/Hubs/TransactionHub.cs
+++ BTCPayServer.Plugins.LNbank/Hubs/TransactionHub.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace BTCPayServer.Plugins.LNbank.Hubs
 {
+    [AllowAnonymous]
     public class TransactionHub : Hub
     {
         public override Task OnConnectedAsync()

08f57558da5415adf8d6c75946911c82a2934f10
d11n <mail@dennisreimann.de>
2021-11-11 10:31:15 +0100

    Fake payment fixes (#3058)
    Fixes and various minor improvements for the fake payment form.

--- BTCPayServer/Controllers/InvoiceController.Testing.cs
+++ BTCPayServer/Controllers/InvoiceController.Testing.cs
@@ -1,42 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Net.Mime;
-using System.Net.WebSockets;
-using System.Threading;
 using System.Threading.Tasks;
-using BTCPayServer.Abstractions.Constants;
-using BTCPayServer.Abstractions.Extensions;
-using BTCPayServer.Abstractions.Models;
-using BTCPayServer.Client;
-using BTCPayServer.Client.Models;
 using BTCPayServer.Data;
-using BTCPayServer.Events;
 using BTCPayServer.Filters;
-using BTCPayServer.Logging;
-using BTCPayServer.HostedServices;
-using BTCPayServer.Models;
-using BTCPayServer.Models.InvoicingModels;
 using BTCPayServer.Payments;
-using BTCPayServer.Payments.Lightning;
-using BTCPayServer.Rating;
-using BTCPayServer.Security;
-using BTCPayServer.Services.Invoices;
-using BTCPayServer.Services.Invoices.Export;
-using BTCPayServer.Services.Rates;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.EntityFrameworkCore;
 using NBitcoin;
-using NBitcoin.RPC;
-using NBitpayClient;
-using NBXplorer;
-using Newtonsoft.Json.Linq;
-using BitpayCreateInvoiceRequest = BTCPayServer.Models.BitpayCreateInvoiceRequest;
-using StoreData = BTCPayServer.Data.StoreData;
 using BTCPayServer.Services;
 
 namespace BTCPayServer.Controllers
@@ -46,7 +15,9 @@ namespace BTCPayServer.Controllers
         public class FakePaymentRequest
         {
             public Decimal Amount { get; set; }
+            public string CryptoCode { get; set; } = "BTC";
         }
+        
         [HttpPost]
         [Route("i/{invoiceId}/test-payment")]
         [CheatModeRoute]
@@ -56,21 +27,16 @@ namespace BTCPayServer.Controllers
             var store = await _StoreRepository.FindStore(invoice.StoreId);
             
             // TODO support altcoins, not just bitcoin
-            //var network = invoice.Networks.GetNetwork(invoice.Currency);
-            var cryptoCode = "BTC";
-            var network = _NetworkProvider.GetNetwork<BTCPayNetwork>(cryptoCode);
-            var paymentMethodId = store.GetDefaultPaymentId();
-
-            //var network = NetworkProvider.GetNetwork<BTCPayNetwork>("BTC");
+            var network = _NetworkProvider.GetNetwork<BTCPayNetwork>(request.CryptoCode);
+            var paymentMethodId = store.GetDefaultPaymentId() ?? store.GetEnabledPaymentIds(_NetworkProvider).FirstOrDefault(p => p.CryptoCode == request.CryptoCode && p.PaymentType == PaymentTypes.BTCLike);
             var bitcoinAddressString = invoice.GetPaymentMethod(paymentMethodId).GetPaymentMethodDetails().GetPaymentDestination();
-            
             var bitcoinAddressObj = BitcoinAddress.Create(bitcoinAddressString, network.NBitcoinNetwork);
             var BtcAmount = request.Amount;
+            
             try
             {
                 var paymentMethod = invoice.GetPaymentMethod(paymentMethodId);
                 var rate = paymentMethod.Rate;
-                
                 var txid = cheater.CashCow.SendToAddress(bitcoinAddressObj, new Money(BtcAmount, MoneyUnit.BTC)).ToString();
                 
                 // TODO The value of totalDue is wrong. How can we get the real total due? invoice.Price is only correct if this is the 2nd payment, not for a 3rd or 4th payment. 

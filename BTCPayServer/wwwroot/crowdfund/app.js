6c54421252f2382523d1cdaf6cd45d3d841dc66b
Umar Bolatov <bolatovumar@gmail.com>
2021-11-13 17:55:31 -0800

    Fix bug with BTC fraction amount display

--- BTCPayServer/wwwroot/crowdfund/app.js
+++ BTCPayServer/wwwroot/crowdfund/app.js
@@ -86,7 +86,10 @@ document.addEventListener("DOMContentLoaded",function (ev) {
         },
         computed: {
             raisedAmount: function(){
-               return parseFloat(this.srvModel.info.currentAmount + this.srvModel.info.currentPendingAmount ).toFixed(this.srvModel.currencyData.divisibility) ;
+                return this.formatAmount(this.srvModel.info.currentAmount + this.srvModel.info.currentPendingAmount);
+            },
+            targetAmount: function(){
+                return this.formatAmount(this.srvModel.targetAmount);
             },
             percentageRaisedAmount: function(){
                 return parseFloat(this.srvModel.info.progressPercentage + this.srvModel.info.pendingProgressPercentage ).toFixed(2);
@@ -200,6 +203,9 @@ document.addEventListener("DOMContentLoaded",function (ev) {
                 if(this.timeoutState){
                     clearTimeout(this.timeoutState);
                 }
+            },
+            formatAmount: function(amount) {
+                return formatAmount(amount, this.srvModel.currencyData.divisibility)
             }
         },
         mounted: function () {
@@ -312,3 +318,18 @@ document.addEventListener("DOMContentLoaded",function (ev) {
     });
 });
 
+/**
+ * Formats input string as a number according to browser locale
+ * with correctly displayed fraction amount (e.g. 0.012345 for BTC instead of just 0.0123)
+ * 
+ * @param {number | string} amount Amount to format
+ * @param {number} divisibility Currency divisibility (e.g., 8 for BTC)
+ * @returns String formatted as a number according to current browser locale and correct fraction amount
+ */
+function formatAmount(amount, divisibility) {
+    var parsedAmount = parseFloat(amount).toFixed(divisibility);
+    var [wholeAmount, fractionAmount] = parsedAmount.split('.');
+    var formattedWholeAmount = new Intl.NumberFormat().format(parseInt(wholeAmount, 10));
+
+    return formattedWholeAmount + (fractionAmount ? '.' + fractionAmount : '');
+}

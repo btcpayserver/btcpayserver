459a754f311453f1f96b2bd0c998134b06a1b4a6
Kukks <evilkukka@gmail.com>
2021-11-18 10:12:36 +0100

    add packer script

---
+++ BTCPayServer.Plugins.LNbank/Pack.ps1
@@ -0,0 +1,2 @@
+﻿dotnet publish -c Release -o bin/publish/BTCPayServer.Plugins.LNbank
+dotnet run -p ../BTCPayServer.PluginPacker bin/publish/BTCPayServer.Plugins.LNbank BTCPayServer.Plugins.LNbank bin/packed

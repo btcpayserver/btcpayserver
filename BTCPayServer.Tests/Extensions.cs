64b85ac1f90ce221ae14f285a9ec0e43f7e1b8a2
nicolas.dorier <nicolas.dorier@gmail.com>
2022-01-07 19:49:33 +0900

    Fix AssertNoError

--- BTCPayServer.Tests/Extensions.cs
+++ BTCPayServer.Tests/Extensions.cs
@@ -33,7 +33,7 @@ namespace BTCPayServer.Tests
                 foreach (var dangerAlert in driver.FindElements(By.ClassName("alert-danger")))
                     Assert.False(dangerAlert.Displayed, $"No alert should be displayed, but found this on {driver.Url}: {dangerAlert.Text}");
             }
-            Assert.DoesNotContain("Access denied</h", driver.PageSource);
+            Assert.DoesNotContain("- Denied</h", driver.PageSource);
             Assert.DoesNotContain("Page not found</h", driver.PageSource);
         }
 

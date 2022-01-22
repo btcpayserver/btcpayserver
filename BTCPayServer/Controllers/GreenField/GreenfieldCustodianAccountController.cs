162ba09579ed1a57344b1eba95b3529d38471b6e
Wouter Samaey <wouter.samaey@storefront.be>
2022-01-22 20:20:43 +0100

    List Custodian Accounts call does not load assetBalances by default, because it can fail. Can be requested when needed.

--- BTCPayServer/Controllers/GreenField/GreenfieldCustodianAccountController.cs
+++ BTCPayServer/Controllers/GreenField/GreenfieldCustodianAccountController.cs
@@ -34,8 +34,9 @@ namespace BTCPayServer.Controllers.Greenfield
         }
 
         [HttpGet("~/api/v1/store/{storeId}/custodian-account")]
-        [Authorize(Policy = Policies.CanViewCustodianAccounts, AuthenticationSchemes = AuthenticationSchemes.Greenfield)]
-        public async Task<IActionResult> ListCustodianAccount(string storeId)
+        [Authorize(Policy = Policies.CanViewCustodianAccounts,
+            AuthenticationSchemes = AuthenticationSchemes.Greenfield)]
+        public async Task<IActionResult> ListCustodianAccount(string storeId,[FromQuery] bool assetBalances = false)
         {
             var store = HttpContext.GetStoreData();
             if (store == null)
@@ -43,19 +44,20 @@ namespace BTCPayServer.Controllers.Greenfield
                 return this.CreateAPIError(404, "store-not-found", "The store was not found");
             }
 
-            var custodianAccounts =_custodianAccountRepository.FindByStoreId(storeId);
+            var custodianAccounts = _custodianAccountRepository.FindByStoreId(storeId);
             var r = custodianAccounts.Result.Select(ToModel).ToList();
-            
-            // TODO fill in the "assetBalances"
-            
-            foreach (var custodianAccount in r)
+
+            if (assetBalances)
             {
-                var custodianCode = custodianAccount.CustodianCode;
-                var custodian = _custodianRegistry.getAll()[custodianCode];
-                var balances = await custodian.GetAssetBalances(custodianAccount);
-                custodianAccount.AssetBalances = balances;
+                foreach (var custodianAccount in r)
+                {
+                    var custodianCode = custodianAccount.CustodianCode;
+                    var custodian = _custodianRegistry.getAll()[custodianCode];
+                    var balances = await custodian.GetAssetBalances(custodianAccount);
+                    custodianAccount.AssetBalances = balances;
+                }
             }
-            
+
             return Ok(r);
         }
 
@@ -70,33 +72,33 @@ namespace BTCPayServer.Controllers.Greenfield
         }
 
         [HttpPost("~/api/v1/store/{storeId}/custodian-account")]
-        [Authorize(Policy = Policies.CanCreateCustodianAccounts, AuthenticationSchemes = AuthenticationSchemes.Greenfield)]
+        [Authorize(Policy = Policies.CanCreateCustodianAccounts,
+            AuthenticationSchemes = AuthenticationSchemes.Greenfield)]
         public async Task<IActionResult> CreateCustodianAccount(string storeId, CreateCustodianAccountRequest request)
         {
             request ??= new CreateCustodianAccountRequest();
 
             // TODO this may throw an exception if custodian is not found. How do I make this better?
             var custodian = _custodianRegistry.getAll()[request.CustodianCode];
-            
+
             // TODO If storeId is not valid, we get a foreign key SQL error. Is this okay or do we want to check the storeId first?
-            
+
             var custodianAccount = new CustodianAccountData()
             {
-                CustodianCode = custodian.getCode(),
-                StoreId = storeId,
-                
+                CustodianCode = custodian.getCode(), StoreId = storeId,
             };
             var newBlob = new CustodianAccountData.CustodianAccountBlob();
             newBlob.config = request.Config;
             custodianAccount.SetBlob(newBlob);
-            
+
             await _custodianAccountRepository.CreateOrUpdate(custodianAccount);
             return Ok(custodianAccount);
         }
 
-         
+
         [HttpDelete("~/api/v1/custodian-account/{id}", Order = 1)]
-        [Authorize(Policy = Policies.CanModifyCustodianAccounts, AuthenticationSchemes = AuthenticationSchemes.Greenfield)]
+        [Authorize(Policy = Policies.CanModifyCustodianAccounts,
+            AuthenticationSchemes = AuthenticationSchemes.Greenfield)]
         public async Task<IActionResult> DeleteCustodianAccount(string id)
         {
             //TODO implement
@@ -119,25 +121,26 @@ namespace BTCPayServer.Controllers.Greenfield
         // }
 
         [HttpGet("~/api/v1/store/{storeId}/custodian-account/{accountId}/{paymentMethod}/address")]
-        [Authorize(Policy = Policies.CanDepositToCustodianAccounts, AuthenticationSchemes = AuthenticationSchemes.Greenfield)]
-        public async Task<IActionResult> GetDepositAddress(string storeId, string accountId, string paymentMethod, CreateCustodianAccountRequest request)
+        [Authorize(Policy = Policies.CanDepositToCustodianAccounts,
+            AuthenticationSchemes = AuthenticationSchemes.Greenfield)]
+        public async Task<IActionResult> GetDepositAddress(string storeId, string accountId, string paymentMethod,
+            CreateCustodianAccountRequest request)
         {
             var custodianAccount = _custodianAccountRepository.FindById(accountId);
             var custodian = _custodianRegistry.getAll()[custodianAccount.Result.CustodianCode];
 
             if (custodian is ICanDeposit)
             {
-                var result = ((ICanDeposit) custodian).GetDepositAddress(paymentMethod);
+                var result = ((ICanDeposit)custodian).GetDepositAddress(paymentMethod);
                 return Ok(result);
             }
+
             return this.CreateAPIError(400, "deposit-payment-method-not-supported",
                 $"Deposits to \"{custodian.getName()}\" are not supported using \"{paymentMethod}\".");
         }
-        
+
         // TODO trade endpoint
-        
+
         // TODO withdraw endpoint
     }
-    
-    
 }

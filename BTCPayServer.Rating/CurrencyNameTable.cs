048d6f89364b81b159eab94a027283b71609dbc8
nicolas.dorier <nicolas.dorier@gmail.com>
2022-01-24 13:47:26 +0900

    Add suggestion list for currency inputs

--- BTCPayServer.Rating/CurrencyNameTable.cs
+++ BTCPayServer.Rating/CurrencyNameTable.cs
@@ -143,6 +143,8 @@ namespace BTCPayServer.Services.Rates
             return currencies;
         }
 
+        public IEnumerable<CurrencyData> Currencies => _Currencies.Values;
+
         public CurrencyData GetCurrencyData(string currency, bool useFallback)
         {
             ArgumentNullException.ThrowIfNull(currency);

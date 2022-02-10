bac4a7225cec56ca3f7864772d9b5f49859dd4f5
Dennis Reimann <mail@dennisreimann.de>
2022-02-10 13:52:48 +0100

    Add CHANGELOG and update release script

---
+++ BTCPayServer.Plugins.LNbank/CHANGELOG.md
@@ -0,0 +1,36 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.0.4] - 2022-02-10
+
+### Added
+
+- Support for private route hints: Will be enabled if the connected store has the
+  required setting or if the toggle on the receive page is activated.
+
+### Changed
+
+- Lowercase page paths.
+- Remove button icons, improve wallet view.
+
+### Fixed
+
+- Logo link on Share page
+
+## [1.0.3] - 2022-02-01
+
+### Added
+
+- Form validation
+
+### Changed
+
+- Improve create wallet for LN node connection case
+
+### Fixed
+
+- Non-admins cannot send and receive when using the internal node.
--- BTCPayServer.Plugins.LNbank/release.sh
+++ BTCPayServer.Plugins.LNbank/release.sh
@@ -1,41 +1,56 @@
 ﻿#!/bin/zsh
-
 set -e
 
 if [ -z "$1" ]; then
-  echo "Please provide a version number"
+  printf "Please provide a version number:\n\n./release.sh 1.0.0\n\n"
   exit 1
 fi
 
-PLUGINS_DIR=../../plugins/
-v=$1
-t="LNbank v${v}"
+version=$1
+versionName="v$version"
+remoteName="dennisreimann"
+remoteBranch="plugins"
+pluginsBranch="lnbank-$versionName"
+pluginsDir="../../plugins/"
+tagName="BTCPayServer.LNbank/$versionName"
+tagDesc="LNbank $versionName"
 
-sed -i "s%<AssemblyVersion>.*</AssemblyVersion>%<AssemblyVersion>${v}</AssemblyVersion>%g" ./BTCPayServer.Plugins.LNbank.csproj
-sed -i "s%<PackageVersion>.*</PackageVersion>%<PackageVersion>${v}</PackageVersion>%g" ./BTCPayServer.Plugins.LNbank.csproj
+# Parse changelog
+changes=$(awk -v ver=$version '/^#+ \[/ { if (p) { exit }; if ($2 == "["ver"]") { p=1; next} } p' CHANGELOG.md | sed -rz 's/^\n+//; s/\n+$/\n/g')
 
+if [ -z "$changes" ]; then
+  printf "Please provide version details in the CHANGELOG.\n\n"
+  exit 1
+fi
+
+printf "\n\n=====> Update version and package plugin\n\n"
+sed -i "s%<AssemblyVersion>.*</AssemblyVersion>%<AssemblyVersion>$version</AssemblyVersion>%g" ./BTCPayServer.Plugins.LNbank.csproj
+sed -i "s%<PackageVersion>.*</PackageVersion>%<PackageVersion>$version</PackageVersion>%g" ./BTCPayServer.Plugins.LNbank.csproj
 ./pack.sh
 
-# Prepare plugins repo
-cd $PLUGINS_DIR
+printf "\n\n=====> Prepare plugins repo\n\n"
+cd $pluginsDir
 git reset --hard
 git checkout master
 git pull --rebase
-git checkout lnbank
-git rebase master
+git checkout -b $pluginsBranch
 cd -
 
-# Copy and create PR for the plugin
-cp bin/packed/* $PLUGINS_DIR
-cd $PLUGINS_DIR
-git add .
-git commit -m $t
-git push
-gh pr create --title $t
+printf "\n\n=====> Copy and commit plugin files\n\n"
+cp bin/packed/* $pluginsDir
+cd $pluginsDir
+git commit -a -m "$tagDesc"
+git push $remoteName $pluginsBranch
+
+printf "\n\n=====> Create plugin pull request\n\n"
+gh pr create --title "$tagDesc" --body "# $tagDesc\n\n$changes" --base master --head "$remoteName/$pluginsBranch"
 cd -
 
-# Commit and tag
-git add .
-git commit -m $t
-git tag "BTCPayServer.LNbank/v$v"
-git push && git push --tags
+printf "\n\n=====> Commit and tag\n\n"
+git commit -a -m "$tagDesc"
+git tag $tagName -a -m $tagDesc
+git push $remoteName $remoteBranch
+git push $remoteName refs/tags/$tagName
+
+printf "\n\n=====> Create release\n\n"
+gh release create $versionName --notes "$changes"

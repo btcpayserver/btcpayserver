f036852eec4dbca63f73c6bf754fba8e0e48274f
Wouter Samaey <wouter.samaey@storefront.be>
2022-03-09 10:32:47 +0100

    Re-added User-Agent header as it is required

--- Plugins/BTCPayServer.Plugins.Custodians.Kraken/Kraken/KrakenExchange.cs
+++ Plugins/BTCPayServer.Plugins.Custodians.Kraken/Kraken/KrakenExchange.cs
@@ -88,6 +88,7 @@ public class KrakenExchange : ICustodian, ICanDeposit, ICanTrade, ICanWithdraw
     private HttpRequestMessage CreateHttpClient()
     {
         HttpRequestMessage request = new();
+        request.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36");
         return request;
     }
 

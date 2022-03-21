267deeed0fb46bd8abb0c310ac4fa0e02e3218c4
Dennis Reimann <mail@dennisreimann.de>
2022-03-21 20:24:22 +0100

    FileService: AddFile from URL

--- BTCPayServer.Abstractions/Contracts/IFileService.cs
+++ BTCPayServer.Abstractions/Contracts/IFileService.cs
@@ -9,6 +9,7 @@ public interface IFileService
 {
     Task<bool> IsAvailable();
     Task<IStoredFile> AddFile(IFormFile file, string userId);
+    Task<IStoredFile> AddFile(Uri file, string userId);
     Task<string?> GetFileUrl(Uri baseUri, string fileId);
     Task<string?> GetTemporaryFileUrl(Uri baseUri, string fileId, DateTimeOffset expiry,
         bool isDownload);

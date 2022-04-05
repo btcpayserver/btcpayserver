c35c545709aaf558c1b959f5a8b0e1c672c48aa0
Dennis Reimann <mail@dennisreimann.de>
2022-04-05 14:56:29 +0200

    Dynamic data for recent invoices tile

--- BTCPayServer/Components/StoreRecentInvoices/StoreRecentInvoicesViewModel.cs
+++ BTCPayServer/Components/StoreRecentInvoices/StoreRecentInvoicesViewModel.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System.Collections.Generic;
 using BTCPayServer.Data;
 
 namespace BTCPayServer.Components.StoreRecentInvoices;
@@ -6,5 +6,5 @@ namespace BTCPayServer.Components.StoreRecentInvoices;
 public class StoreRecentInvoicesViewModel
 {
     public StoreData Store { get; set; }
-    public IEnumerable Entries { get; set; }
+    public IEnumerable<StoreRecentInvoiceViewModel> Invoices { get; set; }
 }

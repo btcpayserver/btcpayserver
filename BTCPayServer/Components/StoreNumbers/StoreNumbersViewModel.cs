a5aeb2a3c1c0c78d4504d92b44f1143f482485e9
Nicolas Dorier <nicolas.dorier@gmail.com>
2022-04-30 12:54:12 +0900

    Fix performance issue on dashboard for big wallets (#3694)

--- BTCPayServer/Components/StoreNumbers/StoreNumbersViewModel.cs
+++ BTCPayServer/Components/StoreNumbers/StoreNumbersViewModel.cs
@@ -8,7 +8,7 @@ public class StoreNumbersViewModel
     public StoreData Store { get; set; }
     public WalletId WalletId { get; set; }
     public int PayoutsPending { get; set; }
-    public int Transactions { get; set; }
+    public int? Transactions { get; set; }
     public int RefundsIssued { get; set; }
     public int TransactionDays { get; set; }
 }

346821f0d6124b86d91498cdbc542e8b6799a746
Saksham Solanki <30777972+SakshamSolanki@users.noreply.github.com>
2022-06-15 13:03:24 +0530

    Added toggle button to switch to store default currency (#3752)
    * Added toggle button to switch to store default currency
    
    * Replaced ["usd"] with the currency variable
    
    * Fix indentation and improve JS part
    
    * Update script and display
    
    * Improve chart display
    
    * Improve rate display
    
    * Address code review comments
    
    Co-authored-by: Dennis Reimann <mail@dennisreimann.de>

--- BTCPayServer/Components/StoreWalletBalance/StoreWalletBalance.cs
+++ BTCPayServer/Components/StoreWalletBalance/StoreWalletBalance.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BTCPayServer.Data;
 using BTCPayServer.Services;
+using BTCPayServer.Services.Rates;
 using BTCPayServer.Services.Stores;
 using BTCPayServer.Services.Wallets;
 using Dapper;
@@ -18,28 +19,37 @@ namespace BTCPayServer.Components.StoreWalletBalance;
 
 public class StoreWalletBalance : ViewComponent
 {
-    private string CryptoCode;
+    private string _cryptoCode;
     private const WalletHistogramType DefaultType = WalletHistogramType.Week;
 
     private readonly StoreRepository _storeRepo;
+    private readonly CurrencyNameTable _currencies;
     private readonly WalletHistogramService _walletHistogramService;
 
-    public StoreWalletBalance(StoreRepository storeRepo, WalletHistogramService walletHistogramService, BTCPayNetworkProvider networkProvider)
+    public StoreWalletBalance(
+        StoreRepository storeRepo,
+        CurrencyNameTable currencies,
+        WalletHistogramService walletHistogramService, 
+        BTCPayNetworkProvider networkProvider)
     {
         _storeRepo = storeRepo;
+        _currencies = currencies;
         _walletHistogramService = walletHistogramService;
-        CryptoCode = networkProvider.DefaultNetwork.CryptoCode;
+        _cryptoCode = networkProvider.DefaultNetwork.CryptoCode;
     }
 
     public async Task<IViewComponentResult> InvokeAsync(StoreData store)
     {
-        var walletId = new WalletId(store.Id, CryptoCode);
+        var walletId = new WalletId(store.Id, _cryptoCode);
         var data = await _walletHistogramService.GetHistogram(store, walletId, DefaultType);
+        var defaultCurrency = store.GetStoreBlob().DefaultCurrency;
         
         var vm = new StoreWalletBalanceViewModel
         {
             Store = store,
-            CryptoCode = CryptoCode,
+            CryptoCode = _cryptoCode,
+            CurrencyData = _currencies.GetCurrencyData(defaultCurrency, true),
+            DefaultCurrency = defaultCurrency,
             WalletId = walletId,
             Series = data?.Series,
             Labels = data?.Labels,
--- BTCPayServer/Components/StoreWalletBalance/StoreWalletBalanceViewModel.cs
+++ BTCPayServer/Components/StoreWalletBalance/StoreWalletBalanceViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using BTCPayServer.Data;
+using BTCPayServer.Services.Rates;
 using BTCPayServer.Services.Wallets;
 
 namespace BTCPayServer.Components.StoreWalletBalance;
@@ -8,6 +9,8 @@ public class StoreWalletBalanceViewModel
 {
     public decimal? Balance { get; set; }
     public string CryptoCode { get; set; }
+    public string DefaultCurrency { get; set; }
+    public CurrencyData CurrencyData { get; set; }
     public StoreData Store { get; set; }
     public WalletId WalletId { get; set; }
     public WalletHistogramType Type { get; set; }

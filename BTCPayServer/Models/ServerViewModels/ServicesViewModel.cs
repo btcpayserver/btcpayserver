3ad29f9eba0478ff90ef7391fa726ab26bae59f0
Dennis Reimann <mail@dennisreimann.de>
2022-06-15 13:30:29 +0200

    Remove more code

--- BTCPayServer/Models/ServerViewModels/ServicesViewModel.cs
+++ BTCPayServer/Models/ServerViewModels/ServicesViewModel.cs
@@ -5,17 +5,15 @@ namespace BTCPayServer.Models.ServerViewModels
 {
     public class ServicesViewModel
     {
-
         public class OtherExternalService
         {
             public string Name { get; set; }
             public string Link { get; set; }
         }
 
-        public List<ExternalService> ExternalServices { get; set; } = new List<ExternalService>();
-        public List<OtherExternalService> OtherExternalServices { get; set; } = new List<OtherExternalService>();
-        public List<OtherExternalService> TorHttpServices { get; set; } = new List<OtherExternalService>();
-        public List<OtherExternalService> TorOtherServices { get; set; } = new List<OtherExternalService>();
-        public List<OtherExternalService> ExternalStorageServices { get; set; } = new List<OtherExternalService>();
+        public List<ExternalService> ExternalServices { get; set; } = new ();
+        public List<OtherExternalService> OtherExternalServices { get; set; } = new ();
+        public List<OtherExternalService> TorHttpServices { get; set; } = new ();
+        public List<OtherExternalService> TorOtherServices { get; set; } = new ();
     }
 }

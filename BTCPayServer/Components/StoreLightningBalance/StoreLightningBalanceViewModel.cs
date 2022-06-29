0f0b2f839d22e1333de0b136310658fdd4c4b1e6
Dennis Reimann <mail@dennisreimann.de>
2022-06-29 16:21:36 +0200

    Remove LightMoney converters

--- BTCPayServer/Components/StoreLightningBalance/StoreLightningBalanceViewModel.cs
+++ BTCPayServer/Components/StoreLightningBalance/StoreLightningBalanceViewModel.cs
@@ -12,11 +12,7 @@ public class StoreLightningBalanceViewModel
     public string DefaultCurrency { get; set; }
     public CurrencyData CurrencyData { get; set; }
     public StoreData Store { get; set; }
-    
-    [JsonConverter(typeof (LightMoneyJsonConverter))]
     public LightMoney TotalOnchain { get; set; }
-    
-    [JsonConverter(typeof (LightMoneyJsonConverter))]
     public LightMoney TotalOffchain { get; set; }
     public LightningNodeBalance Balance { get; set; }
     public string ProblemDescription { get; set; }

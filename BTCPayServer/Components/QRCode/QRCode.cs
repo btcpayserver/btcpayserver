e1d4b945c2799818c9818b51e61a5638559b83d3
Wouter Samaey <wouter.samaey@storefront.be>
2022-08-05 21:07:00 +0200

    Refactored QR Code <img> tag generation to a service that can be used in controllers too

--- BTCPayServer/Components/QRCode/QRCode.cs
+++ BTCPayServer/Components/QRCode/QRCode.cs
@@ -1,24 +1,26 @@
 using System;
 using System.Drawing;
+using BTCPayServer.Services;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
-using QRCoder;
 
 namespace BTCPayServer.Components.QRCode
 {
     public class QRCode : ViewComponent
     {
-        private static QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        private readonly QRCodeImgTagGenerator _imgTagGenerator;
+
+        public QRCode(QRCodeImgTagGenerator imgTagGenerator)
+        {
+            _imgTagGenerator = imgTagGenerator;
+        }
 
 
         public IViewComponentResult Invoke(string data)
         {
-            QRCodeData qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
-            PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
-            var bytes = qrCode.GetGraphic(5, new byte[] { 0, 0, 0, 255 }, new byte[] { 0xf5, 0xf5, 0xf7, 255 }, true);
-            var b64 = Convert.ToBase64String(bytes);
-            return new HtmlContentViewComponentResult(new HtmlString($"<img height=\"256\" style=\"image-rendering: pixelated;image-rendering: -moz-crisp-edges;\" src=\"data:image/png;base64,{b64}\" />"));
+            var html = _imgTagGenerator.generateImgTag(data);
+            return new HtmlContentViewComponentResult(new HtmlString(html));
         }
     }
 }
--- BTCPayServer/Hosting/BTCPayServerServices.cs
+++ BTCPayServer/Hosting/BTCPayServerServices.cs
@@ -130,7 +130,7 @@ namespace BTCPayServer.Hosting
             services.TryAddSingleton<PaymentRequestService>();
             services.TryAddSingleton<UserService>();
             services.AddSingleton<CustodianAccountRepository>();
-            
+            services.AddSingleton<QRCodeImgTagGenerator>();
 
             services.TryAddSingleton<WalletHistogramService>();
             services.TryAddSingleton<CustodianAccountRepository>();

0b93fb8d978292f87bdfb4ee535d722afddbb33a
wakiyamap <wakiyamap@gmail.com>
2022-08-10 04:52:14 +0900

    Change PriceProvider(Mona)

--- BTCPayServer.Common/Altcoins/BTCPayNetworkProvider.Monacoin.cs
+++ BTCPayServer.Common/Altcoins/BTCPayNetworkProvider.Monacoin.cs
@@ -17,7 +17,7 @@ namespace BTCPayServer
                 DefaultRateRules = new[]
                 {
                                 "MONA_X = MONA_BTC * BTC_X",
-                                "MONA_BTC = bittrex(MONA_BTC)"
+                                "MONA_BTC = bitbank(MONA_BTC)"
                 },
                 CryptoImagePath = "imlegacy/monacoin.png",
                 LightningImagePath = "imlegacy/mona-lightning.svg",

42f6bead5660d5c527a57ef340f9292d3ed66430
Kukks <evilkukka@gmail.com>
2022-08-10 11:26:25 +0200

    Allow bind and port for http too

--- BTCPayServer/Hosting/Startup.cs
+++ BTCPayServer/Hosting/Startup.cs
@@ -167,40 +167,48 @@ namespace BTCPayServer.Hosting
             bool hasCertPath = !String.IsNullOrEmpty(httpsCertificateFilePath);
             services.Configure<KestrelServerOptions>(kestrel =>
             {
-                kestrel.Limits.MaxRequestLineSize = 8_192 * 10 * 5; // Around 500K, transactions passed in URI should not be bigger than this
-            });
-            if (hasCertPath || useDefaultCertificate)
-            {
-                var bindAddress = Configuration.GetOrDefault<IPAddress>("bind", IPAddress.Any);
-                int bindPort = Configuration.GetOrDefault<int>("port", 443);
+                kestrel.Limits.MaxRequestLineSize =
+                    8_192 * 10 * 5; // Around 500K, transactions passed in URI should not be bigger than this
 
-                services.Configure<KestrelServerOptions>(kestrel =>
+                var bindAddress = Configuration.GetOrDefault<IPAddress>("bind", IPAddress.Any);
+                int bindPort =
+                    Configuration.GetOrDefault<int>("port", hasCertPath || useDefaultCertificate ? 443 : 14142);
+
+                if (hasCertPath || useDefaultCertificate)
                 {
                     if (hasCertPath && !File.Exists(httpsCertificateFilePath))
                     {
                         // Note that by design this is a fatal error condition that will cause the process to exit.
-                        throw new ConfigException($"The https certificate file could not be found at {httpsCertificateFilePath}.");
-                    }
-                    if (hasCertPath && useDefaultCertificate)
-                    {
-                        throw new ConfigException($"Conflicting settings: if HttpsUseDefaultCertificate is true, HttpsCertificateFilePath should not be used");
+                        throw new ConfigException(
+                            $"The https certificate file could not be found at {httpsCertificateFilePath}.");
                     }
 
-                    kestrel.Listen(bindAddress, bindPort, l =>
+                    if (hasCertPath && useDefaultCertificate)
+                    {
+                        throw new ConfigException(
+                            $"Conflicting settings: if HttpsUseDefaultCertificate is true, HttpsCertificateFilePath should not be used");
+                    }
+                }
+
+                kestrel.Listen(bindAddress, bindPort, l =>
+                {
+                    if (hasCertPath || useDefaultCertificate)
                     {
                         if (hasCertPath)
                         {
-                            Logs.Configuration.LogInformation($"Using HTTPS with the certificate located in {httpsCertificateFilePath}.");
-                            l.UseHttps(httpsCertificateFilePath, Configuration.GetOrDefault<string>("HttpsCertificateFilePassword", null));
+                            Logs.Configuration.LogInformation(
+                                $"Using HTTPS with the certificate located in {httpsCertificateFilePath}.");
+                            l.UseHttps(httpsCertificateFilePath,
+                                Configuration.GetOrDefault<string>("HttpsCertificateFilePassword", null));
                         }
                         else
                         {
                             Logs.Configuration.LogInformation($"Using HTTPS with the default certificate");
                             l.UseHttps();
                         }
-                    });
+                    }
                 });
-            }
+            });
         }
         public void Configure(
             IApplicationBuilder app,

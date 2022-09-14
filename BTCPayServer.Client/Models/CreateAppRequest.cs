5da858435d1f44c03e38fbeb82f887e7468aa2f3
Umar Bolatov <bolatovumar@gmail.com>
2022-09-13 21:14:09 -0700

    replace DateTimeJsonConverter with NBitcoin.JsonConverters.DateTimeToUnixTimeConverter

--- BTCPayServer.Client/Models/CreateAppRequest.cs
+++ BTCPayServer.Client/Models/CreateAppRequest.cs
@@ -4,63 +4,6 @@ using Newtonsoft.Json.Converters;
 
 namespace BTCPayServer.Client.Models
 {
-    class DateTimeJsonConverter : JsonConverter
-    {
-        public override bool CanConvert(Type objectType)
-        {
-            return objectType == typeof(DateTime);
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            if (reader.Value == null)
-            {
-                return null;
-            }
-
-            if (reader.Value is not long)
-            {
-                throw new JsonSerializationException($"Cannot convert {reader.Value} to a datetime value. Please provide a valid UNIX timestamp");
-            }
-
-            var v = (long)reader.Value;
-            Check(v);
-
-            try 
-            {
-                return unixRef + TimeSpan.FromSeconds(v);
-            }
-            catch (Exception ex)
-            {
-                if (ex is System.ArgumentOutOfRangeException)
-                {
-                    // It's possible someone might pass the timestamp in milliseconds so let's just handle it
-                    return unixRef + TimeSpan.FromMilliseconds(v / 1000);
-                }
-                else
-                {
-                    throw new JsonSerializationException($"Cannot convert {reader.Value} to a datetime value.");
-                }
-            }
-
-        }
-
-        static readonly DateTime unixRef = new DateTime(1970, 1, 1, 0, 0, 0);
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            var date = ((DateTime)value).ToUniversalTime();
-            long v = (long)(date - unixRef).TotalMilliseconds;
-            Check(v);
-            writer.WriteValue(v);
-        }
-
-        private static void Check(long v)
-        {
-            if (v < 0)
-                throw new JsonSerializationException("Invalid datetime (less than 1/1/1970)");
-        }
-    }
-
     public enum PosViewType
     {
         Static,
@@ -113,11 +56,11 @@ namespace BTCPayServer.Client.Models
         public string Title { get; set; } = null;
         public bool? Enabled { get; set; } = null;
         public bool? EnforceTargetAmount { get; set; } = null;
-        [JsonConverter(typeof(DateTimeJsonConverter))]
+        [JsonConverter(typeof(NBitcoin.JsonConverters.DateTimeToUnixTimeConverter))]
         public DateTime? StartDate { get; set; } = null;
         public string TargetCurrency { get; set; } = null;
         public string Description { get; set; } = null;
-        [JsonConverter(typeof(DateTimeJsonConverter))]
+        [JsonConverter(typeof(NBitcoin.JsonConverters.DateTimeToUnixTimeConverter))]
         public DateTime? EndDate { get; set; } = null;
         public decimal? TargetAmount { get; set; } = null;
         public string CustomCSSLink { get; set; } = null;

btcpayserver/btcpayserver

commit bc195e771e488e26e1e6cc6e79e80a38b847c7ea

Update WalletTransactions pagination default settings (#4074)
* Update WalletTransactions pagination default settings

Remove the numeric page selection and add displaying data of last 30 days by default.

* Update WalletTransactions to show txs based on Days

* Update text formatting on WalletTransactions view

Keeps the logic changes. Just undo the formatting of the file from previous commit

* Update WalletTransactions to show all after second load

Utilize Model.Days instead of new variables
Moved javascript code to PageFootContent section

* Update WalletTransactions to use ajax for infinite scroll

* Cleanups

* Apply skip and count only when not prefiltering

* Infinite scroll mode

* Improve datetime formatting and switching

* Upgrade NBXplorer to include get_wallets_recent bugfix

* Revert "Upgrade NBXplorer to include get_wallets_recent bugfix"

This reverts commit b390d942d74d88bb1da3ab8e3407184a527175ef.

* JS fixes

* Upgrade ChromeDriver and BundleMinifier

Co-authored-by: Dennis Reimann <mail@dennisreimann.de>

BTCPayServer.Abstractions/Extensions/ViewsRazor.cs
@@ -88,24 +88,23 @@ namespace BTCPayServer.Abstractions.Extensions
 
         public static HtmlString ToBrowserDate(this DateTimeOffset date)
         {
-            var displayDate = date.ToString("o", CultureInfo.InvariantCulture);
-            return new HtmlString($"<span class='localizeDate'>{displayDate}</span>");
+            var displayDate = date.ToString("g", CultureInfo.InvariantCulture);
+            var dateTime = date.ToString("s", CultureInfo.InvariantCulture);
+            return new HtmlString($"<time datetime=\"{dateTime}\" data-relative=\"{date.ToTimeAgo()}\">{displayDate}</time>");
         }
 
         public static HtmlString ToBrowserDate(this DateTime date)
         {
-            var displayDate = date.ToString("o", CultureInfo.InvariantCulture);
-            return new HtmlString($"<span class='localizeDate'>{displayDate}</span>");
+            var displayDate = date.ToString("g", CultureInfo.InvariantCulture);
+            var dateTime = date.ToString("s", CultureInfo.InvariantCulture);
+            return new HtmlString($"<time datetime=\"{dateTime}\" data-relative=\"{date.ToTimeAgo()}\">{displayDate}</time>");
         }
 
-        public static string ToTimeAgo(this DateTimeOffset date)
-        {
-            var diff = DateTimeOffset.UtcNow - date;
-            var formatted = diff.TotalSeconds > 0
-                ? $"{diff.TimeString()} ago"
-                : $"in {diff.Negate().TimeString()}";
-            return formatted;
-        }
+        public static string ToTimeAgo(this DateTimeOffset date) => (DateTimeOffset.UtcNow - date).ToTimeAgo();
+
+        public static string ToTimeAgo(this DateTime date) => (DateTimeOffset.UtcNow - date).ToTimeAgo();
+
+        public static string ToTimeAgo(this TimeSpan diff) => diff.TotalSeconds > 0 ? $"{diff.TimeString()} ago" : $"in {diff.Negate().TimeString()}";
 
         public static string TimeString(this TimeSpan timeSpan)
         {
@@ -117,16 +116,14 @@ namespace BTCPayServer.Abstractions.Extensions
             {
                 return $"{(int)timeSpan.TotalMinutes} minute{Plural((int)timeSpan.TotalMinutes)}";
             }
-            if (timeSpan.Days < 1)
-            {
-                return $"{(int)timeSpan.TotalHours} hour{Plural((int)timeSpan.TotalHours)}";
-            }
-            return $"{(int)timeSpan.TotalDays} day{Plural((int)timeSpan.TotalDays)}";
+            return timeSpan.Days < 1 
+                ? $"{(int)timeSpan.TotalHours} hour{Plural((int)timeSpan.TotalHours)}"
+                : $"{(int)timeSpan.TotalDays} day{Plural((int)timeSpan.TotalDays)}";
         }
 
         private static string Plural(int value)
         {
-            return value > 1 ? "s" : string.Empty;
+            return value == 1 ? string.Empty : "s";
         }
     }
 }

BTCPayServer/Views/UIInvoice/ListInvoices.cshtml
@@ -29,8 +29,6 @@
             });
         });
 
-        delegate('click', '#switchTimeFormat', switchTimeFormat)
-
         delegate('click', '.changeInvoiceState', e => {
             const { invoiceId, newState } = e.target.dataset;
             const pavpill = $("#pavpill_" + invoiceId);
@@ -286,9 +284,7 @@
                             <input id="selectAllCheckbox" type="checkbox" class="form-check-input" />
                         <th style="min-width:90px;" class="col-md-auto">
                             Date
-                            <a id="switchTimeFormat" href="#">
-                                <span class="fa fa-clock-o" title="Switch date format"></span>
-                            </a>
+                            <a id="switchTimeFormat" href="#" class="fa fa-clock-o" title="Switch date format"></a>
                         </th>
                         <th class="text-nowrap">Order Id</th>
                         <th class="text-nowrap">Invoice Id</th>
@@ -304,11 +300,7 @@
                             <td class="only-for-js">
                                 <input name="selectedItems" type="checkbox" class="selector form-check-input" value="@invoice.InvoiceId" />
                             </td>
-                            <td>
-                                <span class="switchTimeFormat" data-switch="@invoice.Date.ToTimeAgo()">
-                                    @invoice.Date.ToBrowserDate()
-                                </span>
-                            </td>
+                            <td>@invoice.Date.ToBrowserDate()</td>
                             <td style="max-width:120px;">
                                 @if (invoice.RedirectUrl != string.Empty)
                                 {

BTCPayServer/Views/UINotifications/Index.cshtml
@@ -33,17 +33,15 @@
                 <table class="table table-hover table-responsive-md">
                     <thead>
                         <tr>
-                            <th width="30px" class="only-for-js">
+                            <th style="width:30px" class="only-for-js">
                                 @if (Model.Items.Count > 0)
                                 {
                                     <input name="selectedItems" id="selectAllCheckbox" type="checkbox" class="form-check-input" />
                                 }
                             </th>
-                            <th width="190px">
+                            <th>
                                 Date
-                                <a href="#" id="switchTimeFormat">
-                                    <span class="fa fa-clock-o" title="Switch date format"></span>
-                                </a>
+                                <a href="#" id="switchTimeFormat" class="fa fa-clock-o" title="Switch date format"></a>
                             </th>
                             <th>Message</th>
                             <th class="text-end">Actions</th>
@@ -56,11 +54,7 @@
                                 <td class="only-for-js">
                                     <input name="selectedItems" type="checkbox" class="selector form-check-input" value="@item.Id" />
                                 </td>
-                                <td class="toggleRowCheckbox">
-                                    <span class="switchTimeFormat" data-switch="@item.Created.ToTimeAgo()">
-                                        @item.Created.ToBrowserDate()
-                                    </span>
-                                </td>
+                                <td class="toggleRowCheckbox">@item.Created.ToBrowserDate()</td>
                                 <td class="toggleRowCheckbox">
                                     @item.Body
                                 </td>
@@ -129,8 +123,6 @@ else
             input.prop('checked', !input.prop("checked"));
             updateSelectors();
         })
-
-        delegate('click', '#switchTimeFormat', switchTimeFormat)
 
         delegate('click', '.btn-toggle-seen', e => {
             const row = $(e.target).parents(".notification-row").toggleClass("loading");

commit 7651c5f2dd726ddef248acb4c219e35df0e0a6ba

Date display improvements
Fixes styling issues introduced in #4074, because the `max-width` was to small for localized dates.

Also adds the ability to choose the prefered initial display format, which can be the localized or relative date.

BTCPayServer.Abstractions/Extensions/ViewsRazor.cs
@@ -14,6 +14,12 @@ namespace BTCPayServer.Abstractions.Extensions
         private const string ACTIVE_ID_KEY = "ActiveId";
         private const string ActivePageClass = "active";
 
+        public enum DateDisplayFormat
+        {
+            Localized,
+            Relative
+        }
+
         public static void SetActivePage<T>(this ViewDataDictionary viewData, T activePage, string title = null, string activeId = null)
             where T : IConvertible
         {
@@ -86,18 +92,22 @@ namespace BTCPayServer.Abstractions.Extensions
             return categoryAndPageMatch && idMatch ? ActivePageClass : null;
         }
 
-        public static HtmlString ToBrowserDate(this DateTimeOffset date)
+        public static HtmlString ToBrowserDate(this DateTimeOffset date, DateDisplayFormat format = DateDisplayFormat.Localized)
         {
-            var displayDate = date.ToString("g", CultureInfo.InvariantCulture);
+            var relative = date.ToTimeAgo();
+            var initial = format.ToString().ToLower();
             var dateTime = date.ToString("s", CultureInfo.InvariantCulture);
-            return new HtmlString($"<time datetime=\"{dateTime}\" data-relative=\"{date.ToTimeAgo()}\">{displayDate}</time>");
+            var displayDate = format == DateDisplayFormat.Relative ? relative : date.ToString("g", CultureInfo.InvariantCulture);
+            return new HtmlString($"<time datetime=\"{dateTime}\" data-relative=\"{relative}\" data-initial=\"{initial}\">{displayDate}</time>");
         }
 
-        public static HtmlString ToBrowserDate(this DateTime date)
+        public static HtmlString ToBrowserDate(this DateTime date, DateDisplayFormat format = DateDisplayFormat.Localized)
         {
-            var displayDate = date.ToString("g", CultureInfo.InvariantCulture);
+            var relative = date.ToTimeAgo();
+            var initial = format.ToString().ToLower();
             var dateTime = date.ToString("s", CultureInfo.InvariantCulture);
-            return new HtmlString($"<time datetime=\"{dateTime}\" data-relative=\"{date.ToTimeAgo()}\">{displayDate}</time>");
+            var displayDate = format == DateDisplayFormat.Relative ? relative : date.ToString("g", CultureInfo.InvariantCulture);
+            return new HtmlString($"<time datetime=\"{dateTime}\" data-relative=\"{relative}\" data-initial=\"{initial}\">{displayDate}</time>");
         }
 
         public static string ToTimeAgo(this DateTimeOffset date) => (DateTimeOffset.UtcNow - date).ToTimeAgo();

BTCPayServer/Views/UIInvoice/ListInvoices.cshtml
@@ -282,9 +282,11 @@
                     <tr>
                         <th style="width:2rem;" class="only-for-js">
                             <input id="selectAllCheckbox" type="checkbox" class="form-check-input" />
-                        <th style="min-width:90px;" class="col-md-auto">
-                            Date
-                            <a id="switchTimeFormat" href="#" class="fa fa-clock-o" title="Switch date format"></a>
+                        <th class="w-150px">
+                            <div class="d-flex align-items-center gap-1">
+                                Date
+                                <button type="button" class="btn btn-link p-0 fa fa-clock-o switch-time-format" title="Switch date format"></button>
+                            </div>
                         </th>
                         <th class="text-nowrap">Order Id</th>
                         <th class="text-nowrap">Invoice Id</th>

BTCPayServer/Views/UINotifications/Index.cshtml
@@ -39,9 +39,11 @@
                                     <input name="selectedItems" id="selectAllCheckbox" type="checkbox" class="form-check-input" />
                                 }
                             </th>
-                            <th>
-                                Date
-                                <a href="#" id="switchTimeFormat" class="fa fa-clock-o" title="Switch date format"></a>
+                            <th class="w-150px">
+                                <div class="d-flex align-items-center gap-1">
+                                    Date
+                                    <button type="button" class="btn btn-link p-0 fa fa-clock-o switch-time-format" title="Switch date format"></button>
+                                </div>
                             </th>
                             <th>Message</th>
                             <th class="text-end">Actions</th>

BTCPayServer/Views/UIWallets/WalletTransactions.cshtml
@@ -206,9 +206,11 @@
                     <th style="width:2rem;" class="only-for-js">
                         <input id="selectAllCheckbox" type="checkbox" class="form-check-input" />
                     </th>
-                    <th style="max-width:125px;">
-                        Date
-                        <a id="switchTimeFormat" class="fa fa-clock-o" title="Switch date format" href="#"></a>
+                    <th class="w-150px">
+                        <div class="d-flex align-items-center gap-1">
+                            Date
+                            <button type="button" class="btn btn-link p-0 fa fa-clock-o switch-time-format" title="Switch date format" id="switchTimeFormat"></button>
+                        </div>
                     </th>
                     <th class="text-start">Label</th>
                     <th>Transaction Id</th>

BTCPayServer/wwwroot/main/site.css
@@ -96,14 +96,6 @@ a.unobtrusive-link {
     transform: rotate(-180deg);
 }
 
-time[datetime] {
-    display: block;
-    max-width: 125px;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-}
-
 /* Badges */
 .badge-new {
     background: #d4edda;

BTCPayServer/wwwroot/main/site.js
@@ -3,14 +3,14 @@ const flatpickrInstances = [];
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
 const dtFormatOpts = { dateStyle: 'short', timeStyle: 'short' };
 
-const formatDateTimes = mode => {
-    if (!mode) mode = 'localized';
+const formatDateTimes = format => {
     // select only elements which haven't been initialized before, those without data-localized
     document.querySelectorAll("time[datetime]:not([data-localized])").forEach($el => {
         const date = new Date($el.getAttribute("datetime"));
         // initialize and set localized attribute
         $el.dataset.localized = new Intl.DateTimeFormat('default', dtFormatOpts).format(date);
         // set text to chosen mode
+        const mode = format || $el.dataset.initial;
         if ($el.dataset[mode]) $el.innerText = $el.dataset[mode];
     });
 };
@@ -139,7 +139,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     
     // Time Format
-    delegate('click', '#switchTimeFormat', switchTimeFormat);
+    delegate('click', '.switch-time-format', switchTimeFormat);
 
     // Theme Switch
     delegate('click', '.btcpay-theme-switch', e => {
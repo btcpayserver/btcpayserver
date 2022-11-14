01e0e7ed2433a5a05a8ee8729600aad2222bb5f9
Dennis Reimann <mail@dennisreimann.de>
2022-11-14 12:25:19 +0100

    Remove theme switch

--- BTCPayServer/wwwroot/checkout-v2/checkout.js
+++ BTCPayServer/wwwroot/checkout-v2/checkout.js
@@ -11,17 +11,6 @@ const urlParams = {};
     }
 })();
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Theme Switch
-    delegate('click', '.btcpay-theme-switch', e => {
-        e.preventDefault()
-        const current = document.documentElement.getAttribute(THEME_ATTR) || COLOR_MODES[0]
-        const mode = current === COLOR_MODES[0] ? COLOR_MODES[1] : COLOR_MODES[0]
-        setColorMode(mode)
-        e.target.closest('.btcpay-theme-switch').blur()
-    })
-});
-
 Vue.directive('collapsible', {
     bind: function (el) {
         el.transitionDuration = 350;
@@ -42,8 +31,7 @@ Vue.directive('collapsible', {
                         el.classList.add('show');
                     }, el.transitionDuration)
                 }, 0);
-            }
-            else {
+            } else {
                 el.style.height = window.getComputedStyle(el).height;
                 el.classList.remove('collapse');
                 el.classList.remove('show');
@@ -52,7 +40,7 @@ Vue.directive('collapsible', {
                 el.classList.add('collapsing');
                 setTimeout(() => {
                     el.classList.add('collapse');
-                    el.classList.remove("collapsing");
+                    el.classList.remove('collapsing');
                 }, el.transitionDuration)
             }
         }

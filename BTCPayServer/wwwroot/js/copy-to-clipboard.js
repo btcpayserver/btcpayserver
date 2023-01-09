bb733c581159e413f4fa9b57f92da58dff0afae9
d11n <mail@dennisreimann.de>
2023-01-09 16:38:03 +0100

    Unify 2FA login boxes (#4506)
    * Unify 2FA login boxes
    
    * 2FA setup: Make auth code copyable

--- BTCPayServer/wwwroot/js/copy-to-clipboard.js
+++ BTCPayServer/wwwroot/js/copy-to-clipboard.js
@@ -4,9 +4,14 @@ function confirmCopy(el, message) {
         el.style.minWidth = el.getBoundingClientRect().width + 'px';
     }
     el.innerHTML = `<span class="text-success">${message}</span>`;
-    setTimeout(function () {
+    if (el.dataset.clipboardHandler) {
+        clearTimeout(parseInt(el.dataset.clipboardHandler));
+    }
+    const timeoutId = setTimeout(function () {
         el.innerHTML = el.dataset.clipboardInitial;
+        el.dataset.clipboardHandler = null;
     }, 2500);
+    el.dataset.clipboardHandler = timeoutId.toString();
 }
 
 window.copyToClipboard = async function (e, data) {
@@ -48,13 +53,14 @@ window.copyUrlToClipboard = function (e) {
 
 document.addEventListener("DOMContentLoaded", function () {
     delegate('click', '[data-clipboard]', function (e) {
-        const data = e.target.closest('[data-clipboard]').getAttribute('data-clipboard')
+        const target = e.target.closest('[data-clipboard]');
+        const data = target.getAttribute('data-clipboard') ||  target.innerText || target.value;
         window.copyToClipboard(e, data)
     })
     delegate('click', '[data-clipboard-target]', function (e) {
-        const selector = e.target.closest('[data-clipboard-target]').getAttribute('data-clipboard-target')
+        const selector = e.target.closest('[data-clipboard-target]').getAttribute('data-clipboard-target');
         const target = document.querySelector(selector)
-        const data = target.innerText
+        const data = target.innerText || target.value;
         window.copyToClipboard(e, data)
     })
 })

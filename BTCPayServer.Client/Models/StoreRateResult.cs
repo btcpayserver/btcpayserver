1b42454486b248c8484ea479bcd0f3fe172cdad2
nicolas.dorier <nicolas.dorier@gmail.com>
2023-02-08 16:40:18 +0900

    Greenfield: Currency rate should be strings

--- BTCPayServer.Client/Models/StoreRateResult.cs
+++ BTCPayServer.Client/Models/StoreRateResult.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using BTCPayServer.JsonConverters;
+using Newtonsoft.Json;
 
 namespace BTCPayServer.Client.Models;
 
 public class StoreRateResult
 {
     public string CurrencyPair { get; set; }
+    [JsonConverter(typeof(NumericStringJsonConverter))]
     public decimal? Rate { get; set; }
     public List<string> Errors { get; set; }
 }

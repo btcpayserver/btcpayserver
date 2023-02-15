a5ff655eed67aed841058965638e627f36d62d6b
nicolas.dorier <nicolas.dorier@gmail.com>
2023-02-15 14:28:34 +0900

    Fix: If user get locked out, unlocking or deleting user fails
    This is due to the fact our UserService is a singleton, and it had a
    reference on UserManager which is scoped.
    
    UserManager is caching user entities at the scope level.
    UserService then had a view completely unsynchronized with the database.

--- BTCPayServer/Roles.cs
+++ BTCPayServer/Roles.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
 namespace BTCPayServer
 {
     public class Roles
     {
         public const string ServerAdmin = "ServerAdmin";
+        public static bool HasServerAdmin(IList<string> roles)
+        {
+            return roles.Contains(Roles.ServerAdmin, StringComparer.Ordinal);
+        }
     }
 }

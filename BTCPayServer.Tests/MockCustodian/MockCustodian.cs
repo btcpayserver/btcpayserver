587d32f88bd321395160a586452e803dd0681ecb
nicolas.dorier <nicolas.dorier@gmail.com>
2023-03-17 16:17:05 +0900

    Fix build

--- BTCPayServer.Tests/MockCustodian/MockCustodian.cs
+++ BTCPayServer.Tests/MockCustodian/MockCustodian.cs
@@ -56,14 +56,9 @@ public class MockCustodian : ICustodian, ICanDeposit, ICanTrade, ICanWithdraw
         return Task.FromResult(r);
     }
 
-    public Task<Form> GetConfigForm(JObject config, string locale, CancellationToken cancellationToken = default)
+    public Task<Form> GetConfigForm(CancellationToken cancellationToken = default)
     {
         return null;
-    }
-
-    public JObject cleanupConfigBeforeSave(JObject config)
-    {
-        return config;
     }
 
     public Task<DepositAddressData> GetDepositAddressAsync(string paymentMethod, JObject config, CancellationToken cancellationToken)

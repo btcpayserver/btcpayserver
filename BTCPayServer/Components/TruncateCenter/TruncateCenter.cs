de9ac9fd43542a766348cf5a89ed3bf0a895fd83
d11n <mail@dennisreimann.de>
2023-03-27 07:07:12 +0200

    Receipt: Add payment proof (#4782)
    * Receipt: Add payment proof
    
    Closes #4685.
    
    * shice
    
    * Add truncate-center component
    
    * Improve view
    
    * Hide button and link when printed
    
    * Describe component
    
    * Remove transaction ID from UI
    
    * Remove modification to interface
    
    ---------
    
    Co-authored-by: nicolas.dorier <nicolas.dorier@gmail.com>

---
+++ BTCPayServer/Components/TruncateCenter/TruncateCenter.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTCPayServer.Components.TruncateCenter;
+
+/// <summary>
+/// Truncates long strings in the center with ellipsis: Turns e.g. a BOLT11 into "lnbcrt7…q2ns60y"
+/// </summary>
+/// <param name="text">The full text, e.g. a Bitcoin address or BOLT11</param>
+/// <param name="link">Optional link, e.g. a block explorer URL</param>
+/// <param name="classes">Optional additional CSS classes</param>
+/// <param name="padding">The number of characters to show on each side</param>
+/// <param name="copy">Display a copy button</param>
+/// <returns>HTML with truncated string</returns>
+public class TruncateCenter : ViewComponent
+{
+    public IViewComponentResult Invoke(string text, string link = null, string classes = null, int padding = 7, bool copy = true)
+    {
+        var vm = new TruncateCenterViewModel
+        {
+            Classes = classes,
+            Padding = padding,
+            Copy = copy,
+            Text = text,
+            Link = link,
+            Truncated = text.Length > 2 * padding ? $"{text[..padding]}…{text[^padding..]}" : text
+        };
+        return View(vm);
+    }
+}

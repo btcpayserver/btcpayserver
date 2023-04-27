5d25b47b3e9523aed046b9831c56235ed7afac4b
Dennis Reimann <mail@dennisreimann.de>
2023-04-27 10:15:24 +0200

    Checkout v2: Improve truncation of displayed addresses

--- BTCPayServer/Components/TruncateCenter/TruncateCenterViewModel.cs
+++ BTCPayServer/Components/TruncateCenter/TruncateCenterViewModel.cs
@@ -3,10 +3,13 @@ namespace BTCPayServer.Components.TruncateCenter
     public class TruncateCenterViewModel
     {
         public string Text { get; set; }
-        public string Truncated { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
         public string Classes { get; set; }
         public string Link { get; set; }
         public int Padding { get; set; }
         public bool Copy { get; set; }
+        public bool Elastic { get; set; }
+        public bool IsVue { get; set; }
     }
 }

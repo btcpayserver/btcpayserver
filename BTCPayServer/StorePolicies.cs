7e8089db99799040dd9af29eeb0ece75f15f8435
Andrew Camilleri <evilkukka@gmail.com>
2023-05-22 13:28:04 +0200

    Update BTCPayServer/StorePolicies.cs
    Co-authored-by: d11n <mail@dennisreimann.de>

--- BTCPayServer/StorePolicies.cs
+++ BTCPayServer/StorePolicies.cs
@@ -9,13 +9,5 @@ namespace BTCPayServer
         public const string Owner = "Owner";
         [Obsolete("You should check authorization policies instead of roles")]
         public const string Guest = "Guest";
-        // public static IEnumerable<String> AllRoles
-        // {
-        //     get
-        //     {
-        //         yield return Owner;
-        //         yield return Guest;
-        //     }
-        // }
     }
 }

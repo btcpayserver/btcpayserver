e7768c9b5f113bddbf088f6498e6cb44b128082e
nicolas.dorier <nicolas.dorier@gmail.com>
2023-06-13 21:09:27 +0900

    Do not crash when an invoice have an amount that is too big

--- BTCPayServer/Security/GreenField/GreenFieldConstants.cs
+++ BTCPayServer/Security/GreenField/GreenFieldConstants.cs
@@ -2,6 +2,7 @@ namespace BTCPayServer.Security.Greenfield
 {
     public static class GreenfieldConstants
     {
+        public const decimal MaxAmount = ulong.MaxValue;
         public const string AuthenticationType = "Greenfield";
 
         public static class ClaimTypes

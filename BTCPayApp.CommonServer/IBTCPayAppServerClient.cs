f4cf4a7e4a05eac36eee321ce624995ff77c2082
Kukks <evilkukka@gmail.com>
2023-07-06 15:07:28 +0200

    wip

--- BTCPayApp.CommonServer/IBTCPayAppServerClient.cs
+++ BTCPayApp.CommonServer/IBTCPayAppServerClient.cs
@@ -1,7 +1,19 @@
-﻿namespace BTCPayApp.CommonServer
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace BTCPayApp.CommonServer
 {
     public interface IBTCPayAppServerClient
     {
-        
+        Task<string> ClientMethod1(string user, string message);
+        Task ClientMethod2();
+        Task<string> ClientMethod3(string user, string message, CancellationToken cancellationToken); // ca
     }
+    
+    public interface IBTCPayAppServerHub
+    {
+        Task<string> SendMessage(string user, string message);
+        Task SomeHubMethod();
+    }
+
 }

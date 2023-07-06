70d7f55378a055746e499bc8ec38918ad95a825a
Kukks <evilkukka@gmail.com>
2023-07-06 15:07:28 +0200

    wip

--- BTCPayServer/App/BTCPayAppHub.cs
+++ BTCPayServer/App/BTCPayAppHub.cs
@@ -1,8 +1,21 @@
-﻿using BTCPayApp.CommonServer;
+﻿using System;
+using System.Threading.Tasks;
+using BTCPayApp.CommonServer;
 using Microsoft.AspNetCore.SignalR;
 
 namespace BTCPayServer.Controllers;
 
-public class BTCPayAppHub : Hub<IBTCPayAppServerClient>
+public class BTCPayAppHub : Hub<IBTCPayAppServerClient>, IBTCPayAppServerHub
 {
+    public async Task<string> SendMessage(string user, string message)
+    {
+        await Clients.All.ClientMethod1(user, message);
+        return $"[Success] Call SendMessage : {user}, {message}";
+    }
+
+    public async Task SomeHubMethod()
+    {
+        await Clients.Caller.ClientMethod2();
+    }
 }
+

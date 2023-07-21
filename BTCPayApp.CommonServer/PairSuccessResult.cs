5b498416ef14e658c9634bcfa19d3b3f2f55334d
Kukks <evilkukka@gmail.com>
2023-07-21 15:35:13 +0200

    wip

--- BTCPayApp.CommonServer/PairSuccessResult.cs
+++ BTCPayApp.CommonServer/PairSuccessResult.cs
@@ -3,7 +3,7 @@
     public class PairSuccessResult
     {
         public string Key { get; set; }
-        public string StoreId { get; set; }
+        public string? StoreId { get; set; }
         public string UserId { get; set; }
 
         public string? ExistingWallet { get; set; }

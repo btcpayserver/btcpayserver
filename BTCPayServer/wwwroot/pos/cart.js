88c66f30f2249980bc270ca384e97c7b59257694
Kukks <evilkukka@gmail.com>
2023-07-28 21:40:47 +0200

    fixes #5204

--- BTCPayServer/wwwroot/pos/cart.js
+++ BTCPayServer/wwwroot/pos/cart.js
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded",function () {
                 return this.cart.reduce((res, item) => res + (parseInt(item.count) || 0), 0)
             },
             amountNumeric () {
-                return parseFloat(this.cart.reduce((res, item) => res + item.price * item.count, 0).toFixed(this.currencyInfo.divisibility))
+                return parseFloat(this.cart.reduce((res, item) => res + (item.price||0) * item.count, 0).toFixed(this.currencyInfo.divisibility))
             },
             posdata () {
                 const data = {

8ff8ff5f79e0dd8edfdb7bf332710814aea1be0f
Vincent Bouzon <vincent.bouzon@live.fr>
2023-08-30 02:42:40 +0200

    Display wallet balance in default currency

--- BTCPayServer/Components/WalletNav/WalletNav.cs
+++ BTCPayServer/Components/WalletNav/WalletNav.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,8 +8,10 @@ using BTCPayServer.Data;
 using BTCPayServer.Models.StoreViewModels;
 using BTCPayServer.Payments;
 using BTCPayServer.Payments.Lightning;
+using BTCPayServer.Services;
 using BTCPayServer.Services.Apps;
 using BTCPayServer.Services.Invoices;
+using BTCPayServer.Services.Rates;
 using BTCPayServer.Services.Stores;
 using BTCPayServer.Services.Wallets;
 using Microsoft.AspNetCore.Identity;
@@ -23,16 +26,22 @@ namespace BTCPayServer.Components.WalletNav
     {
         private readonly BTCPayWalletProvider _walletProvider;
         private readonly UIWalletsController _walletsController;
+        private readonly CurrencyNameTable _currencies;
         private readonly BTCPayNetworkProvider _networkProvider;
+        private readonly IRateProvider _rateProvider;
 
         public WalletNav(
             BTCPayWalletProvider walletProvider,
             BTCPayNetworkProvider networkProvider,
-            UIWalletsController walletsController)
+            UIWalletsController walletsController,
+            CurrencyNameTable currencies,
+            IRateProvider rateProvider)
         {
             _walletProvider = walletProvider;
             _networkProvider = networkProvider;
             _walletsController = walletsController;
+            _currencies = currencies;
+            _rateProvider = rateProvider;
         }
 
         public async Task<IViewComponentResult> InvokeAsync(WalletId walletId)
@@ -41,13 +50,29 @@ namespace BTCPayServer.Components.WalletNav
             var network = _networkProvider.GetNetwork<BTCPayNetwork>(walletId.CryptoCode);
             var wallet = _walletProvider.GetWallet(network);
             var derivation = store.GetDerivationSchemeSettings(_networkProvider, walletId.CryptoCode);
-            var balance = await _walletsController.GetBalanceString(wallet, derivation?.AccountDerivation);
+            var defaultCurrency = store.GetStoreBlob().DefaultCurrency;
+            var balance = (await wallet.GetBalance(derivation?.AccountDerivation)) switch
+            {
+                { Available: null, Total: var total } => total,
+                { Available: var available } => available
+            };
+
+            decimal? balanceDefaultCurrency = null;
+            if (defaultCurrency != network.CryptoCode)
+            {
+                var rate = (await _rateProvider.GetRatesAsync(default)).FirstOrDefault(pair => pair.CurrencyPair.Right == defaultCurrency && pair.CurrencyPair.Left == network.CryptoCode);
+
+                if (rate != null)
+                    balanceDefaultCurrency = balance.GetValue() * rate.BidAsk.Bid;
+            }
 
             var vm = new WalletNavViewModel
             {
                 WalletId = walletId,
                 Network = network,
-                Balance = balance,
+                Balance = balance.ShowMoney(network),
+                BalanceDefaultCurrency = balanceDefaultCurrency,
+                DefaultCurrency = defaultCurrency,
                 Label = derivation?.Label ?? $"{store.StoreName} {walletId.CryptoCode} Wallet"
             };
 
--- BTCPayServer/Components/WalletNav/WalletNavViewModel.cs
+++ BTCPayServer/Components/WalletNav/WalletNavViewModel.cs
@@ -6,5 +6,7 @@ namespace BTCPayServer.Components.WalletNav
         public BTCPayNetwork Network { get; set; }
         public string Label { get; set; }
         public string Balance { get; set; }
+        public decimal? BalanceDefaultCurrency { get; set; }
+        public string DefaultCurrency { get; set; }
     }
 }

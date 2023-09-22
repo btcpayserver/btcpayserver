378210e53eff6b36f5b63b9acda4f49bd9f88fd3
d11n <mail@dennisreimann.de>
2023-09-22 11:11:47 +0200

    Update BTCPayServer.Tests/ThirdPartyTests.cs

--- BTCPayServer.Tests/ThirdPartyTests.cs
+++ BTCPayServer.Tests/ThirdPartyTests.cs
@@ -331,7 +331,7 @@ retry:
                     .Select(c => new CurrencyPair(c.CryptoCode, "USD"))
                     .ToHashSet();
 
-            string[] brokenShitcoins = {};
+            string[] brokenShitcoins = { "BTX" };
             bool IsBrokenShitcoin(CurrencyPair p) => brokenShitcoins.Contains(p.Left) || brokenShitcoins.Contains(p.Right);
             foreach (var _ in brokenShitcoins)
             {

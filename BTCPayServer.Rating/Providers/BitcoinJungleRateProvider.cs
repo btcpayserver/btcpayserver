697cbbc3f3a26dc4b39939d846054f3a38931eb8
Lee Salminen <leesalminen@gmail.com>
2023-10-05 12:31:21 -0600

    set to bull bitcoin

--- BTCPayServer.Rating/Providers/BitcoinJungleRateProvider.cs
+++ BTCPayServer.Rating/Providers/BitcoinJungleRateProvider.cs
@@ -14,7 +14,7 @@ namespace BTCPayServer.Services.Rates
 {
     public class BitcoinJungleRateProvider : IRateProvider
     {
-        public RateSourceInfo RateSourceInfo => new("bitcoinjungle", "BitcoinJungle", "https://orders.bitcoinjungle.app/price");
+        public RateSourceInfo RateSourceInfo => new("bitcoinjungle", "BitcoinJungle", "https://orders.bitcoinjungle.app/ticker");
         private readonly HttpClient _httpClient;
         public BitcoinJungleRateProvider(HttpClient httpClient)
         {
@@ -23,12 +23,12 @@ namespace BTCPayServer.Services.Rates
 
         public async Task<PairRate[]> GetRatesAsync(CancellationToken cancellationToken)
         {
-            var response = await _httpClient.GetAsync("https://orders.bitcoinjungle.app/price", cancellationToken);
+            var response = await _httpClient.GetAsync("https://orders.bitcoinjungle.app/ticker", cancellationToken);
             response.EnsureSuccessStatusCode();
             var jobj = await response.Content.ReadAsAsync<JObject>(cancellationToken);
             var list = new List<PairRate>();
-            var value = jobj["BTCCRC"].Value<decimal>();
-            var usdValue = jobj["BTCUSD"].Value<decimal>();
+            var value = jobj["BTCCRC"]["fromToPrice"].Value<decimal>();
+            var usdValue = jobj["BTCUSD"]["fromToPrice"].Value<decimal>();
 
             list.Add(new PairRate(new CurrencyPair("BTC", "CRC"), new BidAsk(value)));
             list.Add(new PairRate(new CurrencyPair("BTC", "USD"), new BidAsk(usdValue)));

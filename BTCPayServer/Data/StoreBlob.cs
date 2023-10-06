d8422a979f921befa52f042fc6b847d7bc9e28bc
Andrew Camilleri <evilkukka@gmail.com>
2023-10-06 09:08:50 +0200

    Fix number of rates (#5365)
    * Ripio had api changed
    * Exchange rate host now requires an api key so removed
    * Removed unused argoneum rate provider code
    * switched cop and ugx to yadio
    * bumped exchange sharp lib as poloniex api changed and rate source was not working

--- BTCPayServer/Data/StoreBlob.cs
+++ BTCPayServer/Data/StoreBlob.cs
@@ -197,11 +197,11 @@ namespace BTCPayServer.Data
             { "GBP", "kraken" },
             { "CHF", "kraken" },
             { "GTQ", "bitpay" },
-            { "COP", "exchangeratehost" },
+            { "COP", "yadio" },
             { "ARS", "yadio" },
             { "JPY", "bitbank" },
             { "TRY", "btcturk" },
-            { "UGX", "exchangeratehost"},
+            { "UGX", "yadio"},
             { "RSD", "bitpay"}
         };
 

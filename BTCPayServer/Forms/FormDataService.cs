e73d49b53b60d1611457f59d4b1672b1723b884d
Kukks <evilkukka@gmail.com>
2023-11-10 13:16:47 +0100

    Support adjusting form invoice amount by multiplier
    closes #5425

--- BTCPayServer/Forms/FormDataService.cs
+++ BTCPayServer/Forms/FormDataService.cs
@@ -154,14 +154,16 @@ public class FormDataService
         var amtRaw = GetValue(form, $"{InvoiceParameterPrefix}amount");
         var amt = string.IsNullOrEmpty(amtRaw) ? (decimal?) null : decimal.Parse(amtRaw, CultureInfo.InvariantCulture);
         var adjustmentAmount = 0m;
-        foreach (var adjustmentField in form.GetAllFields().Where(f => f.FullName.StartsWith($"{InvoiceParameterPrefix}amount_adjustment")))
+        foreach (var f in form.GetAllFields())
         {
-            if (!decimal.TryParse(GetValue(form, adjustmentField.Field), out var adjustment))
+            if (f.FullName.StartsWith($"{InvoiceParameterPrefix}amount_adjustment") && decimal.TryParse(GetValue(form, f.Field), out var adjustment))
             {
-                continue;
+                adjustmentAmount += adjustment;
+            } 
+            if (f.FullName.StartsWith($"{InvoiceParameterPrefix}amount_multiply_adjustment") && decimal.TryParse(GetValue(form, f.Field), out var adjustmentM))
+            {
+                adjustmentAmount *= adjustmentM;
             }
-
-            adjustmentAmount += adjustment;
         }
 
         if (amt is null && adjustmentAmount > 0)

3ab2968c960748f7c7577f356733136ce89a0188
Dennis Reimann <mail@dennisreimann.de>
2024-01-23 18:57:45 +0100

    Cleanups

--- BTCPayServer/Services/WalletFileParsing/NBXDerivGenericWalletFileParser.cs
+++ BTCPayServer/Services/WalletFileParsing/NBXDerivGenericWalletFileParser.cs
@@ -1,13 +1,11 @@
 #nullable enable
-using System;
 using System.Diagnostics.CodeAnalysis;
-using BTCPayServer;
 namespace BTCPayServer.Services.WalletFileParsing;
 public class NBXDerivGenericWalletFileParser : IWalletFileParser
 {
     public bool TryParse(BTCPayNetwork network, string data, [MaybeNullWhen(false)] out DerivationSchemeSettings derivationSchemeSettings)
     {
-        derivationSchemeSettings = BTCPayServer.DerivationSchemeSettings.Parse(data, network);
+        derivationSchemeSettings = DerivationSchemeSettings.Parse(data, network);
         derivationSchemeSettings.Source = "Generic";
         return true;
     }

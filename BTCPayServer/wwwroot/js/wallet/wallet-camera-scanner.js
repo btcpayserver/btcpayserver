7701d115c946dd03166f694f8d997213ef74e6de
Dennis Reimann <mail@dennisreimann.de>
2024-03-19 13:27:09 +0100

    Wallet UI quick wins
    - Unify single/multiple inputs display
    - Destination: Move labels above amount
    - Coin Selection: Add sorting by amount and confirmations, add page size. Closes #5850
    - Move PSBT and BIP21 buttons up to input related button group
    - Turn checkboxes in Advanced Settings into toggles
    - Improve spacings and button groups

--- BTCPayServer/wwwroot/js/wallet/wallet-camera-scanner.js
+++ BTCPayServer/wwwroot/js/wallet/wallet-camera-scanner.js
@@ -1,6 +1,15 @@
-$(function () {
-    initCameraScanningApp("Scan address/ payment link", data => {
-        $("#BIP21").val(data);
-        $("form").submit();
+window.addEventListener("load", () => {
+    let $input = null;
+    initCameraScanningApp("Scan address or payment link", data => {
+        if (data.includes('?') || $input == null) {
+            document.getElementById("BIP21").value = data;
+            document.getElementById("SendForm").submit();
+        } else {
+            $input.value = data;
+        }
     }, "scanModal");
+    document.getElementById('scanModal').addEventListener('show.bs.modal', e => {
+        const { index } = e.relatedTarget.dataset;
+        $input = index ? document.querySelector(`[name="Outputs[${index}].DestinationAddress"]`) : null;
+    });
 });

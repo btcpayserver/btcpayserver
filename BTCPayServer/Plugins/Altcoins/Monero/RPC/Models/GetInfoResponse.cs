a2df9ed44c8c123b52df7bd004c55dfb1e0c5655
Jabster28 <29015942+Jabster28@users.noreply.github.com>
2024-04-04 09:38:41 +0100

    fix: switch to using get_info for monerod (#5885)

--- BTCPayServer/Plugins/Altcoins/Monero/RPC/Models/GetInfoResponse.cs
+++ BTCPayServer/Plugins/Altcoins/Monero/RPC/Models/GetInfoResponse.cs
@@ -3,10 +3,10 @@ using Newtonsoft.Json;
 
 namespace BTCPayServer.Services.Altcoins.Monero.RPC.Models
 {
-    public partial class SyncInfoResponse
+    public partial class GetInfoResponse
     {
         [JsonProperty("height")] public long Height { get; set; }
-        [JsonProperty("peers")] public List<Peer> Peers { get; set; }
+        [JsonProperty("busy_syncing")] public bool BusySyncing { get; set; }
         [JsonProperty("status")] public string Status { get; set; }
         [JsonProperty("target_height")] public long? TargetHeight { get; set; }
     }

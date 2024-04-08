13f8a30538874ad57f94bb17cf41213132791e77
Kukks <evilkukka@gmail.com>
2024-04-08 15:49:34 +0200

    add common classes

---
+++ BTCPayApp.CommonServer/AccessTokenResult.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace BTCPayApp.CommonServer;
+
+public class AccessTokenResult(string accessToken, string refreshToken, DateTimeOffset expiry)
+{
+    public string AccessToken { get; init; } = accessToken;
+    public string RefreshToken { get; init; } = refreshToken;
+    public DateTimeOffset Expiry { get; init; } = expiry;
+}

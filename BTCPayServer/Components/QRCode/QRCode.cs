7e92048374b54e6766fd91076fc03e09d594e342
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2024-04-09 19:35:03 +0100

    Expose QR code generator as htmlstring in order to use it in payroll plugin

--- BTCPayServer/Components/QRCode/QRCode.cs
+++ BTCPayServer/Components/QRCode/QRCode.cs
@@ -20,5 +20,15 @@ namespace BTCPayServer.Components.QRCode
             return new HtmlContentViewComponentResult(new HtmlString(
                 $"<img style=\"image-rendering:pixelated;image-rendering:-moz-crisp-edges;min-width:{size}px;min-height:{size}px\" src=\"data:image/png;base64,{b64}\" class=\"qr-code\" />"));
         }
+
+        public HtmlString InvokeHtmlString(string data, int size)
+        {
+            var qrCodeData = _qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new PngByteQRCode(qrCodeData);
+            var bytes = qrCode.GetGraphic(5, new byte[] { 0, 0, 0, 255 }, new byte[] { 0xf5, 0xf5, 0xf7, 255 });
+            var b64 = Convert.ToBase64String(bytes);
+            return new HtmlString(
+                $"<img style=\"image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:{size}px;height:{size}px\" src=\"data:image/png;base64,{b64}\" class=\"qr-code\" />");
+        }
     }
 }

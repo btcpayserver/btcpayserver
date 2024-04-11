5d1413f733b168ec8c34f0370ad878591d287fc4
Dennis Reimann <mail@dennisreimann.de>
2024-04-11 11:06:08 +0200

    Support registration

---
+++ BTCPayApp.CommonServer/AppInstanceInfo.cs
@@ -0,0 +1,12 @@
+namespace BTCPayApp.CommonServer;
+
+public class AppInstanceInfo
+{
+    public string BaseUrl { get; set; }
+    public string ServerName { get; set; } = "BTCPay Server";
+    public string? ContactUrl { get; set; }
+    public string? LogoUrl { get; set; }
+    public string? CustomThemeCssUrl { get; set; }
+    public string? CustomThemeExtension { get; set; }
+    public bool RegistrationEnabled { get; set; }
+}
---
+++ BTCPayApp.CommonServer/SignupResult.cs
@@ -0,0 +1,8 @@
+namespace BTCPayApp.CommonServer;
+
+public class SignupResult
+{
+    public string Email { get; set; }
+    public bool RequiresConfirmedEmail { get; set; }
+    public bool RequiresUserApproval { get; set; }
+}

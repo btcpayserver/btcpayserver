553e560b75f944c9803f10892d196ef7b3d8c1a2
Dennis Reimann <mail@dennisreimann.de>
2024-04-11 11:06:08 +0200

    Support registration

--- BTCPayServer/Services/ServerSettings.cs
+++ BTCPayServer/Services/ServerSettings.cs
@@ -1,13 +1,11 @@
 using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
 
 namespace BTCPayServer.Services;
 
 public class ServerSettings
 {
     [Display(Name = "Server Name")]
-    public string ServerName { get; set; }
+    public string ServerName { get; set; } = "BTCPay Server";
 
     [Display(Name = "Contact URL")]
     public string ContactUrl { get; set; }

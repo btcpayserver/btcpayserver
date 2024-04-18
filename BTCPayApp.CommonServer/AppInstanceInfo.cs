abe7d489b4b75e1ced096f7736851163f6bcebb9
Dennis Reimann <mail@dennisreimann.de>
2024-04-18 10:38:56 +0200

    Unset default server name

--- BTCPayApp.CommonServer/AppInstanceInfo.cs
+++ BTCPayApp.CommonServer/AppInstanceInfo.cs
@@ -3,7 +3,7 @@ namespace BTCPayApp.CommonServer;
 public class AppInstanceInfo
 {
     public string BaseUrl { get; set; }
-    public string ServerName { get; set; } = "BTCPay Server";
+    public string ServerName { get; set; }
     public string? ContactUrl { get; set; }
     public string? LogoUrl { get; set; }
     public string? CustomThemeCssUrl { get; set; }

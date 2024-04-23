btcpayserver/btcpayserver

commit 33d3a25928fb16810c7e48a8ee6c5817495ccff6

Apps: Don't redirect .onion requests to canonical domain (#5776)
Fixes #5729.

BTCPayServer/Filters/DomainMappingConstraintAttribute.cs
@@ -1,6 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using BTCPayServer.Abstractions.Extensions;
 using BTCPayServer.Services;
 using Microsoft.AspNetCore.Mvc.ActionConstraints;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,7 +25,7 @@ namespace BTCPayServer.Filters
         {
             var req = context.RouteContext.HttpContext.Request;
             var policies = context.RouteContext.HttpContext.RequestServices.GetService<PoliciesSettings>();
-            var mapping = policies?.DomainToAppMapping?.ToList() ?? new List<PoliciesSettings.DomainToAppMappingItem>();
+            var mapping = policies?.DomainToAppMapping?.ToList() ?? [];
             if (policies is { RootAppId: { } rootAppId, RootAppType: { } rootAppType })
             {
                 mapping.Add(new PoliciesSettings.DomainToAppMappingItem
@@ -37,7 +37,7 @@ namespace BTCPayServer.Filters
             }
 
             // If we have an appId, we can redirect to the canonical domain
-            if ((string)context.RouteContext.RouteData.Values["appId"] is string appId)
+            if ((string)context.RouteContext.RouteData.Values["appId"] is { } appId && !req.IsOnion())
             {
                 var redirectDomain = mapping.FirstOrDefault(item => item.AppId == appId)?.Domain;
                 // App is accessed via path, redirect to canonical domain

commit eda70227171854d1cf9d87c04766b951786a2a18

Domain mapping constraint: Fix .onion case
Fixes #5917, which is a regression introduced in #5776. The Tor-check must happen to prevent redirecting, but we must still return true if we already resolved an `appId`.

BTCPayServer/Filters/DomainMappingConstraintAttribute.cs
@@ -37,11 +37,11 @@ namespace BTCPayServer.Filters
             }
 
             // If we have an appId, we can redirect to the canonical domain
-            if ((string)context.RouteContext.RouteData.Values["appId"] is { } appId && !req.IsOnion())
+            if ((string)context.RouteContext.RouteData.Values["appId"] is { } appId)
             {
                 var redirectDomain = mapping.FirstOrDefault(item => item.AppId == appId)?.Domain;
                 // App is accessed via path, redirect to canonical domain
-                if (!string.IsNullOrEmpty(redirectDomain) && req.Method != "POST" && !req.HasFormContentType)
+                if (!string.IsNullOrEmpty(redirectDomain) && req.Method != "POST" && !req.HasFormContentType && !req.IsOnion())
                 {
                     var uri = new UriBuilder(req.Scheme, redirectDomain);
                     if (req.Host.Port.HasValue)
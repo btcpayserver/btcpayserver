6f7be969fca53853c308ff795a830407e9aaa5ca
Dennis Reimann <mail@dennisreimann.de>
2024-06-14 16:50:17 +0200

    Extract POSDataParser

---
+++ BTCPayServer.Abstractions/Converters/POSDataParser.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace BTCPayServer.Abstractions.Converters;
+
+public class PosDataParser
+{
+    public static Dictionary<string, object> ParsePosData(JToken? posData)
+    {
+        var result = new Dictionary<string, object>();
+        if (posData is JObject jobj)
+        {
+            foreach (var item in jobj)
+            {
+                ParsePosDataItem(item, ref result);
+            }
+        }
+        return result;
+    }
+
+    static void ParsePosDataItem(KeyValuePair<string, JToken?> item, ref Dictionary<string, object> result)
+    {
+        switch (item.Value?.Type)
+        {
+            case JTokenType.Array:
+                var items = item.Value.AsEnumerable().ToList();
+                var arrayResult = new List<object>();
+                for (var i = 0; i < items.Count; i++)
+                {
+                    arrayResult.Add(items[i] is JObject
+                        ? ParsePosData(items[i])
+                        : items[i].ToString());
+                }
+
+                result.TryAdd(item.Key, arrayResult);
+
+                break;
+            case JTokenType.Object:
+                result.TryAdd(item.Key, ParsePosData(item.Value));
+                break;
+            case null:
+                break;
+            default:
+                result.TryAdd(item.Key, item.Value.ToString());
+                break;
+        }
+    }
+}
--- BTCPayServer/Controllers/UIInvoiceController.UI.cs
+++ BTCPayServer/Controllers/UIInvoiceController.UI.cs
@@ -6,6 +6,7 @@ using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
 using BTCPayServer.Abstractions.Constants;
+using BTCPayServer.Abstractions.Converters;
 using BTCPayServer.Abstractions.Extensions;
 using BTCPayServer.Abstractions.Models;
 using BTCPayServer.Client;
@@ -1342,50 +1343,6 @@ namespace BTCPayServer.Controllers
         private InvoiceEntity GetCurrentInvoice() => HttpContext.GetInvoiceData();
 
         private string GetUserId() => _UserManager.GetUserId(User)!;
-
-        public class PosDataParser
-        {
-            public static Dictionary<string, object> ParsePosData(JToken? posData)
-            {
-                var result = new Dictionary<string, object>();
-                if (posData is JObject jobj)
-                {
-                    foreach (var item in jobj)
-                    {
-                        ParsePosDataItem(item, ref result);
-                    }
-                }
-                return result;
-            }
-
-            static void ParsePosDataItem(KeyValuePair<string, JToken?> item, ref Dictionary<string, object> result)
-            {
-                switch (item.Value?.Type)
-                {
-                    case JTokenType.Array:
-                        var items = item.Value.AsEnumerable().ToList();
-                        var arrayResult = new List<object>();
-                        for (var i = 0; i < items.Count; i++)
-                        {
-                            arrayResult.Add(items[i] is JObject
-                                ? ParsePosData(items[i])
-                                : items[i].ToString());
-                        }
-
-                        result.TryAdd(item.Key, arrayResult);
-
-                        break;
-                    case JTokenType.Object:
-                        result.TryAdd(item.Key, ParsePosData(item.Value));
-                        break;
-                    case null:
-                        break;
-                    default:
-                        result.TryAdd(item.Key, item.Value.ToString());
-                        break;
-                }
-            }
-        }
 
         private SelectList GetPaymentMethodsSelectList(StoreData store)
         {

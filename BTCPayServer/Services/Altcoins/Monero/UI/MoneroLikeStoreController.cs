24c8a878aa6dd612b91298419ffb1c65b70758a8
napoly <napolytan@protonmail.com>
2024-06-23 21:42:19 +0200

    Fix Monero development environment with wallet load

--- BTCPayServer/Services/Altcoins/Monero/UI/MoneroLikeStoreController.cs
+++ BTCPayServer/Services/Altcoins/Monero/UI/MoneroLikeStoreController.cs
@@ -253,10 +253,28 @@ namespace BTCPayServer.Services.Altcoins.Monero.UI
                         }
                     }
 
+                    try
+                    {
+                        var response = await _MoneroRpcProvider.WalletRpcClients[cryptoCode].SendCommandAsync<OpenWalletRequest, OpenWalletResponse>("open_wallet", new OpenWalletRequest
+                        {
+                            Filename = "wallet",
+                            Password = viewModel.WalletPassword
+                        });
+                        if (response?.Error != null)
+                        {
+                            throw new Exception(response.Error.Message);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError(nameof(viewModel.AccountIndex), $"Could not open the wallet: {ex.Message}");
+                        return View(viewModel);
+                    }
+
                     return RedirectToAction(nameof(GetStoreMoneroLikePaymentMethod), new
                     {
                         cryptoCode,
-                        StatusMessage = "View-only wallet files uploaded. If they are valid the wallet will soon become available."
+                        StatusMessage = "View-only wallet files uploaded. The wallet will soon become available."
 
                     });
                 }
@@ -343,7 +361,7 @@ namespace BTCPayServer.Services.Altcoins.Monero.UI
             public IFormFile WalletKeysFile { get; set; }
             [Display(Name = "Wallet Password")]
             public string WalletPassword { get; set; }
-            [Display(Name = "Consider the invoice settled when the payment transaction ï¿½")]
+            [Display(Name = "Consider the invoice settled when the payment transaction …")]
             public MoneroLikeSettlementThresholdChoice SettlementConfirmationThresholdChoice { get; set; }
             [Display(Name = "Required Confirmations"), Range(0, 100)]
             public long? CustomSettlementConfirmationThreshold { get; set; }

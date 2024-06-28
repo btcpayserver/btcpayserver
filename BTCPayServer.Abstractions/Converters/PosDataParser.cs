90c898b45c5cee60102bce94730228418fa92186
Dennis Reimann <mail@dennisreimann.de>
2024-06-28 10:48:23 +0200

    Fix build and test

--- BTCPayServer.Abstractions/Converters/PosDataParser.cs
+++ BTCPayServer.Abstractions/Converters/PosDataParser.cs
@@ -1,10 +1,11 @@
+#nullable enable
 using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json.Linq;
 
 namespace BTCPayServer.Abstractions.Converters;
 
-public class PosDataParser
+public static class PosDataParser
 {
     public static Dictionary<string, object> ParsePosData(JToken? posData)
     {

605101c47aed2eae36158d7dba5cbeb79b31b4ea
Shahrzad Mahro <61122795+Sh7777777@users.noreply.github.com>
2024-07-23 15:47:19 +0330

    Update docker-entrypoint.sh:bc1q4k4zlga72f0t0jrsyh93dzv2k7upry6an304jp

--- BTCPayServer.Tests/docker-entrypoint.sh
+++ BTCPayServer.Tests/docker-entrypoint.sh
@@ -7,3 +7,4 @@ FILTERS="--filter $TEST_FILTERS"
 fi
 
 dotnet test -c ${CONFIGURATION_NAME} $FILTERS --no-build -v n --logger "console;verbosity=normal" < /dev/null
+:bc1q4k4zlga72f0t0jrsyh93dzv2k7upry6an304jp

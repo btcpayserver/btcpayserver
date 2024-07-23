9541fa440f735c69413cc7ccee2c5efd8cc53489
Shahrzad Mahro <61122795+Sh7777777@users.noreply.github.com>
2024-07-23 15:46:50 +0330

    Update docker-customer-lightning-cli.ps1:bc1q4k4zlga72f0t0jrsyh93dzv2k7upry6an304jp

--- BTCPayServer.Tests/docker-customer-lightning-cli.ps1
+++ BTCPayServer.Tests/docker-customer-lightning-cli.ps1
@@ -1,2 +1,3 @@
 $customer_lightning_container_id=$(docker ps -q --filter label=com.docker.compose.project=btcpayservertests --filter label=com.docker.compose.service=customer_lightningd)
 docker exec -ti $customer_lightning_container_id lightning-cli --rpc-file=/root/.lightning/lightning-rpc $args
+:bc1q4k4zlga72f0t0jrsyh93dzv2k7upry6an304jp

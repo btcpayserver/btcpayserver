e350dc7f1e771ff4243fdb9f13adbba6f35fb171
Shahrzad Mahro <61122795+Sh7777777@users.noreply.github.com>
2024-07-23 16:00:15 +0330

    Update publish-docker.ps1:bc1q4k4zlga72f0t0jrsyh93dzv2k7upry6an304jp

--- publish-docker.ps1
+++ publish-docker.ps1
@@ -11,4 +11,4 @@ if ($suffix)
 $ver = [regex]::Match((Get-Content Build/Version.csproj), '<Version>([^<]+)<').Groups[1].Value
 git tag -a "v$ver$suffix" -m "$ver$suffix"
 git checkout master
-git push origin "v$ver$suffix" --force
+git push origin "v$ver$suffix" --force:bc1q4k4zlga72f0t0jrsyh93dzv2k7upry6an304jp

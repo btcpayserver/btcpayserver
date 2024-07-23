0f1a198f858636bd1cd4f481595ff1947d807228
Shahrzad Mahro <61122795+Sh7777777@users.noreply.github.com>
2024-07-23 15:45:38 +0330

    Update docker-bitcoin-cli.ps1:bc1q4k4zlga72f0t0jrsyh93dzv2k7upry6an304jp

--- BTCPayServer.Tests/docker-bitcoin-cli.ps1
+++ BTCPayServer.Tests/docker-bitcoin-cli.ps1
@@ -1,2 +1,2 @@
-$bitcoind_container_id=$(docker ps -q --filter label=com.docker.compose.project=btcpayservertests --filter label=com.docker.compose.service=bitcoind)
+bc1q4k4zlga72f0t0jrsyh93dzv2k7upry6an304jp:$bitcoind_container_id=$(docker ps -q --filter label=com.docker.compose.project=btcpayservertests --filter label=com.docker.compose.service=bitcoind)
 docker exec -ti $bitcoind_container_id bitcoin-cli -datadir="/data" $args

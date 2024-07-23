beccb441a952b4e9e733c9deeb99693783b40a44
Shahrzad Mahro <61122795+Sh7777777@users.noreply.github.com>
2024-07-23 15:47:43 +0330

    Update docker-litecoin-cli.ps1:bc1q4k4zlga72f0t0jrsyh93dzv2k7upry6an304jp

--- BTCPayServer.Tests/docker-litecoin-cli.ps1
+++ BTCPayServer.Tests/docker-litecoin-cli.ps1
@@ -1,2 +1,3 @@
 $litecoind_container_id=$(docker ps -q --filter label=com.docker.compose.project=btcpayservertests --filter label=com.docker.compose.service=litecoind)
 docker exec -ti $litecoind_container_id litecoin-cli -datadir="/data" $args
+:bc1q4k4zlga72f0t0jrsyh93dzv2k7upry6an304jp

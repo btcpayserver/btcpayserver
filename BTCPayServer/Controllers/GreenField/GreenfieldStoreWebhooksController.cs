9a20a3f2c7d7a1525a404ff597d4866ceef990c4
Dennis Reimann <mail@dennisreimann.de>
2024-09-04 14:38:33 +0200

    Make Role Permissions more human legible
    Had to rename `CanModifyStoreWebhooks` to `CanModifyWebhooks` for this, but the value stayed the same, so I don't think it's a big deal.
    
    Closes #6183.

--- BTCPayServer/Controllers/GreenField/GreenfieldStoreWebhooksController.cs
+++ BTCPayServer/Controllers/GreenField/GreenfieldStoreWebhooksController.cs
@@ -24,7 +24,7 @@ namespace BTCPayServer.Controllers.Greenfield
 {
     [ApiController]
     [Authorize(AuthenticationSchemes = AuthenticationSchemes.Greenfield,
-               Policy = Policies.CanModifyStoreWebhooks)]
+               Policy = Policies.CanModifyWebhooks)]
     [EnableCors(CorsPolicies.All)]
     public class GreenfieldStoreWebhooksController : ControllerBase
     {

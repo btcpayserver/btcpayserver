1530b5a6c7bd17820a52518ac8d7f06326b9f228
Hector Chu <hectorchu@gmail.com>
2024-09-19 23:56:18 +0100

    Fix build

--- BTCPayServer/Services/Altcoins/Litecoin/Services/MwebScannerService.cs
+++ BTCPayServer/Services/Altcoins/Litecoin/Services/MwebScannerService.cs
@@ -159,13 +159,13 @@ namespace BTCPayServer.Services.Altcoins.Litecoin.Services
         private async Task CheckInvoice(DerivationSchemeSettings derivationScheme, Utxo utxo, int height)
         {
             var network = networks.GetNetwork<BTCPayNetwork>("MWEB");
+            var pmi = PaymentTypes.CHAIN.GetPaymentMethodId(network.CryptoCode);
             var coin = await CoinFromUtxo(derivationScheme, utxo, height);
             if (coin == null) return;
             var key = network.GetTrackedDestination(coin.ScriptPubKey);
-            var invoice = (await invoiceRepository.GetInvoicesFromAddresses([key])).FirstOrDefault();
+            var invoice = await invoiceRepository.GetInvoiceFromAddress(pmi, key);
             if (invoice != null)
             {
-                var pmi = PaymentTypes.CHAIN.GetPaymentMethodId(network.CryptoCode);
                 var details = new BitcoinLikePaymentData(coin.OutPoint, false, coin.KeyPath)
                 {
                     ConfirmationCount = coin.Confirmations
@@ -175,7 +175,7 @@ namespace BTCPayServer.Services.Altcoins.Litecoin.Services
                 {
                     Id = coin.OutPoint.ToString(),
                     Created = DateTimeOffset.UtcNow,
-                    Status = NBXplorerListener.IsSettled(invoice, details) ? PaymentStatus.Settled : PaymentStatus.Processing,
+                    Status = NBXplorerListener.ConfirmationRequired(invoice, details) <= details.ConfirmationCount ? PaymentStatus.Settled : PaymentStatus.Processing,
                     Amount = ((Money)coin.Value).ToDecimal(MoneyUnit.BTC),
                     Currency = network.Currency
                 }.Set(invoice, handlers[pmi], details);
@@ -244,7 +244,7 @@ namespace BTCPayServer.Services.Altcoins.Litecoin.Services
 
         private async Task UpdatePaymentStates(int height)
         {
-            var invoices = await invoiceRepository.GetPendingInvoices(skipNoPaymentInvoices: true);
+            var invoices = await invoiceRepository.GetPendingInvoices();
             await Task.WhenAll(invoices.Select(i => UpdatePaymentStates(i, height)).ToArray());
         }
 
@@ -275,12 +275,8 @@ namespace BTCPayServer.Services.Altcoins.Litecoin.Services
                 }
 
                 var prevStatus = payment.Status;
-                payment.Status = NBXplorerListener.IsSettled(invoice, paymentData) ? PaymentStatus.Settled : PaymentStatus.Processing;
+                payment.Status = NBXplorerListener.ConfirmationRequired(invoice, paymentData) <= paymentData.ConfirmationCount ? PaymentStatus.Settled : PaymentStatus.Processing;
                 updated |= prevStatus != payment.Status;
-
-                // if needed add invoice back to pending to track number of confirmations
-                if (paymentData.ConfirmationCount < network.MaxTrackedConfirmation)
-                    await invoiceRepository.AddPendingInvoiceIfNotPresent(invoice.Id);
                 if (updated) updatedPaymentEntities.Add(payment);
             }
 

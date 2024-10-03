cf3755d963504510e761603c0a426f080070f90f
Dennis Reimann <mail@dennisreimann.de>
2024-10-03 13:22:47 +0200

    Do not ignore nullable values in JSON

--- BTCPayServer.Client/Models/AppItem.cs
+++ BTCPayServer.Client/Models/AppItem.cs
@@ -18,27 +18,16 @@ public class AppItem
     public string Id { get; set; }
     public string Title { get; set; }
     public bool Disabled { get; set; }
-
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public string Description { get; set; }
-
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public string[] Categories { get; set; }
-
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public string Image { get; set; }
 
     [JsonConverter(typeof(StringEnumConverter))]
     public AppItemPriceType PriceType { get; set; }
 
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     [JsonConverter(typeof(NumericStringJsonConverter))]
     public decimal? Price { get; set; }
-
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public string BuyButtonText { get; set; }
-
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public int? Inventory { get; set; }
 
     [JsonExtensionData]

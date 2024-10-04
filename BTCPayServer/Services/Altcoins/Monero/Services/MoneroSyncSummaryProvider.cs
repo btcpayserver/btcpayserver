2e114d7c29d583e3fb29e2063be863f356fd7f19
Nicolas Dorier <nicolas.dorier@gmail.com>
2024-10-04 16:58:31 +0900

    Remove references to cryptoCode in SyncStatus (#6275)

--- BTCPayServer/Services/Altcoins/Monero/Services/MoneroSyncSummaryProvider.cs
+++ BTCPayServer/Services/Altcoins/Monero/Services/MoneroSyncSummaryProvider.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using BTCPayServer.Abstractions.Contracts;
 using BTCPayServer.Client.Models;
+using BTCPayServer.Payments;
 
 namespace BTCPayServer.Services.Altcoins.Monero.Services
 {
@@ -24,13 +25,18 @@ namespace BTCPayServer.Services.Altcoins.Monero.Services
         {
             return _moneroRpcProvider.Summaries.Select(pair => new MoneroSyncStatus()
             {
-                Summary = pair.Value, CryptoCode = pair.Key
+                Summary = pair.Value, PaymentMethodId = PaymentMethodId.Parse(pair.Key)
             });
         }
     }
 
     public class MoneroSyncStatus: SyncStatus, ISyncStatus
     {
+        public new PaymentMethodId PaymentMethodId
+        {
+            get => PaymentMethodId.Parse(base.PaymentMethodId);
+            set => base.PaymentMethodId = value.ToString();
+        }
         public override bool Available
         {
             get

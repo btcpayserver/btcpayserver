5704919b3a6b60cd284c1162ff820147706c5bbc
Nicolas Dorier <nicolas.dorier@gmail.com>
2024-10-04 16:58:13 +0900

    BlockExplorer links should be using payment method ids (#6273)

--- BTCPayServer/Services/Altcoins/Zcash/UI/ZcashPaymentViewModel.cs
+++ BTCPayServer/Services/Altcoins/Zcash/UI/ZcashPaymentViewModel.cs
@@ -1,15 +1,17 @@
 using System;
+using BTCPayServer.Payments;
 
 namespace BTCPayServer.Services.Altcoins.Zcash.UI
 {
     public class ZcashPaymentViewModel
     {
-        public string Crypto { get; set; }
+        public PaymentMethodId PaymentMethodId { get; set; }
         public string Confirmations { get; set; }
         public string DepositAddress { get; set; }
         public string Amount { get; set; }
         public string TransactionId { get; set; }
         public DateTimeOffset ReceivedTime { get; set; }
         public string TransactionLink { get; set; }
+        public string Currency { get; set; }
     }
 }

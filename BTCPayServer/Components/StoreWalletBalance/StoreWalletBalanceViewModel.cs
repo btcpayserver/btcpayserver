2494bd23b8b53b704718a80869bd8ea4c5c78b28
Dennis Reimann <mail@dennisreimann.de>
2024-10-05 10:05:34 +0200

    Histograms: Add Lightning data and API endpoints
    Ported over from the mobile-working-branch.
    
    Adds histogram data for Lightning and exposes the wallet/lightning histogram data via the API. It also add a dashboard graph for the Lightning balance.
    
    Caveat: The Lightning histogram is calculated by using the current channel balance and going backwards through as much invoices and transactions as we have. The "start" of the LN graph data might not be accurate though. That's because we don't track (and not even have) the LN onchain data. It is calculated by using the current channel balance and going backwards through as much invoices and transactions as we have. So the historic graph data for LN is basically a best effort of trying to reconstruct it with what we have: The LN channel transactions.

--- BTCPayServer/Components/StoreWalletBalance/StoreWalletBalanceViewModel.cs
+++ BTCPayServer/Components/StoreWalletBalance/StoreWalletBalanceViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using BTCPayServer.Data;
 using BTCPayServer.Services.Rates;
@@ -14,6 +15,6 @@ public class StoreWalletBalanceViewModel
     public StoreData Store { get; set; }
     public WalletId WalletId { get; set; }
     public WalletHistogramType Type { get; set; }
-    public IList<string> Labels { get; set; }
+    public IList<DateTimeOffset> Labels { get; set; }
     public IList<decimal> Series { get; set; }
 }

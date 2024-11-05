dc51150663b66e699180d17b7a1023efb31804f7
Dennis Reimann <mail@dennisreimann.de>
2024-11-05 16:06:04 +0100

    Remove redendant Item model

--- BTCPayServer/Plugins/PointOfSale/Models/ViewPointOfSaleViewModel.cs
+++ BTCPayServer/Plugins/PointOfSale/Models/ViewPointOfSaleViewModel.cs
@@ -2,50 +2,13 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using BTCPayServer.Client.Models;
-using BTCPayServer.JsonConverters;
 using BTCPayServer.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
 
 namespace BTCPayServer.Plugins.PointOfSale.Models
 {
     public class ViewPointOfSaleViewModel
     {
-        public enum ItemPriceType
-        {
-            Topup,
-            Minimum,
-            Fixed
-        }
-        
-        public class Item
-        {
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public string Description { get; set; }
-            public string Id { get; set; }
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public string[] Categories { get; set; }
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public string Image { get; set; }
-            [JsonConverter(typeof(StringEnumConverter))]
-            public ItemPriceType PriceType { get; set; }
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            [JsonConverter(typeof(NumericStringJsonConverter))]
-            public decimal? Price { get; set; }
-            public string Title { get; set; }
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public string BuyButtonText { get; set; }
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public int? Inventory { get; set; } = null;
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public string[] PaymentMethods { get; set; }
-            public bool Disabled { get; set; } = false;
-            [JsonExtensionData]
-            public Dictionary<string, JToken> AdditionalData { get; set; }
-        }
-
         public StoreBrandingViewModel StoreBranding { get; set; }
         public string StoreName { get; set; }
         public NumberFormatInfo CurrencyInfo { get; set; }

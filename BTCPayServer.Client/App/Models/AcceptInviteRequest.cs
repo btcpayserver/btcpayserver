ab77e918451b132a617febcae6bed876af9db163
Dennis Reimann <mail@dennisreimann.de>
2025-01-16 12:36:16 +0100

    Move client models to app core

--- BTCPayServer.Client/App/Models/AcceptInviteRequest.cs
+++
@@ -1,8 +0,0 @@
-#nullable enable
-namespace BTCPayServer.Client.App.Models;
-
-public class AcceptInviteRequest
-{
-    public string? UserId { get; set; }
-    public string? Code { get; set; }
-}
--- BTCPayServer.Client/App/Models/ResetPasswordRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BTCPayServer.Client.App.Models;
-
-public class ResetPasswordRequest
-{
-    public string Email { get; set; }
-    public string ResetCode { get; set; }
-    public string NewPassword { get; set; }
-}

78e3e3f887dab4eef1bccdb6032ea4ebb38abdda
nicolas.dorier <nicolas.dorier@gmail.com>
2025-02-24 22:34:17 +0900

    Remove deprecated Shopify integration

--- BTCPayServer/Plugins/Shopify/ApiModels/TransactionsCreateResp.cs
+++
@@ -1,9 +0,0 @@
-using BTCPayServer.Plugins.Shopify.ApiModels.DataHolders;
-
-namespace BTCPayServer.Plugins.Shopify.ApiModels
-{
-    public class TransactionsCreateResp
-    {
-        public TransactionDataHolder transaction { get; set; }
-    }
-}

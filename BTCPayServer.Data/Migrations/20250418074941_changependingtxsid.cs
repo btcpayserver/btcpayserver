81522aeb8053ac275b7c0884cfd98b9eb6d4a7f1
nicolas.dorier <nicolas.dorier@gmail.com>
2025-04-18 17:02:31 +0900

    Fix: Recreating an aborted TX in MultiSig on Server setup crashes

---
+++ BTCPayServer.Data/Migrations/20250418074941_changependingtxsid.cs
@@ -0,0 +1,69 @@
+﻿using BTCPayServer.Data;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BTCPayServer.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20250418074941_changependingtxsid")]
+    public partial class changependingtxsid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PendingTransactions",
+                table: "PendingTransactions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TransactionId",
+                table: "PendingTransactions",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CryptoCode",
+                table: "PendingTransactions",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "PendingTransactions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.Sql(
+                """
+                UPDATE "PendingTransactions" SET "Id" =
+                    lpad(to_hex(trunc(random() * 1e10)::bigint), 8, '0') || '-' ||
+                    lpad(to_hex(trunc(random() * 1e10)::bigint), 4, '0') || '-' ||
+                    lpad(to_hex(trunc(random() * 1e10)::bigint), 4, '0') || '-' ||
+                    lpad(to_hex(trunc(random() * 1e10)::bigint), 4, '0') || '-' ||
+                    lpad(to_hex(trunc(random() * 1e10)::bigint), 12, '0');
+                """);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PendingTransactions",
+                table: "PendingTransactions",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PendingTransactions_TransactionId",
+                table: "PendingTransactions",
+                column: "TransactionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}

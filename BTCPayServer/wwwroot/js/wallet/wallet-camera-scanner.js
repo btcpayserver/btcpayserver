abd180f4d3f336b1233ba7fcc0279a5a8ae6c6fe
d11n <mail@dennisreimann.de>
2025-04-24 02:00:32 +0200

    QR scanner: Properly handle BIP21 input (#6693)
    * QR scanner: Properly handle BIP21 input
    
    Fixes #6665. Supersedes and closes #6672.
    
    Details [here](https://github.com/btcpayserver/btcpayserver/pull/6672#issuecomment-2809299548)
    
    * Update BTCPayServer/wwwroot/js/wallet/wallet-camera-scanner.js
    
    * Fix scanner Vue component

--- BTCPayServer/wwwroot/js/wallet/wallet-camera-scanner.js
+++ BTCPayServer/wwwroot/js/wallet/wallet-camera-scanner.js
@@ -1,15 +1,28 @@
 window.addEventListener("load", () => {
-    let $input = null;
+    let outputIndex = null;
     initCameraScanningApp("Scan address or payment link", data => {
-        if (data.includes('?') || $input == null) {
+        const bip21 = data.match(/bitcoin:(\w+)(?:\?(.*))?/i)
+        if (bip21 && outputIndex == null) {
             document.getElementById("BIP21").value = data;
             document.getElementById("SendForm").submit();
+        } else if (bip21 && bip21.length >= 2) {
+            document.querySelector(`[name="Outputs[${outputIndex}].DestinationAddress"]`).value = bip21[1];
+            if (bip21.length === 3) {
+                const p = new URLSearchParams(bip21[2]);
+                if (p.has('amount')) {
+                    document.querySelector(`[name="Outputs[${outputIndex}].Amount"]`).value = p.get('amount');
+                }
+            }
         } else {
-            $input.value = data;
+            document.querySelector(`[name="Outputs[${outputIndex}].DestinationAddress"]`).value = data;
         }
-    }, "scanModal");
-    document.getElementById('scanModal').addEventListener('show.bs.modal', e => {
+    }, 'scanModal');
+    const $scanModal = document.getElementById('scanModal');
+    $scanModal.addEventListener('show.bs.modal', e => {
         const { index } = e.relatedTarget.dataset;
-        $input = index ? document.querySelector(`[name="Outputs[${index}].DestinationAddress"]`) : null;
+        outputIndex = index ? index : null;
+    });
+    $scanModal.addEventListener('hide.bs.modal', e => {
+        outputIndex = null;
     });
 });

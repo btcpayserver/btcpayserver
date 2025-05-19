19336032bea3a9ba8a11c1a907c7837e854242a9
napoly <napolytan@protonmail.com>
2025-05-19 12:49:56 +0200

    BTCpayServerTester.WaitIsFullySynched should not wait only NBX

--- BTCPayServer.Tests/BTCPayServerTester.cs
+++ BTCPayServer.Tests/BTCPayServerTester.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using BTCPayServer.Abstractions.Constants;
+using BTCPayServer.Abstractions.Contracts;
 using BTCPayServer.Configuration;
 using BTCPayServer.HostedServices;
 using BTCPayServer.Hosting;
@@ -260,8 +261,8 @@ namespace BTCPayServer.Tests
 
         private async Task WaitIsFullySynched(CancellationToken cancellationToken)
         {
-            var dashBoard = GetService<NBXplorerDashboard>();
-            while (!dashBoard.IsFullySynched())
+            var o = GetService<IEnumerable<ISyncSummaryProvider>>().ToArray();
+            while (!o.All(d => d.AllAvailable()))
             {
                 await Task.Delay(10, cancellationToken).ConfigureAwait(false);
             }

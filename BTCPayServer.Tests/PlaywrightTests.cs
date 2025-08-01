b493aecb5a655f8c5f3ad4b508cec8d347c5a354
Abhijay007 <Abhijay007j@gmail.com>
2025-08-01 14:09:05 +0000

    (Test):Converted/Added Playwright Test for CanSigninWithLoginCode
    Signed-off-by: Abhijay007 <Abhijay007j@gmail.com>

--- BTCPayServer.Tests/PlaywrightTests.cs
+++ BTCPayServer.Tests/PlaywrightTests.cs
@@ -2892,7 +2892,45 @@ namespace BTCPayServer.Tests
             Assert.DoesNotContain(Policies.CanModifyServerSettings, await s.Page.ContentAsync());
         }
 
-     
+        [Fact]
+        [Trait("Playwright", "Playwright")]
+        public async Task CanSigninWithLoginCode()
+        {
+            await using var s = CreatePlaywrightTester();
+            await s.StartAsync();
+            var user = await s.RegisterNewUser();
+            await s.GoToHome();
+            await s.GoToProfile(ManageNavPages.LoginCodes);
+
+            string code = null;
+            await s.Page.WaitForSelectorAsync("#LoginCode .qr-code");
+            code = await s.Page.Locator("#LoginCode .qr-code").GetAttributeAsync("alt");
+            string prevCode = code;
+            await s.Page.ReloadAsync();
+            await s.Page.WaitForSelectorAsync("#LoginCode .qr-code");
+            code = await s.Page.Locator("#LoginCode .qr-code").GetAttributeAsync("alt");
+            Assert.NotEqual(prevCode, code);
+            await s.Page.WaitForSelectorAsync("#LoginCode .qr-code");
+            code = await s.Page.Locator("#LoginCode .qr-code").GetAttributeAsync("alt");
+            await s.Logout();
+            await s.GoToLogin();
+            await s.Page.EvaluateAsync("document.getElementById('LoginCode').value = 'bad code'");
+            await s.Page.EvaluateAsync("document.getElementById('logincode-form').submit()");
+            await s.Page.WaitForLoadStateAsync();
+
+            await s.GoToLogin();
+            await s.Page.EvaluateAsync($"document.getElementById('LoginCode').value = '{code}'");
+            await s.Page.EvaluateAsync("document.getElementById('logincode-form').submit()");
+            await s.Page.WaitForLoadStateAsync();
+            await s.Page.WaitForLoadStateAsync();
+            
+            await s.CreateNewStore();
+            await s.GoToHome();
+            await s.Page.WaitForLoadStateAsync();
+            await s.Page.WaitForLoadStateAsync();
+            var content = await s.Page.ContentAsync();
+            Assert.Contains(user, content);
+        }
     }
 }
 

8aaf771e9b581eb6bec2aefb84bfbc53c76652b9
Abhijay007 <Abhijay007j@gmail.com>
2025-09-01 18:12:20 +0000

    Add missing Display attributes to user management ViewModels
    Signed-off-by: Abhijay007 <Abhijay007j@gmail.com>

--- BTCPayServer/Models/AccountViewModels/SetPasswordViewModel.cs
+++ BTCPayServer/Models/AccountViewModels/SetPasswordViewModel.cs
@@ -6,10 +6,12 @@ namespace BTCPayServer.Models.AccountViewModels
     {
         [Required]
         [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
--- BTCPayServer/Models/ServerViewModels/UsersViewModel.cs
+++ BTCPayServer/Models/ServerViewModels/UsersViewModel.cs
@@ -12,18 +12,36 @@ namespace BTCPayServer.Models.ServerViewModels
         public class UserViewModel
         {
             public string Id { get; set; }
+            
+            [Display(Name = "Email")]
             public string Email { get; set; }
+            
+            [Display(Name = "Name")]
             public string Name { get; set; }
+            
             [Display(Name = "Invitation URL")]
             public string InvitationUrl { get; set; }
+            
             [Display(Name = "Image")]
             public IFormFile ImageFile { get; set; }
+            
             public string ImageUrl { get; set; }
+            
+            [Display(Name = "Email Confirmed")]
             public bool? EmailConfirmed { get; set; }
+            
+            [Display(Name = "Approved")]
             public bool? Approved { get; set; }
+            
+            [Display(Name = "Disabled")]
             public bool Disabled { get; set; }
+            
+            [Display(Name = "Is Admin")]
             public bool IsAdmin { get; set; }
+            
+            [Display(Name = "Created")]
             public DateTimeOffset? Created { get; set; }
+            
             public IEnumerable<string> Roles { get; set; }
             public IEnumerable<UserStore> Stores { get; set; }
         }

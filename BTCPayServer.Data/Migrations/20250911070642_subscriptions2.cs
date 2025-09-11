4b69c43fd9a7c44de4588196bbcceff85d3d48af
nicolas.dorier <nicolas.dorier@gmail.com>
2025-09-11 22:54:01 +0900

    dafaf

---
+++ BTCPayServer.Data/Migrations/20250911070642_subscriptions2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BTCPayServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class subscriptions2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "zone",
+                table: "subscription_members",
+                newName: "phase");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "phase",
+                table: "subscription_members",
+                newName: "zone");
+        }
+    }
+}

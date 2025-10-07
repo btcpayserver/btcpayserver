84c2caf2c86f86d0cd818e41d7f4fc0ddd2dac67
Nicolas Dorier <nicolas.dorier@gmail.com>
2025-10-07 17:06:26 +0900

    Minor refactoring (#6939)

--- BTCPayServer.Data/ApplicationDbContextFactory.cs
+++ BTCPayServer.Data/ApplicationDbContextFactory.cs
@@ -2,6 +2,7 @@ using System;
 using BTCPayServer.Abstractions.Contracts;
 using BTCPayServer.Abstractions.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
@@ -22,6 +23,11 @@ namespace BTCPayServer.Data
             var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
             builder.UseLoggerFactory(LoggerFactory);
             builder.AddInterceptors(MigrationInterceptor.Instance);
+            builder.ConfigureWarnings(w =>
+            {
+                w.Log((CoreEventId.OptimisticConcurrencyException, LogLevel.Debug));
+                w.Log((CoreEventId.SaveChangesFailed, LogLevel.Debug));
+            });
             ConfigureBuilder(builder, npgsqlOptionsAction);
             return new ApplicationDbContext(builder.Options);
         }

1ed22df3941bac2aa33f1ed71b603d33fd94b120
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-01-28 23:27:07 +0900

    Add nicolasdorier.asc file to check signed commit

--- .circleci/verify-signed-commit.sh
+++ .circleci/verify-signed-commit.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 set -e
 
+gpg --batch --import *.asc
 echo "Checking commit signature..."
 status=$(git log -1 --format="%G?" HEAD)
 
 case "$status" in
   G|U)  ;;   # signed (trusted or not)
-  *) 
+  *)
     echo "ERROR: commit is not properly signed (status: $status)"
     exit 1
     ;;
 esac
+
+echo "The commit is signed"
+git log -1 --show-signature

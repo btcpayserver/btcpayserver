89e1b9203276fbf385f8e1e5c0cc016d33f28c89
thgO.O <thgoil@proton.me>
2026-03-05 14:08:25 -0300

    feat(auth): add wallet permissions and role migration

---
+++ BTCPayServer/Controllers/UIStoresController.WalletAuth.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using BTCPayServer.Client;
+
+namespace BTCPayServer.Controllers;
+
+public partial class UIStoresController
+{
+    private async Task<bool> AuthorizeLightningWalletSettingsAsync(string storeId)
+    {
+        if ((await _authorizationService.AuthorizeAsync(User, storeId, Policies.CanManageWallets)).Succeeded)
+        {
+            await EnsureStoreContextAsync(storeId);
+            return true;
+        }
+        foreach (var policy in new[] { Policies.CanModifyBitcoinLightning, Policies.CanManageWalletSettings })
+        {
+            if (!(await _authorizationService.AuthorizeAsync(User, storeId, policy)).Succeeded)
+                return false;
+        }
+        await EnsureStoreContextAsync(storeId);
+        return true;
+    }
+
+    private async Task EnsureStoreContextAsync(string storeId)
+    {
+        if (HttpContext.GetStoreData() is null)
+        {
+            var store = await _storeRepo.FindStore(storeId);
+            if (store != null)
+                HttpContext.SetStoreData(store);
+        }
+    }
+}

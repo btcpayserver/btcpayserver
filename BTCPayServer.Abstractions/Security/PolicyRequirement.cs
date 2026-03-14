99ddc693364d153fb0f543f56dae484a53991bd4
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-03-14 21:32:55 +0900

    Reintroduce PolicyRequirement(string policy) to avoid breaking plugins

--- BTCPayServer.Abstractions/Security/PolicyRequirement.cs
+++ BTCPayServer.Abstractions/Security/PolicyRequirement.cs
@@ -5,8 +5,12 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace BTCPayServer.Security;
 
-public class PolicyRequirement(string policy, bool requireUnscoped = false) : IAuthorizationRequirement
+public class PolicyRequirement(string policy, bool requireUnscoped) : IAuthorizationRequirement
 {
+    public PolicyRequirement(string policy) : this(policy, false)
+    {
+    }
+
     public bool RequireUnscoped { get; } = requireUnscoped;
     public string Policy { get; } = Permission.IsValidPolicy(policy) ? policy : throw new ArgumentException("Invalid policy (it should be 'btcpay.some.permission.name')", nameof(policy));
 }

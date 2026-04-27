40406327cb98ed5288556a322a122e8941d89577
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-04-27 16:53:15 +0900

    Speed up CI by caching nuget packages

--- .circleci/run-tests.sh
+++ .circleci/run-tests.sh
@@ -5,6 +5,20 @@ cd ../BTCPayServer.Tests
 docker-compose --version
 docker-compose -f "docker-compose.altcoins.yml" down -v
 
+NUGET_CACHE=false
+docker volume create nuget_datadir
+if [ -d "$HOME/.nuget" ]; then
+  echo "Populate nuget_datadir from the cache"
+  docker run --rm \
+    -v nuget_datadir:/target \
+    -v "$HOME/.nuget:/source:ro" \
+    alpine:latest \
+    sh -c 'cp -a /source/. /target/'
+  NUGET_CACHE=true
+else
+  echo "No nuget cache detected"
+fi
+
 # For some reason, docker-compose pull fails time to time, so we try several times
 n=0
 until [ "$n" -ge 10 ]
@@ -16,3 +30,16 @@ done
 
 docker-compose -f "docker-compose.altcoins.yml" build
 docker-compose -f "docker-compose.altcoins.yml" run -e "TEST_FILTERS=$1" tests
+
+if ! $NUGET_CACHE; then
+  rm -rf "$HOME/.nuget"
+  docker create --name temp_extract btcpayservertests-tests
+  docker cp temp_extract:/root/.nuget "$HOME/.nuget"
+  docker rm temp_extract
+  echo "Saving the cache in $HOME/.nuget"
+    docker run --rm \
+      -v nuget_datadir:/source:ro \
+      -v "$HOME/.nuget:/target" \
+      alpine:latest \
+      sh -c 'cp -a /source/. /target/'
+fi

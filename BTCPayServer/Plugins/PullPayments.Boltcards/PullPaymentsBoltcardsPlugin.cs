fee391e4c180e00db28fefb6231a5b89486ee596
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-08-27 09:11:13 +0900

    Remove Boltcard smartcard setup flow (#7515)

--- BTCPayServer/Plugins/PullPayments.Boltcards/PullPaymentsBoltcardsPlugin.cs
+++ BTCPayServer/Plugins/PullPayments.Boltcards/PullPaymentsBoltcardsPlugin.cs
@@ -5,7 +5,6 @@ namespace BTCPayServer.Plugins.PullPayments.Boltcards;
 
 public class PullPaymentsBoltcardsPlugin : BaseBTCPayServerPlugin
 {
-    public const string Area = "PullPayments.Boltcards";
     public override string Identifier => "BTCPayServer.Plugins.PullPayments.Boltcards";
     public override string Name => "Boltcards";
     public override string Description => "Create boltcards from pull payments";
@@ -14,6 +13,5 @@ public class PullPaymentsBoltcardsPlugin : BaseBTCPayServerPlugin
     public override void Execute(IServiceCollection applicationBuilder)
     {
         applicationBuilder.AddUIExtension("pullpayment-store-footer", "/Plugins/PullPayments.Boltcards/Views/BoltcardLinks.cshtml");
-        applicationBuilder.AddUIExtension("pullpayment-foot", "/Plugins/PullPayments.Boltcards/Views/BoltcardScripts.cshtml");
     }
 }

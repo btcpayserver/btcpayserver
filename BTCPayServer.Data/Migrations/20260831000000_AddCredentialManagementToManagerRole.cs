2e5e8fb0c2697ad5a6e196e02a4f80d8799007e1
okjodom <okjodom@gmail.com>
2026-08-31 20:17:33 +0300

    Grant credential management to manager role

---
+++ BTCPayServer.Data/Migrations/20260831000000_AddCredentialManagementToManagerRole.cs
@@ -0,0 +1,35 @@
+using BTCPayServer.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BTCPayServer.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20260831000000_AddCredentialManagementToManagerRole")]
+    public partial class AddCredentialManagementToManagerRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                UPDATE "StoreRoles"
+                SET "Permissions" = COALESCE("Permissions", ARRAY[]::TEXT[]) || ARRAY['btcpay.store.canmanagestorecredentials']::TEXT[]
+                WHERE "Id" = 'Manager'
+                  AND "StoreDataId" IS NULL
+                  AND NOT (COALESCE("Permissions", ARRAY[]::TEXT[]) @> ARRAY['btcpay.store.canmanagestorecredentials']::TEXT[]);
+                """);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                UPDATE "StoreRoles"
+                SET "Permissions" = array_remove("Permissions", 'btcpay.store.canmanagestorecredentials')
+                WHERE "Id" = 'Manager'
+                  AND "StoreDataId" IS NULL;
+                """);
+        }
+    }
+}

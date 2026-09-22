fd21c28e2cb114852b9505c04f837af6d9d0c49f
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-09-22 11:48:06 +0900

    Hide output from passing tests

--- BTCPayServer.Tests/docker-entrypoint.sh
+++ BTCPayServer.Tests/docker-entrypoint.sh
@@ -6,4 +6,4 @@ if [ -n "${TEST_FILTERS:-}" ]; then
 set -- --filter "$TEST_FILTERS"
 fi
 
-dotnet test -c "${CONFIGURATION_NAME}" "$@" --no-build -v n --output Normal --progress on --ansi off --report-gh --xunit-info --xunit-diagnostics on --long-running 180
+dotnet test -c "${CONFIGURATION_NAME}" "$@" --no-build -v n --output Detailed --show-stdout Failed --show-stderr Failed --progress on --ansi off --report-gh --xunit-info --xunit-diagnostics on --long-running 180
